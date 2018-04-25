273ab0b36a9c71a09061a3c57b52d98625384be7
Robert Challen <rob@terminological.co.uk>
2018-04-25 14:03:53 +0100

    / ‘src/main/java/uk/co/terminological/costbenefit/Analysis.java’

--- src/main/java/uk/co/terminological/costbenefit/Analysis.java
+++ src/main/java/uk/co/terminological/costbenefit/Analysis.java
@@ -3,9 +3,14 @@ package uk.co.terminological.costbenefit;
 import java.io.FileNotFoundException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import uk.co.terminological.datatypes.EavMap;
 import uk.co.terminological.parser.ParserException;
@@ -25,20 +30,65 @@ public class Analysis {
 		EavMap<String,String,String> tmp = in.getContents();
 		EavMap<String,String,String> tmp2 = tmp.transpose();
 		
-		List<Boolean> actual = tmp2.get("actual").values().stream().map(convert01TF).collect(Collectors.toList());
-		List<Boolean> predicted = tmp2.get("predicted").values().stream().map(convert01TF).collect(Collectors.toList());
-		List<Double> prob_pos = tmp2.get("prob_pos").values().stream().map(convertToDouble).collect(Collectors.toList());
-		List<Double> prob_neg = tmp2.get("prob_neg").values().stream().map(convertToDouble).collect(Collectors.toList());
+		ClassifierResult res = new ClassifierResult();
+		
+		tmp.streamEntities()
+			.map(kv -> kv.getValue())
+			.map(m -> new Prediction(
+				convert01TF.apply(m.get("actual")),
+				Double.parseDouble(m.get("prediction"))))
+			.forEach(p -> res.add(p));;
 		
 		// tmp.numberEntities();
-		actual.stream().limit(50).forEach(System.out::println);
+		
 		
 		
 
 	}
 
 	static Function<String,Boolean> convert01TF = s -> s.equals("1") ? true : (s.equals("0") ? false: null);
-	static Function<String,Double> convertToDouble = s -> Double.parseDouble(s);
+	
+	public static class Prediction {
+		Prediction(Boolean actual, Double predicted) {
+			this.actual = actual;
+			this.predicted = predicted;
+		}
+		
+		Boolean actual;
+		Double predicted;
+		
+		public Double getPredicted() {return predicted;}
+		public Boolean getActual() {return actual;}
+		
+	}
+	
+	public static class ClassifierResult {
+		
+		List<Prediction> predictions = new ArrayList<>();
+		
+		public void add(Prediction p) {this.predictions.add(p);}
+		
+		public Stream<Prediction> streamByPrediction() {
+			return predictions.stream().sorted(Comparator.comparing(Prediction::getPredicted));
+		}
+		
+		
+		
+	}
+	
+	public static class BandedCutoff {
+		
+	}
+	
+	/*public static class Grouping<X,Y> {
+		Function<X,Y> value;
+		Predicate<Y> test;
+		List<X> members = new ArrayList<>();	
+		 
+		
+	}*/
+	
+	
 	
 }
 
