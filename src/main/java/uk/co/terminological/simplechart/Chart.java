a0144ee88c80c006a06ed3b838e59769db0bb790
Robert Challen <rob@terminological.co.uk>
2018-05-02 12:05:45 +0100

    / ‘src/main/java/uk/co/terminological/simplechart/Chart.java’

--- src/main/java/uk/co/terminological/simplechart/Chart.java
+++ src/main/java/uk/co/terminological/simplechart/Chart.java
@@ -1,15 +1,25 @@
 package uk.co.terminological.simplechart;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
 
+import freemarker.core.ParseException;
+import freemarker.template.MalformedTemplateNameException;
+import freemarker.template.TemplateNotFoundException;
 import uk.co.terminological.datatypes.Tuple;
 
 public class Chart<X> {
 
 	List<X> data;
+	Config config = new Config();
 	List<Tuple<Dimension,Function<X,Object>>> bindings = new ArrayList<>();
+	Map<String,String> customField = new HashMap<>();
 	
 	public Chart(List<X> data) {
 		this.data = data;
@@ -19,23 +29,49 @@ public class Chart<X> {
 		return new Chart<>(data);
 	}
 	
-	public Chart<X> with(Dimension dimension, Function<X,Object> binding) {
+	public Chart<X> bind(Dimension dimension, Function<X,Object> binding) {
 		bindings.add(Tuple.create(dimension, binding));
 		return this;
 	};
 	
-	public void render(ChartType type) {
-		Writer.write(this);
+	public Chart<X> withConfig(Config config) {
+		this.config = config;
+		this.config.chart = this;
+		return this;
 	}
 	
-	public void render(String templateName) {
-		Writer.write(this);
+	public Chart<X> withCustomField(String key, Object value) {
+		if (Arrays.asList("data","config").contains(key)) throw new RuntimeException("Cannot use the custom key: "+key);
+		this.customField.put(key, value.toString());
+		return this;
+	}
+	
+	public Config config() {return config;}
+	
+	public void render(ChartType type) throws IOException {
+		Writer.write(this, type);
+	}
+	
+	public void render(File template) throws IOException {
+		Writer.write(this, template);
 	}
 	
 	public enum Dimension {
 		X,Y,Z,COLOUR,SIZE,LABEL
 	}
 	
-	
+	public static class Config {
+		
+		private Chart<?> chart;
+		Config withChart(Chart<?> chart) {
+			this.chart = chart;
+			this.chart.config = this;
+			return this;
+		}
+		
+		String title; Config withTitle(String title) {this.title = title; return this; }
+		String xLabel; Config withXLabel(String xLabel) {this.xLabel = xLabel; return this; }
+		String yLabel; Config withYLabel(String yLabel) {this.yLabel = yLabel; return this; }
+	}
 	
 }
