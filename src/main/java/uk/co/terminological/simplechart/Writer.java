43fa96181dddff5f0ef83c2ac480c6c0b8b76995
Robert Challen <rob@terminological.co.uk>
2018-05-02 10:50:40 +0100

    / ‘src/main/java/uk/co/terminological/simplechart/Writer.java’

--- src/main/java/uk/co/terminological/simplechart/Writer.java
+++ src/main/java/uk/co/terminological/simplechart/Writer.java
@@ -1,15 +1,26 @@
 package uk.co.terminological.simplechart;
 
 import freemarker.template.Configuration;
+import freemarker.template.DefaultObjectWrapperBuilder;
+import freemarker.template.TemplateExceptionHandler;
+
 
 public class Writer {
 
 	private Configuration cfg;
 	
-	public static void write(Chart<X> chart) {
+	public static <X> void write(Chart<X> chart) {
 		
-		File sele
+		
 		
 	}
 
+	public Writer() {
+		cfg = new Configuration(Configuration.VERSION_2_3_25);
+		cfg.setObjectWrapper(new DefaultObjectWrapperBuilder(Configuration.VERSION_2_3_25).build());
+		cfg.setDefaultEncoding("UTF-8");
+		cfg.setTemplateExceptionHandler(TemplateExceptionHandler.DEBUG_HANDLER);
+		cfg.setClassForTemplateLoading(Writer.class, "/freemarker");
+	}
+	
 }
