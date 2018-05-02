df34fa5173a41732321f5d1262688e1d8fc1da81
Robert Challen <rob@terminological.co.uk>
2018-05-02 10:40:57 +0100

    / ‘src/main/java/uk/co/terminological/charts/Dimension.java’ / ‘src/main/java/uk/co/terminological/simplechart/Chart.java’

--- src/main/java/uk/co/terminological/charts/Dimension.java
+++ src/main/java/uk/co/terminological/charts/Dimension.java
@@ -7,6 +7,7 @@ public enum Dimension {
 	Z,
 	SIZE,
 	COLOUR,
+	LABEL,
 	ALPHA,
 	SHAPE,
 	OMEGA,
--- src/main/java/uk/co/terminological/simplechart/Chart.java
+++ src/main/java/uk/co/terminological/simplechart/Chart.java
@@ -1,5 +1,33 @@
 package uk.co.terminological.simplechart;
 
-public class Chart {
+import java.util.List;
+import java.util.function.Function;
 
+import uk.co.terminological.datatypes.Tuple;
+
+public class Chart<X> {
+
+	ChartType type;
+	List<X> data;
+	List<Tuple<Dimension,Function<X,Object>>> bindings = new ArrayList<>();
+	
+	public Chart(ChartType type,List<X> data) {
+		this.data = data;
+		this.type = type;
+	}
+	
+	public static <X> Chart<X> create(ChartType type,List<X> data) {
+		return new Chart<>(type,data);
+	}
+	
+	public Chart<X> with(Dimension dimension, Function<X,Object> binding) {
+		
+	};
+	
+	public enum Dimension {
+		X,Y,Z,COLOUR,SIZE,LABEL
+	}
+	
+	
+	
 }
