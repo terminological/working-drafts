fd4f99d87464acfe73b6d1760e46991c16199b42
Robert Challen <rob@terminological.co.uk>
2018-05-02 12:06:24 +0100

    / ‘src/main/java/uk/co/terminological/simplechart/Chart.java’

--- src/main/java/uk/co/terminological/simplechart/Chart.java
+++ src/main/java/uk/co/terminological/simplechart/Chart.java
@@ -9,9 +9,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 
-import freemarker.core.ParseException;
-import freemarker.template.MalformedTemplateNameException;
-import freemarker.template.TemplateNotFoundException;
 import uk.co.terminological.datatypes.Tuple;
 
 public class Chart<X> {
