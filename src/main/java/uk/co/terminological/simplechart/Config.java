f588b4541ab79f78a6e6a33e995ab306f7c52b13
Rob Challen <rob@terminological.co.uk>
2018-05-02 21:03:32 +0100

    / ‘src/main/java/uk/co/terminological/simplechart/Config.java’

--- src/main/java/uk/co/terminological/simplechart/Config.java
+++ src/main/java/uk/co/terminological/simplechart/Config.java
@@ -1,32 +1,77 @@
 package uk.co.terminological.simplechart;
 
 public class Config {
-	
+
 	Chart<?> chart;
+	String title;
+	String xLabel;
+	String yLabel;
+	OutputTarget target = OutputTarget.SCREEN;
+
 	
-	private Config() {}
+	// ====== Getters =======
 	
+	public OutputTarget getOutputTarget() {
+		return target;
+	}
+
+	public String getOutputFile() {
+		return chart.getFile(target.getFileType()).getAbsolutePath();
+	}
+
+	public String getYLabel() {
+		return yLabel;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+	
+	public String getXLabel() {
+		return xLabel;
+	}
+
+	
+	// ====== Fluent Builders =======
+	
+	private Config() {
+	}
+
 	public static Config create(Chart<?> chart, String title) {
 		Config out = new Config();
-		out.chart =chart;
+		out.chart = chart;
 		out.title = title;
 		return out;
 	}
-	
+
 	Config withChart(Chart<?> chart) {
 		this.chart = chart;
 		this.chart.config = this;
 		return this;
 	}
-	
-	String title; Config withTitle(String title) {this.title = title; return this; } public String getTitle() {return title;}
-	String xLabel; Config withXLabel(String xLabel) {this.xLabel = xLabel; return this; } public String getXLabel() {return xLabel;}
-	String yLabel; Config withYLabel(String yLabel) {this.yLabel = yLabel; return this; } public String getYLabel() {return yLabel;}
-	OutputTarget target = OutputTarget.SCREEN; Config withOutputTarget(OutputTarget target) {this.target = target; return this;} public OutputTarget getOutputTarget() {return target;}
-	
-	public String getOutputFile() {
-		return chart.getFile(target.getFileType()).getAbsolutePath();
+
+	Config withTitle(String title) {
+		this.title = title;
+		return this;
+	}
+
+	Config withXLabel(String xLabel) {
+		this.xLabel = xLabel;
+		return this;
+	}
+
+	Config withYLabel(String yLabel) {
+		this.yLabel = yLabel;
+		return this;
+	}
+
+	Config withOutputTarget(OutputTarget target) {
+		this.target = target;
+		return this;
 	}
 	
-	public Chart<?> done() {return chart;}
+	public Chart<?> done() {
+		return chart;
+	}
+	
 }
