2a478c33a72545ca6f0f3cabf0ccfbe4b0cabef3
Robert Challen <rob@terminological.co.uk>
2018-06-04 11:16:59 +0100

    / ‘src/main/java/uk/co/terminological/costbenefit/Kumaraswamy.java’

--- src/main/java/uk/co/terminological/costbenefit/Kumaraswamy.java
+++ src/main/java/uk/co/terminological/costbenefit/Kumaraswamy.java
@@ -1,19 +1,43 @@
 package uk.co.terminological.costbenefit;
 
 import org.apache.commons.math3.analysis.ParametricUnivariateFunction;
+import org.apache.commons.math3.exception.DimensionMismatchException;
+import org.apache.commons.math3.exception.NullArgumentException;
 
+/**
+ * Implementation of a parametric Kumaraswamy distribution where we fit the cumulative distribution function
+ * and the first derivative is the probability distribution function. This will generally fit signoidal probability
+ * curves between 0 and 1 
+ * https://en.wikipedia.org/wiki/Kumaraswamy_distribution
+ * @author rc538
+ *
+ */
 public class Kumaraswamy implements ParametricUnivariateFunction {
 
 	@Override
-	public double[] gradient(double x, double... parameters) {
-		// TODO Auto-generated method stub
-		return null;
+	public double[] gradient(double x, double... parameters) throws NullArgumentException, DimensionMismatchException {
+		if (parameters.length != 2) throw new DimensionMismatchException(parameters.length, 2);
+		if (x < 0 || x > 1) throw new NullArgumentException();
+		double a = parameters[0];
+		double b = parameters[1];
+		double xa = Math.pow(x, a); 
+		return new double[] {
+				b*xa*Math.pow(1-xa,b-1)*Math.log(x)
+				,
+				-Math.pow(1-xa,b)*Math.log(1-xa)
+		};
+		
 	}
 
 	@Override
-	public double value(double x, double... parameters) {
-		// TODO Auto-generated method stub
-		return 0;
+	public double value(double x, double... parameters) throws NullArgumentException, DimensionMismatchException {
+		if (parameters.length != 2) throw new DimensionMismatchException(parameters.length, 2);
+		if (x < 0 || x > 1) throw new NullArgumentException();
+		double a = parameters[0];
+		double b = parameters[1];
+		return 1-Math.pow(
+				(1-Math.pow(x, a))
+				,b);
 	}
 
 }
