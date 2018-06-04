de4680a525414623c5a5b875fe9e4b49e9c13f90
Robert Challen <rob@terminological.co.uk>
2018-06-04 11:02:12 +0100

    + ‘src/main/java/uk/co/terminological/costbenefit/Kumaraswamy.java’

---
+++ src/main/java/uk/co/terminological/costbenefit/Kumaraswamy.java
@@ -0,0 +1,19 @@
+package uk.co.terminological.costbenefit;
+
+import org.apache.commons.math3.analysis.ParametricUnivariateFunction;
+
+public class Kumaraswamy implements ParametricUnivariateFunction {
+
+	@Override
+	public double[] gradient(double x, double... parameters) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double value(double x, double... parameters) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
