6b6c2dc121f4c4f4bc3080f16c08f4eab84cc300
Rob Challen <rob@terminological.co.uk>
2018-07-30 11:13:12 +0100

    / ‘src/test/java/uk/co/terminological/deid/TestXmlLoad.java’

--- src/test/java/uk/co/terminological/deid/TestXmlLoad.java
+++ src/test/java/uk/co/terminological/deid/TestXmlLoad.java
@@ -17,9 +17,9 @@ public class TestXmlLoad {
 		XmlText tmp = xml.doXpath("/deIdi2b2/TEXT[1]/text()").getOne(XmlText.class);
 		System.out.print(tmp.getValue());
 		for (XmlElement tags: xml.doXpath("/deIdi2b2/TAGS/*").getMany(XmlElement.class)) {
-			System.out.print("NAME: "+tags.getName());
-			System.out.print("ID: "+tags.getAttributeValue("id"));
-			System.out.print("ID: "+tags.getAttributeValue("start"));
+			System.out.println("NAME: "+tags.getName());
+			System.out.println("ID: "+tags.getAttributeValue("id"));
+			System.out.println("ID: "+tags.getAttributeValue("start"));
 		};
 		
 	}
