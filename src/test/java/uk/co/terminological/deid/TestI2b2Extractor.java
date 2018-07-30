e865c716845250a9293401b564ca53d0f4393e0c
Rob Challen <rob@terminological.co.uk>
2018-07-30 17:09:12 +0100

    / ‘src/test/java/uk/co/terminological/deid/TestI2b2Extractor.java’

--- src/test/java/uk/co/terminological/deid/TestI2b2Extractor.java
+++ src/test/java/uk/co/terminological/deid/TestI2b2Extractor.java
@@ -12,6 +12,7 @@ import javax.management.openmbean.OpenMBeanOperationInfoSupport;
 import org.apache.commons.compress.archivers.ArchiveInputStream;
 import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
 import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
+import org.apache.log4j.BasicConfigurator;
 
 import uk.co.terminological.fluentxml.XmlException;
 
@@ -20,7 +21,7 @@ public class TestI2b2Extractor {
 	static final String INFILE = "/media/data/Data/i2b2/2014Track1/Track1-de-indentification.tar.gz";
 	
 	public static void main(String[] args) throws FileNotFoundException, IOException, XmlException {
-		
+		BasicConfigurator.configure();
 		I2b2Extractor extr = new I2b2Extractor();
 		
 		Writer out = Files.newBufferedWriter(Paths.get("/home/terminological/output.txt"));
