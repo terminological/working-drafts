4e2456cab517b5ac88b4e7151a9e5de96c55a5cb
Rob Challen <rob@terminological.co.uk>
2018-07-30 10:45:00 +0100

    / ‘src/test/java/uk/co/terminological/deid/TestXmlLoad.java’

--- src/test/java/uk/co/terminological/deid/TestXmlLoad.java
+++ src/test/java/uk/co/terminological/deid/TestXmlLoad.java
@@ -1,5 +1,18 @@
 package uk.co.terminological.deid;
 
+import java.io.InputStream;
+
+import uk.co.terminological.fluentxml.Xml;
+import uk.co.terminological.fluentxml.XmlException;
+
 public class TestXmlLoad {
 
+	public static void main(String[] args) throws XmlException {
+		
+		InputStream in = TestXmlLoad.class.getClassLoader().getResourceAsStream("deid/i2b2example.xml");
+		Xml xml = Xml.fromStream(in);
+		System.out.print(xml.doXpath("/deIdi2b2/TEXT[1]/text()").getOne().toString());
+		
+	}
+	
 }
