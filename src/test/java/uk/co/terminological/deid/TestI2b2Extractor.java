f31e02fdbf2937edac3f2fc25497d5c6f41994eb
Rob Challen <rob@terminological.co.uk>
2018-07-30 22:02:39 +0100

    / ‘src/main/resources/deid/CRFmodel.prop’ / ‘src/test/java/uk/co/terminological/deid/TestI2b2Extractor.java’

--- src/test/java/uk/co/terminological/deid/TestI2b2Extractor.java
+++ src/test/java/uk/co/terminological/deid/TestI2b2Extractor.java
@@ -6,12 +6,17 @@ import java.io.IOException;
 import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Properties;
 
 import org.apache.commons.compress.archivers.ArchiveInputStream;
 import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
 import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
 import org.apache.log4j.BasicConfigurator;
 
+import edu.stanford.nlp.ie.crf.CRFClassifier;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.sequences.SeqClassifierFlags;
+import edu.stanford.nlp.util.StringUtils;
 import uk.co.terminological.fluentxml.XmlException;
 
 public class TestI2b2Extractor {
@@ -22,11 +27,15 @@ public class TestI2b2Extractor {
 			"/media/data/Data/i2b2/2014Track1/training-PHI-Gold-Set2.tar.gz"
 	};
 	
+	static final String TRAINING_FILE = "/home/terminological/output.txt";
+	
+	static final String OUTFILE = "/home/terminological/CRFmodel.ser";
+	
 	public static void main(String[] args) throws FileNotFoundException, IOException, XmlException {
 		BasicConfigurator.configure();
 		I2b2Extractor extr = new I2b2Extractor();
 		
-		Writer out = Files.newBufferedWriter(Paths.get("/home/terminological/output.txt"));
+		Writer out = Files.newBufferedWriter(Paths.get(TRAINING_FILE));
 		
 		for (String file: INFILE) {
 		ArchiveInputStream ais = new TarArchiveInputStream(
@@ -37,6 +46,19 @@ public class TestI2b2Extractor {
 		extr.convert(ais, out);
 		}
 		
+		out.close();
+		
 	}
+	
+	public static void trainAndWrite(String modelOutPath, String prop) {
+		   Properties props = StringUtils.propFileToProperties(prop);
+		   props.setProperty("serializeTo", modelOutPath);
+		   //if input use that, else use from properties file.
+		   props.setProperty("trainFile", TRAINING_FILE);
+		   SeqClassifierFlags flags = new SeqClassifierFlags(props);
+		   CRFClassifier<CoreLabel> crf = new CRFClassifier<>(flags);
+		   crf.train();
+		   crf.serializeClassifier(modelOutPath);
+		}
 
 }
