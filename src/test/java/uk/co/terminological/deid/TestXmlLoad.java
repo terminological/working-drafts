ca2ba0f72eb2a660c621fea9c2955fb5dcdec743
Rob Challen <rob@terminological.co.uk>
2018-07-30 11:12:49 +0100

    / ‘src/test/java/uk/co/terminological/deid/TestXmlLoad.java’

--- src/test/java/uk/co/terminological/deid/TestXmlLoad.java
+++ src/test/java/uk/co/terminological/deid/TestXmlLoad.java
@@ -16,7 +16,7 @@ public class TestXmlLoad {
 		Xml xml = Xml.fromStream(in);
 		XmlText tmp = xml.doXpath("/deIdi2b2/TEXT[1]/text()").getOne(XmlText.class);
 		System.out.print(tmp.getValue());
-		for (XmlElement tags: xml.doXpath("/deIdi2b2/TAGS").getMany(XmlElement.class)) {
+		for (XmlElement tags: xml.doXpath("/deIdi2b2/TAGS/*").getMany(XmlElement.class)) {
 			System.out.print("NAME: "+tags.getName());
 			System.out.print("ID: "+tags.getAttributeValue("id"));
 			System.out.print("ID: "+tags.getAttributeValue("start"));
