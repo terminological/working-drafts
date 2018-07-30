1e7883f190f35b88dd3213dbc278fa6482996e43
Rob Challen <rob@terminological.co.uk>
2018-07-30 12:38:40 +0100

    / ‘src/test/java/uk/co/terminological/deid/TestXmlLoad.java’

--- src/test/java/uk/co/terminological/deid/TestXmlLoad.java
+++ src/test/java/uk/co/terminological/deid/TestXmlLoad.java
@@ -49,7 +49,7 @@ public class TestXmlLoad {
 		Properties props = new Properties();
 	    // set the list of annotators to run
 	    // props.setProperty("annotators", "tokenize,ssplit,pos,lemma,ner,parse,depparse,coref,kbp,quote");
-		props.setProperty("annotators", "tokenize,ssplit,ner");
+		props.setProperty("annotators", "tokenize,ssplit,pos,lemma,ner");
 	    // set a property for an annotator, in this case the coref annotator is being set to use the neural algorithm
 	    props.setProperty("coref.algorithm", "neural");
 	    // build pipeline
