9569f112d3b453578fc8e66a33dfce5060bb5de3
Rob Challen <rob@terminological.co.uk>
2018-08-07 12:36:02 +0100

    / ‘src/main/java/uk/co/terminological/pubmedclient/PubMedBatch.java’

--- src/main/java/uk/co/terminological/pubmedclient/PubMedBatch.java
+++ src/main/java/uk/co/terminological/pubmedclient/PubMedBatch.java
@@ -1,13 +1,20 @@
 package uk.co.terminological.pubmedclient;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Iterator;
 
-public class PubMedBatch {
+import gov.nih.nlm.ncbi.eutils.generated.efetch.PubmedArticleSet;
+import gov.nih.nlm.ncbi.eutils.generated.esearch.ESearchResult;
+import gov.nih.nlm.ncbi.eutils.generated.esummary.ESummaryResult;
+
+public class PubMedBatch implements Iterator<PubMedBatch> {
 	
 	private PubMedBatchQuery.Page page;
 	private PubMedBatchQuery query;
 	private String queryString;
+	private ESearchResult search;
+	private ESummaryResult summary;
+	private PubmedArticleSet articles;
+	private int resultSize = 0;
 	
 	protected PubMedBatch(PubMedBatchQuery query, String queryString, PubMedBatchQuery.Page page) {
 		this.query = query;
@@ -16,4 +23,13 @@ public class PubMedBatch {
 		
 	}
 	
+	@Override
+	public boolean hasNext() {
+		return page.start+page.batchSize < resultSize ;
+	}
+
+	@Override
+	public PubMedBatch next() {
+		return new PubMedBatch(query, queryString, page.withStart(page.start+page.batchSize));
+	}
 }
