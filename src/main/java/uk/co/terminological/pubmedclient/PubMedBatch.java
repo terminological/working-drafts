a83954b116b14e8e040d9eaed6c7c10aeae96d27
Rob Challen <rob@terminological.co.uk>
2018-08-07 12:57:13 +0100

    / ‘src/main/java/uk/co/terminological/pubmedclient/PubMedBatch.java’

--- src/main/java/uk/co/terminological/pubmedclient/PubMedBatch.java
+++ src/main/java/uk/co/terminological/pubmedclient/PubMedBatch.java
@@ -1,9 +1,12 @@
 package uk.co.terminological.pubmedclient;
 
 import java.util.Iterator;
+import java.util.List;
+
+import javax.xml.bind.JAXBException;
 
 import gov.nih.nlm.ncbi.eutils.generated.efetch.PubmedArticleSet;
-import gov.nih.nlm.ncbi.eutils.generated.esearch.ESearchResult;
+import gov.nih.nlm.ncbi.eutils.generated.esearch.*;
 import gov.nih.nlm.ncbi.eutils.generated.esummary.ESummaryResult;
 
 public class PubMedBatch implements Iterator<PubMedBatch> {
@@ -20,7 +23,22 @@ public class PubMedBatch implements Iterator<PubMedBatch> {
 		this.query = query;
 		this.page = page;
 		this.queryString = queryString;
-		
+		try {
+			this.search = query.getClient().searchPubmed(queryString, page.start, page.batchSize);
+			this.resultSize = search.getCountOrRetMaxOrRetStartOrQueryKeyOrWebEnvOrIdListOrTranslationSetOrTranslationStackOrQueryTranslationOrERROR().stream()
+					.filter(a -> a instanceof Count).map(a -> Integer.parseInt(((Count) a).getvalue()))
+					.findFirst().orElse(0);
+		} catch (JAXBException e) {
+			throw new RuntimeException("Some sort of API parsing error has occurred",e);
+		}
+	}
+	
+	public List<String> getTitles() {
+		if (articles != null) {
+			articles.getPubmedArticleOrPubmedBookArticle().stream()
+				.filter(a -> a instanceof PubmedArticle).map(a -> (PubmedArticle) a)
+				.map(a -> a.)
+		}
 	}
 	
 	@Override
