c3c41089b5d7d5aa4b4d3feeebb4c62b78ff2af0
Robert Challen <rob@terminological.co.uk>
2018-08-08 10:32:35 +0100

    / ‘install-models.sh’

--- install-models.sh
+++ install-models.sh
@@ -3,15 +3,18 @@
 # /media/data/Data/coreNLP
 # loc="$1"
 
-loc="/media/data/Data/coreNLP"
-mvn install:install-file -Dfile="$loc/stanford-english-corenlp-2018-02-27-models.jar" \
+wget "http://nlp.stanford.edu/software/stanford-english-corenlp-2018-02-27-models.jar"
+wget "http://nlp.stanford.edu/software/stanford-english-kbp-corenlp-2018-02-27-models.jar"
+
+# loc="/media/data/Data/coreNLP"
+mvn install:install-file -Dfile="stanford-english-corenlp-2018-02-27-models.jar" \
                          -DgroupId=edu.stanford.nlp \
                          -DartifactId=stanford-corenlp-eng \
                          -Dversion=3.9.1 \
                          -Dpackaging=jar \
                          -DgeneratePom=true
                          
-mvn install:install-file -Dfile="$loc/stanford-english-corenlp-2018-02-27-models.jar" \
+mvn install:install-file -Dfile="stanford-english-corenlp-2018-02-27-models.jar" \
                          -DgroupId=edu.stanford.nlp \
                          -DartifactId=stanford-corenlp-eng-additional \
                          -Dversion=3.9.1 \
