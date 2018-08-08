96584a8131b381f2e60f07fe6dbb3ead554337e6
Robert Challen <rob@terminological.co.uk>
2018-08-08 18:01:48 +0100

    / ‘deid/src/main/java/uk/co/terminological/deid/CommonFormat.java’ / ‘deid/src/main/java/uk/co/terminological/deid/I2b2_2006_Format.java’ / ‘deid/src/main/java/uk/co/terminological/deid/I2b2_2014_Format.java’

--- deid/src/main/java/uk/co/terminological/deid/I2b2_2006_Format.java
+++ deid/src/main/java/uk/co/terminological/deid/I2b2_2006_Format.java
@@ -1,5 +1,18 @@
 package uk.co.terminological.deid;
 
+import java.io.InputStream;
+
+import uk.co.terminological.fluentxml.Xml;
+import uk.co.terminological.fluentxml.XmlException;
+
 public class I2b2_2006_Format {
 
+	Xml xml;
+	
+	public I2b2_2006_Format(InputStream is) throws XmlException {
+		xml = Xml.fromStream(is);
+	}
+	
+	
+	
 }
--- deid/src/main/java/uk/co/terminological/deid/I2b2_2014_Format.java
+++ deid/src/main/java/uk/co/terminological/deid/I2b2_2014_Format.java
@@ -55,40 +55,5 @@ public class I2b2_2014_Format {
 		return spans;
 	}
 		
-	public static class Spans extends FluentList<Span> {}
 	
-	public static class Span implements Comparable<Span> {
-		Integer start;
-		Integer end;
-		String type;
-		String subtype;
-		
-		public static Span from(Integer start, Integer end, String type, String subtype) {
-			Span out = new Span(); out.start = start; out.end = end; out.type = type; out.subtype = subtype; return out;
-		}
-
-		@Override
-		public int compareTo(Span that) {
-			return this.start.compareTo(that.start) != 0 ?
-					this.start.compareTo(that.start) :
-						this.end.compareTo(that.end);
-		}
-		
-		public boolean intersects(Integer start, Integer end) {
-			return (this.start <= start && this.end >= start) ||
-					(this.start <= end && this.end >= end);
-		}
-		
-		public boolean after(Integer end) {
-			return (this.start > end);
-		}
-		
-		public boolean before(Integer start) {
-			return (this.end < start);
-		}
-
-		public boolean isType(String string) {
-			return type.equals(string);
-		}
-	}
 }
