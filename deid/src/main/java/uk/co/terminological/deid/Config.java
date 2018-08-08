ee36c9e41c73b7989bf63881f2c999a618345bbe
Robert Challen <rob@terminological.co.uk>
2018-08-08 16:25:55 +0100

    / ‘deid/src/main/java/uk/co/terminological/deid/Config.java’

--- deid/src/main/java/uk/co/terminological/deid/Config.java
+++ deid/src/main/java/uk/co/terminological/deid/Config.java
@@ -2,22 +2,25 @@ package uk.co.terminological.deid;
 
 public class Config {
 
-	static final String[] INFILE = {
-			"/media/data/Data/i2b2/2014Track1/Track1-de-indentification.tar.gz",
-			"/media/data/Data/i2b2/2014Track1/training-PHI-Gold-Set1.tar.gz",
-			"/media/data/Data/i2b2/2014Track1/training-PHI-Gold-Set2.tar.gz"
+	static String HOME = "/home/rc538";
+	
+	static String[] INFILE = {
+			HOME+"/Data/i2b2/2014Track1/Track1-de-indentification.tar.gz",
+			HOME+"/Data/i2b2/2014Track1/training-PHI-Gold-Set1.tar.gz",
+			HOME+"/Data/i2b2/2014Track1/training-PHI-Gold-Set2.tar.gz"
 	};
 	
-	static final String[] TESTFILE = {
-			"/media/data/Data/i2b2/2014Track1/training-PHI-Gold-Set1.tar.gz"
+	static String[] TESTFILE = {
+			HOME+"/Data/i2b2/2014Track1/training-PHI-Gold-Set1.tar.gz"
 	};
 	
-	static final String TRAINING_FILE = "/home/terminological/train.txt";
-	static final String TESTING_FILE = "/home/terminological/test.txt";
+	static String TRAINING_FILE = HOME+"/home/terminological/train.txt";
+	static String TESTING_FILE = HOME+"/home/terminological/test.txt";
 	
-	static final String OUTFILE = "/home/terminological/CRFmodel.ser";
+	static String OUTFILE = "/home/terminological/CRFmodel.ser";
 	
-	static final String PROP = TestI2b2Extractor.class.getClassLoader().getResource("deid/CRFmodel.prop").getFile();
-	static final String GAZETTE = TestI2b2Extractor.class.getClassLoader().getResource("deid/lastNamesGazette.txt").getFile();
+	static String PROP = Config.class.getClassLoader().getResource("deid/CRFmodel.prop").getFile();
+	
+	static String GAZETTE = Config.class.getClassLoader().getResource("deid/lastNamesGazette.txt").getFile();
 	
 }
