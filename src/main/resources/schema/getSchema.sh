dbdae9a7e60b271514e341b718dde3baafaada37
Robert Challen <rob@terminological.co.uk>
2018-08-08 11:03:05 +0100

    + ‘src/main/resources/schema/getSchema.sh’

---
+++ src/main/resources/schema/getSchema.sh
@@ -0,0 +1,4 @@
+wget https://eutils.ncbi.nlm.nih.gov/eutils/dtd/20060628/esearch.dtd
+wget https://dtd.nlm.nih.gov/ncbi/pubmed/out/pubmed_180101.dtd
+wget https://eutils.ncbi.nlm.nih.gov/eutils/dtd/20041029/esummary-v1.dtd
+wget http://dtd.nlm.nih.gov/ncbi/pmc/articleset/nlm-articleset-2.0.dtd
