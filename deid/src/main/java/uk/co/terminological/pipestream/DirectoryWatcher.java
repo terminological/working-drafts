299dfcd042ac0243b11f209314330d4fd3cae923
Robert Challen <rob@terminological.co.uk>
2018-08-09 16:15:02 +0100

    / ‘deid/src/main/java/uk/co/terminological/pipestream/DirectoryWatcher.java’

--- deid/src/main/java/uk/co/terminological/pipestream/DirectoryWatcher.java
+++ deid/src/main/java/uk/co/terminological/pipestream/DirectoryWatcher.java
@@ -1,5 +1,18 @@
 package uk.co.terminological.pipestream;
 
-public class DirectoryWatcher {
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.List;
 
+public interface DirectoryWatcher {
+
+	public default void registerWith(Manager manager) {
+		manager.register(this);
+	};
+	
+	public void watchDirectory(File directory, FilenameFilter filter);
+	public File getWatchedDirectory();
+	public List<File> getChangedFiles();
+	public void setProcessed(File file); 
+	
 }
