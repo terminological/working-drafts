1783ba72925951a09875f172c58960c3e1182fc7
Rob Challen <rob@terminological.co.uk>
2018-08-10 15:23:38 +0100

    / ‘deid/src/main/java/uk/co/terminological/pipestream/idea3/Test.java’

--- deid/src/main/java/uk/co/terminological/pipestream/idea3/Test.java
+++ deid/src/main/java/uk/co/terminological/pipestream/idea3/Test.java
@@ -1,5 +1,29 @@
 package uk.co.terminological.pipestream.idea3;
 
 public class Test {
+	
+	
+	public interface Event {}
+	
+	public interface EventBus {
+		
+		void registerHandler(EventHandler<?> handler);
+		void receive(Event message);
+		void dispatchMessages();
+		
+	}
+	
+		
+	public interface EventHandler<X extends Event> {
+		void handle(X message);
+	}
+	
+	
+	public interface EventGenerator<X extends Event> {
+		
+		EventBus eventBus();
+		void sendMessage(X message);
+		
+	}
 
 }
