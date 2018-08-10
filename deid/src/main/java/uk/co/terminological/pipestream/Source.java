6c84cbb7cbe30542df19475b484fcc5803ab7b22
Rob Challen <rob@terminological.co.uk>
2018-08-10 13:58:13 +0100

    / ‘deid/src/main/java/uk/co/terminological/pipestream/Source.java’ + ‘deid/src/main/java/uk/co/terminological/pipestream/idea1/’

--- deid/src/main/java/uk/co/terminological/pipestream/Source.java
+++ deid/src/main/java/uk/co/terminological/pipestream/Source.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.InputStream;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 public interface Source extends Iterator<InputStream> {
 
