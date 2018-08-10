97dbd0eeb541c95eed3c6a3dddc976c191817846
Rob Challen <rob@terminological.co.uk>
2018-08-10 15:59:44 +0100

    / ‘deid/src/main/java/uk/co/terminological/pipestream/idea3/Test.java’

--- deid/src/main/java/uk/co/terminological/pipestream/idea3/Test.java
+++ deid/src/main/java/uk/co/terminological/pipestream/idea3/Test.java
@@ -1,29 +1,96 @@
 package uk.co.terminological.pipestream.idea3;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Optional;
+
 public class Test {
 	
 	
-	public interface Event {}
+	public interface Event<Y> extends Cloneable {
+		Optional<String> name(Y instance);
+		String typeDescription();
+		Class<Y> getType();
+		Y getCopy();
+	}
 	
 	public interface EventBus {
-		
 		void registerHandler(EventHandler<?> handler);
-		void receive(Event message);
-		void dispatchMessages();
-		
+		void receive(Event<?> event);
 	}
 	
 		
-	public interface EventHandler<X extends Event> {
-		void handle(X message);
+	public interface EventHandler<X extends Event<?>> {
+		boolean canHandle(X event);
+		void handle(X event);
 	}
 	
 	
-	public interface EventGenerator<X extends Event> {
-		
+	public interface EventGenerator<Y> {
 		EventBus eventBus();
-		void sendMessage(X message);
-		
+		void send(Event<Y> event);
 	}
 
+	public interface Adaptor<X extends Event<?>,Y> extends EventHandler<X>, EventGenerator<Y> {
+		
+	}
+	
+	
+	
+	
+	public class InputStreamAvailableEvent implements Event<InputStream> {
+
+		Path file;
+		String key;
+		
+		InputStreamAvailableEvent(Path file, String key) {
+			this.file = file;
+		}
+		
+		@Override
+		public Optional<String> name(InputStream instance) {
+			return Optional.of(file.toString());
+		}
+
+		@Override
+		public String typeDescription() {
+			return key;
+		}
+
+		@Override
+		public Class<InputStream> getType() {
+			return InputStream.class;
+		}
+
+		@Override
+		public InputStream getCopy() {
+			return Files.newInputStream(file);
+		}
+		
+	}
+	
+	public class Reader implements EventGenerator<InputStream> {
+
+		EventBus bus;
+		public Reader(EventBus bus, Path file, String key) {
+			this.bus = bus;
+			file.toFile().exists()
+			send(new InputStreamAvailableEvent(file, key));
+		}
+		
+		@Override
+		public EventBus eventBus() {
+			return bus;
+		}
+
+		@Override
+		public void send(Event<InputStream> event) {
+			eventBus().receive(event);
+		}
+		
+	}
+	
 }
