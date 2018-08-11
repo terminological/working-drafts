7e0ef2199da93b50597527ee4f1f83913180e8a9
Robert Challen <rob@terminological.co.uk>
2018-08-11 09:35:42 +0100

    / ‘deid/src/main/java/uk/co/terminological/pipestream/idea3/Event.java’

--- deid/src/main/java/uk/co/terminological/pipestream/idea3/Event.java
+++ deid/src/main/java/uk/co/terminological/pipestream/idea3/Event.java
@@ -2,9 +2,39 @@ package uk.co.terminological.pipestream.idea3;
 
 import java.util.Optional;
 
-public interface Event<Y> extends Cloneable {
+public interface Event<Y> {
 	Optional<String> name();
 	String typeDescription();
 	Class<Y> getType();
-	Y getCopy();
+	Y get();
+	
+	public static class Default<Y> implements Event<Y> {
+
+		Y message;
+		public Default(Y message) {
+			this.message = message;
+		};
+		
+		@Override
+		public Optional<String> name() {
+			return Optional.of(Integer.toHexString(message.hashCode()));
+		}
+
+		@Override
+		public String typeDescription() {
+			return getType().getCanonicalName();
+		}
+
+		@SuppressWarnings("unchecked")
+		@Override
+		public Class<Y> getType() {
+			return (Class<Y>) message.getClass();
+		}
+
+		@Override
+		public Y get() {
+			return message;
+		}
+		
+	}
 }
