4498c9227809bc5cb7aa8a3bbcab37970448a015
Robert Challen <rob@terminological.co.uk>
2018-08-11 14:11:17 +0100

    / ‘deid/src/main/java/uk/co/terminological/pipestream/idea3/EventHandler.java’

--- deid/src/main/java/uk/co/terminological/pipestream/idea3/EventHandler.java
+++ deid/src/main/java/uk/co/terminological/pipestream/idea3/EventHandler.java
@@ -3,4 +3,27 @@ package uk.co.terminological.pipestream.idea3;
 public interface EventHandler<X extends Event<?>> extends EventBusAware {
 	boolean canHandle(Event<?> event);
 	void handle(X event);
+	
+	public static abstract class Default<X extends Event<?>> implements EventHandler<X> {
+
+		EventBus bus;
+		
+		@Override
+		public EventBus getEventBus() {
+			return bus;
+		}
+
+		@Override
+		public void setEventBus(EventBus eventBus) {
+			this.bus = eventBus;
+		}
+		
+		public Default() {
+			EventBus.get().registerHandler((EventHandler<Event<?>>) this); 
+		}
+
+		
+	}
+	
+	
 }
