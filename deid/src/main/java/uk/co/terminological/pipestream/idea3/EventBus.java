a3d776683e3dfaa9cc836fb20912bde19f25ea03
Robert Challen <rob@terminological.co.uk>
2018-08-11 10:46:34 +0100

    / ‘deid/src/main/java/uk/co/terminological/pipestream/idea3/EventBus.java’

--- deid/src/main/java/uk/co/terminological/pipestream/idea3/EventBus.java
+++ deid/src/main/java/uk/co/terminological/pipestream/idea3/EventBus.java
@@ -1,18 +1,61 @@
 package uk.co.terminological.pipestream.idea3;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
-public interface EventBus {
-	void registerHandlerGenerator(EventHandlerGenerator<?> handler);
-	void registerHandler(EventHandler<?> handler);
-	void receive(Event<?> event);
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EventBus {
 	
-	void handleException(Exception e);
-	void logError(String message);
-	void logInfo(String message);
+	static EventBus singleton;
+	public static EventBus get() {
+		if (singleton == null) singleton = new EventBus();
+		return singleton;
+	}
 	
-	List<EventHandler<?>> getHandlers();
-	List<EventHandlerGenerator<?>> getHandlerGenerators();
+	List<EventHandler<?>> handlers = new ArrayList<>();
+	List<EventHandlerGenerator<?>> handlerGenerators = new ArrayList<>();;
 	
-	void releaseHandler(EventHandler<?> handler);
+	Logger log = LoggerFactory.getLogger(EventBus.class);
+	
+	
+	void registerHandlerGenerator(EventHandlerGenerator<?> handlerGenerator) {
+		handlerGenerators.add(handlerGenerator);
+	};
+	void registerHandler(EventHandler<?> handler) {
+		handlers.add(handler);
+	};
+	
+	<X extends Event<?>> void receive(X event) {
+		//TODO do something in parallel here unsing ? fibers
+		// https://github.com/reactive-streams/reactive-streams-jvm/blob/v1.0.0/README.md
+		// http://www.paralleluniverse.co/quasar/
+		handlers.parallelStream().filter(h -> h.canHandle(event)).forEach(h -> h.handle(event));
+	};
+	
+	
+	void handleException(Exception e) {
+		PrintStream ps = new PrintStream(new ByteArrayOutputStream());
+		e.printStackTrace(ps);
+		logError(ps.toString());
+	};
+	
+	
+	void logError(String message) {
+		log.error(message);
+	};
+	void logInfo(String message) {
+		log.info(message);
+	};
+	
+	List<EventHandler<?>> getHandlers() {return handlers;};
+	List<EventHandlerGenerator<?>> getHandlerGenerators() {return handlerGenerators;}
+	
+	void releaseHandler(EventHandler<?> handler) {
+		handlers.remove(handler);
+	};
 }
