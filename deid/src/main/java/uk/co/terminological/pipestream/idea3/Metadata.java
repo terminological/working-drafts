b11453aad7518383bffa76d132b34bf410945965
Rob Challen <rob@terminological.co.uk>
2018-08-13 10:03:22 +0100

    / ‘deid/src/main/java/uk/co/terminological/pipestream/idea3/Event.java’ + ‘deid/src/main/java/uk/co/terminological/pipestream/idea3/Metadata.java’

---
+++ deid/src/main/java/uk/co/terminological/pipestream/idea3/Metadata.java
@@ -0,0 +1,41 @@
+package uk.co.terminological.pipestream.idea3;
+
+import java.io.Serializable;
+import java.util.Optional;
+
+import uk.co.terminological.pipestream.idea3.Event.Metadata;
+
+public class Metadata<Y> implements Serializable {
+	
+	String name;
+	String typeDescription;
+	Class<Y> type;
+	boolean multiProcess;
+	
+	public Optional<String> name() {
+		return Optional.ofNullable(name);
+	};
+	public String typeDescription() {
+		return typeDescription;
+	};
+	public Class<Y> getType(){
+		return type;
+	};
+	public boolean multiProcess() {
+		return multiProcess;
+	}
+	
+	public Metadata(String name, String typeDescription, Class<Y> type,	boolean multiProcess) {
+		this.name = name; this.typeDescription = typeDescription; this.type = type; this.multiProcess = multiProcess;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static <Z> Metadata<Z> defaultFor(Z instance) {
+		Metadata<Z> out = new Metadata<Z>(
+				Integer.toHexString(instance.hashCode()),
+				instance.getClass().getCanonicalName(),
+				(Class<Z>) instance.getClass(),
+				true);
+		return out;
+	}
+}
