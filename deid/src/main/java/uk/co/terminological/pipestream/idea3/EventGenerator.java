bc4f8dfc4b7cc526c2b26037de0b33d146c8eb63
Rob Challen <rob@terminological.co.uk>
2018-08-13 11:15:42 +0100

    / ‘deid/src/main/java/uk/co/terminological/pipestream/idea3/EventGenerator.java’ / ‘deid/src/main/java/uk/co/terminological/pipestream/idea3/Handlers.java’

--- deid/src/main/java/uk/co/terminological/pipestream/idea3/EventGenerator.java
+++ deid/src/main/java/uk/co/terminological/pipestream/idea3/EventGenerator.java
@@ -4,9 +4,6 @@ public interface EventGenerator<Y> extends EventBusAware {
 	void send(Event<Y> event);
 	
 	public static abstract class Default<Y> implements EventGenerator<Y> {
-
-		
-		
 		
 		public void send(Event<Y> event) {
 			getEventBus().receive(event);
--- deid/src/main/java/uk/co/terminological/pipestream/idea3/Handlers.java
+++ deid/src/main/java/uk/co/terminological/pipestream/idea3/Handlers.java
@@ -3,7 +3,25 @@ package uk.co.terminological.pipestream.idea3;
 public class Handlers {
 
 	
-	public interface Adaptor<X extends Event<?>,Y> extends EventHandler<X>, EventGenerator<Y> {
+	public class Adaptor<X,Y> extends EventGenerator.Default<Y> implements EventHandler<Event<X>>  {
+
+		@Override
+		public boolean canHandle(Event<?> event) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public void handle(Event<X> event) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public HandlerMetadata getMetadata() {
+			// TODO Auto-generated method stub
+			return null;
+		}
 		
 	}
 	
@@ -23,11 +41,21 @@ public class Handlers {
 		}
 
 		@Override
-		public HandlerMetadata<? extends EventHandler<Event<?>>> getMetadata() {
+		public HandlerMetadata getMetadata() {
 			// TODO Auto-generated method stub
 			return null;
 		}
 		
 	}
+	
+	/*
+	public interface EventIntegrator extends EventHandler<Event<?>> {
+		public String eventName();
+	}
+	
+	public interface EventIntegratorGenerator extends EventHandlerGenerator<Event<?>> {
+		public String eventName();
+	}
+	*/
 	
 }
