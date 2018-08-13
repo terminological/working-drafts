db334a0827739dd9b6f125035d58e783133917b6
Rob Challen <rob@terminological.co.uk>
2018-08-13 10:47:47 +0100

    / ‘deid/src/main/java/uk/co/terminological/pipestream/idea3/EventHandlerGenerator.java’

--- deid/src/main/java/uk/co/terminological/pipestream/idea3/EventHandlerGenerator.java
+++ deid/src/main/java/uk/co/terminological/pipestream/idea3/EventHandlerGenerator.java
@@ -2,7 +2,7 @@ package uk.co.terminological.pipestream.idea3;
 
 public interface EventHandlerGenerator<X extends Event<?>> extends EventBusAware {
 	boolean canCreateHandler(X event);
-	EventHandler<X> createHandlerAndHandle(X event);
+	EventHandler<X> createHandlerFor(X event);
 	
 	public static abstract class Default<X extends Event<?>> implements EventHandlerGenerator<X> {
 
