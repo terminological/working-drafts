2a34b07af4128cc6ba099507f07ee9ec136c12d5
Rob Challen <rob@terminological.co.uk>
2018-08-13 12:49:03 +0100

    / ‘deid/src/main/java/uk/co/terminological/pipestream/idea3/EventGenerator.java’

--- deid/src/main/java/uk/co/terminological/pipestream/idea3/EventGenerator.java
+++ deid/src/main/java/uk/co/terminological/pipestream/idea3/EventGenerator.java
@@ -5,18 +5,14 @@ import java.util.Optional;
 public interface EventGenerator<Y> extends EventBusAware {
 	
 	public Metadata getMetadata();
-	void send(Event<Y> event, Metadata sender);
+	void send(Event<Y> event);
 	
 	public static abstract class Default<Y> implements EventGenerator<Y> {
 		
-		public void send(Event<Y> event, Metadata sender) {
-			getEventBus().receive(event, sender);
+		public void send(Event<Y> event) {
+			getEventBus().receive(event, getMetadata());
 		}
 
-		public void send(Event<Y> event) {
-			send(event, getMetadata());
-		}
-		
 		public abstract Optional<Event<Y>> generate();
 		
 		public boolean execute() {
