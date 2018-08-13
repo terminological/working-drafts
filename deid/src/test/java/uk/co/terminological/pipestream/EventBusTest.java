60be7469ecfc03cd35a73e551fa743c47344ae1e
Rob Challen <rob@terminological.co.uk>
2018-08-13 12:08:00 +0100

    + ‘deid/src/test/java/uk/co/terminological/pipestream/EventBusTest.java’

---
+++ deid/src/test/java/uk/co/terminological/pipestream/EventBusTest.java
@@ -0,0 +1,19 @@
+package uk.co.terminological.pipestream;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class EventBusTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public final void test() {
+		fail("Not yet implemented");
+	}
+
+}
