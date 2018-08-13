e846f9919ec0b0a07d808c6b6f8b1947f0cc2e1d
Rob Challen <rob@terminological.co.uk>
2018-08-13 09:37:06 +0100

    / ‘deid/src/main/java/uk/co/terminological/pipestream/idea3/Event.java’

--- deid/src/main/java/uk/co/terminological/pipestream/idea3/Event.java
+++ deid/src/main/java/uk/co/terminological/pipestream/idea3/Event.java
@@ -1,33 +1,56 @@
 package uk.co.terminological.pipestream.idea3;
 
+import java.io.Serializable;
 import java.util.Optional;
 
 public interface Event<Y> {
-	Optional<String> name();
-	String typeDescription();
-	Class<Y> getType();
+	
+	Metadata<Y> getMetadata();
 	Y get();
-	default boolean multiProcess() {return true;}
+	
+	
+	
+	public static class Metadata<Y> implements Serializable {
+		
+		String name;
+		String typeDescription;
+		Class<Y> type;
+		boolean multiProcess;
+		
+		public Optional<String> name() {
+			return Optional.ofNullable(name);
+		};
+		public String typeDescription() {
+			return typeDescription;
+		};
+		public Class<Y> getType(){
+			return type;
+		};
+		public boolean multiProcess() {
+			return multiProcess;
+		}
+		
+		public Metadata(String name, String typeDescription, Class<Y> type,	boolean multiProcess) {
+			this.name = name; this.typeDescription = typeDescription; this.type = type; this.multiProcess = multiProcess;
+		}
+		
+		@SuppressWarnings("unchecked")
+		public static <Z> Metadata<Z> defaultFor(Z instance) {
+			Metadata<Z> out = new Metadata<Z>(
+					Integer.toHexString(instance.hashCode()),
+					instance.getClass().getCanonicalName(),
+					(Class<Z>) instance.getClass(),
+					true);
+			return out;
+		}
+	}
+	
 	
 	public static abstract class Default<Y> implements Event<Y> {
 
-		@Override
-		public Optional<String> name() {
-			return Optional.of(Integer.toHexString(get().hashCode()));
+		public Metadata<Y> getMetadata() {
+			return Metadata.defaultFor(this.get());
 		}
 
-		@Override
-		public String typeDescription() {
-			return getType().getCanonicalName();
-		}
-
-		@SuppressWarnings("unchecked")
-		@Override
-		public Class<Y> getType() {
-			return (Class<Y>) get().getClass();
-		}
-
-		
-		
 	}
 }
