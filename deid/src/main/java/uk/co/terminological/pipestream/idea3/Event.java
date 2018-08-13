2ca8ecc44896b4b34321cca3115ac2a32b04d735
Rob Challen <rob@terminological.co.uk>
2018-08-13 10:08:30 +0100

    / ‘deid/src/main/java/uk/co/terminological/pipestream/idea3/Event.java’

--- deid/src/main/java/uk/co/terminological/pipestream/idea3/Event.java
+++ deid/src/main/java/uk/co/terminological/pipestream/idea3/Event.java
@@ -1,16 +1,38 @@
 package uk.co.terminological.pipestream.idea3;
 
 public interface Event<Y> {
-	
+
 	Metadata<Y> getMetadata();
 	Y get();
-	
-	
-	
+
+	public static class EventMetadata<Y> extends Metadata<Y> {
+		
+		boolean multiProcess;
+		public boolean multiProcess() {
+			return multiProcess;
+		}
+
+		public EventMetadata(String name, String typeDescription, Class<Y> type, boolean multiProcess) {
+			super(name,typeDescription,type); 
+			this.multiProcess = multiProcess;
+		}
+
+		@SuppressWarnings("unchecked")
+		public static <Z> EventMetadata<Z> defaultFor(Z instance) {
+			EventMetadata<Z> out = new EventMetadata<Z>(
+					Integer.toHexString(instance.hashCode()),
+					instance.getClass().getCanonicalName(),
+					(Class<Z>) instance.getClass(),
+					true);
+			return out;
+		}
+	}
+
+
 	public static abstract class Default<Y> implements Event<Y> {
 
-		public Metadata<Y> getMetadata() {
-			return Metadata.defaultFor(this.get());
+		public EventMetadata<Y> getMetadata() {
+			return EventMetadata.defaultFor(this.get());
 		}
 
 	}
