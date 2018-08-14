9fe2e98f1147c8757a6425267a44c7a4a5c6ee5c
Rob Challen <rob@terminological.co.uk>
2018-08-14 12:35:09 +0100

    / ‘deid/src/main/java/uk/co/terminological/pipestream/Event.java’

--- deid/src/main/java/uk/co/terminological/pipestream/Event.java
+++ deid/src/main/java/uk/co/terminological/pipestream/Event.java
@@ -24,25 +24,6 @@ public interface Event<Y> {
 			 this.reusable = multiProcess;
 		}
 
-		@SuppressWarnings("unchecked")
-		public static <Z> EventMetadata<Z> defaultFor(Z instance) {
-			EventMetadata<Z> out = new EventMetadata<Z>(
-					Integer.toHexString(instance.hashCode()),
-					instance.getClass().getCanonicalName(),
-					(Class<Z>) instance.getClass(),
-					true);
-			return out;
-		}
-		
-		public static <Z> EventMetadata<Z> named(Class<Z> className, String name) {
-			EventMetadata<Z> out = new EventMetadata<Z>(
-					name,
-					className.getCanonicalName(),
-					className,
-					true);
-			return out;
-		}
-		
 		public EventMetadata<Y> singleShot() {
 			this.reusable = false;
 			return this;
