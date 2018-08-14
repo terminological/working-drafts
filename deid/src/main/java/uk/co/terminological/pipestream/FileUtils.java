3958126bb5a0aa6621a660864a9d6820ffce1967
Rob Challen <rob@terminological.co.uk>
2018-08-14 17:07:07 +0100

    / ‘deid/src/main/java/uk/co/terminological/pipestream/FileUtils.java’

--- deid/src/main/java/uk/co/terminological/pipestream/FileUtils.java
+++ deid/src/main/java/uk/co/terminological/pipestream/FileUtils.java
@@ -16,7 +16,6 @@ import java.nio.file.WatchService;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.BiConsumer;
-import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
