d8242a1376837d5c694dd498c8f44ed1c87a6879
Rob Challen <rob@terminological.co.uk>
2018-08-14 17:12:49 +0100

    < ‘deid/src/main/java/uk/co/terminological/pipestream/EventSerialiser.java’ > ‘deid/src/main/java/uk/co/terminological/pipestream/EventSerializer.java’

--- deid/src/main/java/uk/co/terminological/pipestream/EventSerializer.java
+++ deid/src/main/java/uk/co/terminological/pipestream/EventSerializer.java
@@ -2,15 +2,23 @@ package uk.co.terminological.pipestream;
 
 import java.io.Closeable;
 import java.io.IOException;
+import java.io.Serializable;
 import java.nio.file.Path;
 import java.util.function.BiConsumer;
 
-public abstract class EventSerialiser<X> implements BiConsumer<Path,Event<X>>, Closeable {
-	public EventSerialiser() {
+public abstract class EventSerializer<X> implements BiConsumer<Path,Event<X>>, Closeable {
+	public EventSerializer() {
 		if (leaveOpen()) EventBus.get().registerCloseable(this);
 	}
 	public abstract void ensureOpen(Path path);
 	public abstract boolean leaveOpen();
 	public abstract void close() throws IOException;
 	public abstract void accept(Path t, Event<X> u);
+	
+	
+	
+	public static class JavaSerialiser extends EventSerializer<Serializable> {
+		
+	}
+	
 }
