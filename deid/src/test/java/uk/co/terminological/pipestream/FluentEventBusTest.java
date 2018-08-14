08274bf3b8bb08a69ed1a4114d96196d65e5140e
Rob Challen <rob@terminological.co.uk>
2018-08-14 13:50:48 +0100

    / ‘deid/src/test/java/uk/co/terminological/pipestream/FluentEventBusTest.java’

--- deid/src/test/java/uk/co/terminological/pipestream/FluentEventBusTest.java
+++ deid/src/test/java/uk/co/terminological/pipestream/FluentEventBusTest.java
@@ -1,5 +1,35 @@
 package uk.co.terminological.pipestream;
 
-public class FluentEventBusTest {
+import org.apache.log4j.BasicConfigurator;
+import org.junit.Before;
+import org.junit.Test;
 
+import uk.co.terminological.pipestream.EventBusTest.TestGenerator;
+
+public class FluentEventBusTest {
+			EventBus bus;
+		
+		public static void main(String[] args) throws Exception {
+			EventBusTest tmp = new EventBusTest();
+			tmp.setUp();
+			tmp.test();
+		}
+		
+		@Before
+		public void setUp() throws Exception {
+
+			BasicConfigurator.configure();
+			bus = EventBus.get();
+					
+		}
+
+		@Test
+		public final void test() {
+			EventBus.get().logInfo("Starting");
+			TestGenerator stringLoader = new TestGenerator(); 
+			
+			while(stringLoader.execute());
+			
+			EventBus.get().logInfo("Finished");
+		}
 }
