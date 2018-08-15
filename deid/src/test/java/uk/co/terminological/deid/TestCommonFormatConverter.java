6084c35ca581b530704c8ba91c97f9704c0ba5f8
Rob Challen <rob@terminological.co.uk>
2018-08-15 18:49:48 +0100

    / ‘deid/src/test/java/uk/co/terminological/deid/TestCommonFormatConverter.java’

--- deid/src/test/java/uk/co/terminological/deid/TestCommonFormatConverter.java
+++ deid/src/test/java/uk/co/terminological/deid/TestCommonFormatConverter.java
@@ -7,7 +7,7 @@ public class TestCommonFormatConverter {
 
 	public static void main(String[] args) throws XmlException {
 		
-		Xml xml = Xml.fromStream(TestCommonFormatConverter.class.getResourceAsStream("deid_surrogate_test_all_groundtruth_version2.xml"));
+		Xml xml = Xml.fromStream(TestCommonFormatConverter.class.getResourceAsStream("/deid_surrogate_test_all_groundtruth_version2.xml"));
 		CommonFormat.Records rec = new CommonFormatConverter().fromI2B2_2006_Xml(xml);
 		rec.stream().findFirst().get().spans.forEach(System.out::println);
 
