d0e11e68feed3d33c291d559fc1ccafb85544f70
Rob Challen <rob@terminological.co.uk>
2018-08-15 09:41:03 +0100

    / ‘deid/src/main/java/uk/co/terminological/deid/I2B2Experiment.java’

--- deid/src/main/java/uk/co/terminological/deid/I2B2Experiment.java
+++ deid/src/main/java/uk/co/terminological/deid/I2B2Experiment.java
@@ -1,24 +1,19 @@
 package uk.co.terminological.deid;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import org.apache.commons.compress.archivers.ArchiveInputStream;
 import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
 import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
 import org.apache.log4j.BasicConfigurator;
 
-import com.sun.codemodel.internal.writer.FileCodeWriter;
-
-import uk.co.terminological.pipestream.Event;
 import uk.co.terminological.pipestream.FileUtils.DeferredInputStream;
+import uk.co.terminological.pipestream.FileUtils.DirectoryScanner;
+import uk.co.terminological.pipestream.FluentEvents.Generators;
+import uk.co.terminological.pipestream.FluentEvents.Handlers;
+import uk.co.terminological.pipestream.FluentEvents.Predicates;
 import uk.co.terminological.pipestream.Handlers.Adaptor;
-
-import static uk.co.terminological.pipestream.FluentEvents.*;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FilenameFilter;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.function.Function;
 
 
 public class I2B2Experiment {
@@ -28,16 +23,15 @@ public class I2B2Experiment {
 		
 		BasicConfigurator.configure();
 		
-		File file;
-		;
+		
 		
 	}
 	
 	
 	DirectoryScanner zipFinder(Path directory, String zipType) {
 		return Generators.directoryScanner(directory, 
-				file -> file.getAbsolutePath().endsWith(".xml")
-				, zipType, "ZIP_FILE_FOUND");
+				file -> file.getAbsolutePath().endsWith(".xml"), 
+				zipType, "ZIP_FILE_FOUND");
 	}
 	
 	Adaptor<Path,DeferredInputStream<ArchiveInputStream>> zipLoader(Path file, String zipType) {
@@ -53,8 +47,6 @@ public class I2B2Experiment {
 				name -> zipType,
 				type -> "ZIP_FILE_READY");
 	}
-						
-				
-			
 
+	
 }
