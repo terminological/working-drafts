dee1ab1e5ab3457a80cd34dab8cca8b917cbaaab
Rob Challen <rob@terminological.co.uk>
2018-10-23 21:28:02 +0100

    / ‘pubmedclient/src/main/java/uk/co/terminological/pubmedclient/CrossRefApiResponse.java’

--- pubmedclient/src/main/java/uk/co/terminological/pubmedclient/CrossRefApiResponse.java
+++ pubmedclient/src/main/java/uk/co/terminological/pubmedclient/CrossRefApiResponse.java
@@ -114,7 +114,7 @@ public class CrossRefApiResponse {
 		@JsonProperty("journal-issue") public JournalIssue journalIssue; //NOT IN SPEC
 		@JsonProperty("isbn-type") public List<ISSNWithType> isbnType; //NOT IN SPEC
 		@JsonProperty("publisher-location") public String publisherLocation; //NOT IN SPEC
-		@JsonAnySetter public Map<String,String> unknownProperties;
+		@JsonAnySetter //public Map<String,String> unknownProperties;
 	    public void handleUnknownProperty(String key, Object value) {
 	        System.out.printf("JSON property: %s: %s", key, value.toString());
 	    }
