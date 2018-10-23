94d3be1e86979690c22aa9dd82bdce9395595f1c
Rob Challen <rob@terminological.co.uk>
2018-10-23 20:13:40 +0100

    / ‘pubmedclient/src/test/java/uk/co/terminological/pubmedclient/TestJsonParsing.java’

--- pubmedclient/src/test/java/uk/co/terminological/pubmedclient/TestJsonParsing.java
+++ pubmedclient/src/test/java/uk/co/terminological/pubmedclient/TestJsonParsing.java
@@ -1,13 +1,21 @@
 package uk.co.terminological.pubmedclient;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class TestJsonParsing {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws JsonParseException, JsonMappingException, IOException {
 		// TODO Auto-generated method stub
 
 		ObjectMapper objectMapper = new ObjectMapper();
-		ClassLoader.getSystemResourceAsStream("ontologiesExample.json");
-		CrossRefApiResponse.Response = objectMapper.
+		InputStream is = ClassLoader.getSystemResourceAsStream("ontologiesExample.json");
+		CrossRefApiResponse.Response  response = objectMapper.readValue(is, CrossRefApiResponse.Response.class);
+		response.message.items.stream().forEach(item -> item.originalTitle.forEach(System.out::println));
 		
 	}
 
