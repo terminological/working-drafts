bda221b48a5870cee1032fbf4bc99ffc96e813d1
Rob Challen <rob@terminological.co.uk>
2018-10-24 11:31:25 +0100

    / ‘pubmedclient/pom.xml’ / ‘pubmedclient/src/main/java/uk/co/terminological/pubmedclient/CrossRefClient.java’

--- pubmedclient/src/main/java/uk/co/terminological/pubmedclient/CrossRefClient.java
+++ pubmedclient/src/main/java/uk/co/terminological/pubmedclient/CrossRefClient.java
@@ -1,9 +1,19 @@
 package uk.co.terminological.pubmedclient;
 
+import java.io.InputStream;
+
 public class CrossRefClient {
 	// https://www.crossref.org/schemas/
 	// https://github.com/CrossRef/rest-api-doc/blob/master/api_format.md
 	// https://github.com/CrossRef/rest-api-doc
 	// http://tdmsupport.crossref.org/researchers/
 	// http://clickthroughsupport.crossref.org/
+	
+	private String clickThroughToken;
+	
+	
+	public InputStream getTDM(CrossRefApiResponse.Work work) {
+		work.li
+	}
+	
 }
