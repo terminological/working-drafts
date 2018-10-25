64120317d8aff9bb830b0fa706c2de33e2f355b3
Rob Challen <rob@terminological.co.uk>
2018-10-25 16:06:09 +0100

    / ‘pubmedclient/src/test/java/uk/co/terminological/pubmedclient/TestConverterApiClient.java’

--- pubmedclient/src/test/java/uk/co/terminological/pubmedclient/TestConverterApiClient.java
+++ pubmedclient/src/test/java/uk/co/terminological/pubmedclient/TestConverterApiClient.java
@@ -1,10 +1,25 @@
 package uk.co.terminological.pubmedclient;
 
+import java.util.Arrays;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import uk.co.terminological.pubmedclient.ConverterApiClient.IdType;
+import uk.co.terminological.pubmedclient.CrossRefClient.CrossRefException;
+
 public class TestConverterApiClient {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	public static Logger logger = LoggerFactory.getLogger(TestPubMedRestClient.class);
+	public static String APP_ID = "test_client";
+	public static String DEVELOPER_EMAIL = "rob@terminological.co.uk";
+	
+	public static void main(String[] args) throws CrossRefException {
+			
+		ConverterApiClient client = new ConverterApiClient(APP_ID,DEVELOPER_EMAIL);
+		
+		client.getDoisFor(Arrays.asList("12964947"), IdType.PMID).forEach(System.out::println);
 	}
 
 }
