e8904958c00e5dd0d526ffced87b4158f62136c3
Rob Challen <rob@terminological.co.uk>
2018-10-26 12:41:33 +0100

    / ‘pubmedclient/src/main/resources/schema/getSchema.sh’

--- pubmedclient/src/main/resources/schema/getSchema.sh
+++ pubmedclient/src/main/resources/schema/getSchema.sh
@@ -2,3 +2,4 @@ wget https://eutils.ncbi.nlm.nih.gov/eutils/dtd/20060628/esearch.dtd
 wget https://dtd.nlm.nih.gov/ncbi/pubmed/out/pubmed_180101.dtd
 wget https://eutils.ncbi.nlm.nih.gov/eutils/dtd/20041029/esummary-v1.dtd
 wget http://dtd.nlm.nih.gov/ncbi/pmc/articleset/nlm-articleset-2.0.dtd
+wget https://eutils.ncbi.nlm.nih.gov/eutils/dtd/20101123/elink.dtd
