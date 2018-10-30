c10306dc7a936d3a97e4962ecad8caa8983308ac
Rob Challen <rob@terminological.co.uk>
2018-10-30 12:36:09 +0000

    / ‘pubmedclient/src/main/java/uk/co/terminological/pubmedclient/UnpaywallClient.java’

--- pubmedclient/src/main/java/uk/co/terminological/pubmedclient/UnpaywallClient.java
+++ pubmedclient/src/main/java/uk/co/terminological/pubmedclient/UnpaywallClient.java
@@ -10,7 +10,6 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import javax.ws.rs.core.MultivaluedMap;
 
