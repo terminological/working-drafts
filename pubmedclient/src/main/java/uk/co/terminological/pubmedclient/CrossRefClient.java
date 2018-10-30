2d9b69f0a712342ddeb3263c896b0b847cd73d52
Rob Challen <rob@terminological.co.uk>
2018-10-30 16:37:41 +0000

    / ‘pubmedclient/src/main/java/uk/co/terminological/pubmedclient/CrossRefClient.java’

--- pubmedclient/src/main/java/uk/co/terminological/pubmedclient/CrossRefClient.java
+++ pubmedclient/src/main/java/uk/co/terminological/pubmedclient/CrossRefClient.java
@@ -117,9 +117,9 @@ public class CrossRefClient {
 		String url = baseUrl+"works/"+encode(doi);
 		WebResource wr = client.resource(url).queryParams(defaultApiParams());
 		try {
-			ClientResponse r = wr.get(ClientResponse.class);
+			//ClientResponse r = wr.get(ClientResponse.class);
 			//updateRateLimits(r.getHeaders());
-			InputStream is = r.getEntityInputStream(); 
+			InputStream is = wr.get(InputStream.class); //r.getEntityInputStream(); 
 			CrossRefResult.SingleResult  response = objectMapper.readValue(is, CrossRefResult.SingleResult.class);
 			return response;
 		} catch (JsonParseException | JsonMappingException e) {
