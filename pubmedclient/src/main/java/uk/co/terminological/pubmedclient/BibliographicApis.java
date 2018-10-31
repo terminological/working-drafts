fe1fe6e62bcc01229f80abc9a2bc1f909c692fec
Rob Challen <rob@terminological.co.uk>
2018-10-31 09:24:19 +0000

    / ‘pubmedclient/src/main/java/uk/co/terminological/pubmedclient/BibliographicApis.java’

--- pubmedclient/src/main/java/uk/co/terminological/pubmedclient/BibliographicApis.java
+++ pubmedclient/src/main/java/uk/co/terminological/pubmedclient/BibliographicApis.java
@@ -5,7 +5,8 @@ public class BibliographicApis {
 	// https://elibrary.exeter.ac.uk/idp/shibboleth
 	// https://ssologin.exeter.ac.uk/distauth/UI/Login?realm=%2Fpeople
 	// TODO: don't think this will really work as shibbolteh login requires going to login links on different publishers sites.
-	
-	// https://unpaywall.org/products/api
 		
+	// TODO: integrate CSL: https://michel-kraemer.github.io/citeproc-java/api/1.0.1/de/undercouch/citeproc/csl/CSLItemDataBuilder.html
+	// TODO: Consider a google scholar scraper
+	
 }
