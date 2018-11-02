c5d600f53e4404918c72d7d623098df425017f84
Rob Challen <rob@terminological.co.uk>
2018-11-02 09:00:53 +0000

    + ‘literature-review/src/test/java/uk/co/terminological/literaturereview/TestGraphDatabaseApi.java’

---
+++ literature-review/src/test/java/uk/co/terminological/literaturereview/TestGraphDatabaseApi.java
@@ -0,0 +1,19 @@
+package uk.co.terminological.literaturereview;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestGraphDatabaseApi {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public final void testCreate() {
+		fail("Not yet implemented");
+	}
+
+}
