8c70e35a632d4ad497744d5211d03fd2661359f5
Rob Challen <rob@terminological.co.uk>
2018-11-02 20:32:05 +0000

    / ‘literature-review/src/main/java/uk/co/terminological/literaturereview/PubMed2Neo4jUtils.java’

--- literature-review/src/main/java/uk/co/terminological/literaturereview/PubMed2Neo4jUtils.java
+++ literature-review/src/main/java/uk/co/terminological/literaturereview/PubMed2Neo4jUtils.java
@@ -1,7 +1,10 @@
 package uk.co.terminological.literaturereview;
 
+import java.util.Optional;
+
 import org.neo4j.graphdb.Label;
 import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.RelationshipType;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.schema.Schema;
 
@@ -11,6 +14,13 @@ public class PubMed2Neo4jUtils {
 
 	
 	public static Label ARTICLE = Label.label("Article"); 
+	public static Label AUTHOR = Label.label("Author");
+	public static Label AFFILIATION = Label.label("Affiliation");
+	public static Label TOKEN = Label.label("Token");
+	
+	public enum Rel implements RelationshipType {
+	    KNOWS
+	}
 	
 	
 	public static void setupSchema(GraphDatabaseApi graph) {
@@ -26,18 +36,29 @@ public class PubMed2Neo4jUtils {
 		}
 	}
 	
-	public static void mapEntryToNode(PubMedEntry entry, GraphDatabaseApi graph) {
+	public static Optional<Long> mapEntryToNode(PubMedEntry entry, GraphDatabaseApi graph) {
+		
+		Long nodeId = null;
 		
 		try ( Transaction tx = graph.get().beginTx() )
 		{
-			Node node;
-			node = graph.get().findNode(ARTICLE, "pmid", entry.getPMID());
-			if (node == null) node = graph.get().createNode(ARTICLE);
+			Node tmp = graph.get().findNode(ARTICLE, "pmid", entry.getPMID());
+			if (tmp == null) {
+				tmp = graph.get().createNode(ARTICLE);
+				tmp.setProperty("pmid", entry.getPMID());
+			}
+			
+			Node node = tmp;
+			entry.getDoi().ifPresent(doi -> node.setProperty("doi", doi));
+			node.setProperty("abstract", entry.getAbstract());
 			
 			
-			
+			nodeId = node.getId();
 		    tx.success();
 		}
+		
+		return Optional.ofNullable(nodeId);
+		
 	}
 
 }
