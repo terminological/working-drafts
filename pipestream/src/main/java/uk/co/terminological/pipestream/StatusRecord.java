2c735861fd3b6037b7cd41fee05960b1fe64ab18
Rob Challen <rob@terminological.co.uk>
2018-11-02 15:28:18 +0000

    / ‘pipestream/src/main/java/uk/co/terminological/pipestream/StatusRecord.java’

--- pipestream/src/main/java/uk/co/terminological/pipestream/StatusRecord.java
+++ pipestream/src/main/java/uk/co/terminological/pipestream/StatusRecord.java
@@ -1,5 +1,11 @@
 package uk.co.terminological.pipestream;
 
-public class StatusRecord {
+import uk.co.terminological.datatypes.EavMap;
 
+public class StatusRecord<STATUSES> extends EavMap<String,STATUSES,Boolean> {
+
+	public Boolean has(STATUSES status, String key) {
+		Boolean tmp = get(key,status);
+		return tmp != null ? tmp : Boolean.FALSE;
+	}
 }
