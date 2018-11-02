ea9add88bd43dcc1193be71ce36e266316a9afe8
Rob Challen <rob@terminological.co.uk>
2018-11-02 16:42:02 +0000

    / ‘literature-review/src/main/java/uk/co/terminological/literaturereview/PubMed2Neo4jUtils.java’

--- literature-review/src/main/java/uk/co/terminological/literaturereview/PubMed2Neo4jUtils.java
+++ literature-review/src/main/java/uk/co/terminological/literaturereview/PubMed2Neo4jUtils.java
@@ -3,7 +3,6 @@ package uk.co.terminological.literaturereview;
 import org.neo4j.graphdb.Label;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Transaction;
-import org.neo4j.graphdb.schema.IndexDefinition;
 import org.neo4j.graphdb.schema.Schema;
 
 import uk.co.terminological.pubmedclient.EntrezResult.PubMedEntry;
@@ -14,27 +13,29 @@ public class PubMed2Neo4jUtils {
 	public static Label ARTICLE = Label.label("Article"); 
 	
 	
-	IndexDefinition pmidIndex;
-	IndexDefinition doiIndex;
-	
-	public void setupSchema(GraphDatabaseApi graph) {
+	public static void setupSchema(GraphDatabaseApi graph) {
 		
 		try ( Transaction tx = graph.get().beginTx() )
 		{
 		    Schema schema = graph.get().schema();
-		    pmidIndex = schema.indexFor( ARTICLE ).on( "pmid" ).create();
-		    doiIndex = schema.indexFor( ARTICLE ).on( "doi" ).create();
+		    schema.indexFor( ARTICLE ).on( "pmid" ).create();
+		    schema.indexFor( ARTICLE ).on( "doi" ).create();
+		    schema.constraintFor(ARTICLE).assertPropertyIsUnique("pmid");
+		    schema.constraintFor(ARTICLE).assertPropertyIsUnique("doi");
 		    tx.success();
 		}
 	}
 	
-	public void mapEntryToNode(PubMedEntry entry, GraphDatabaseApi graph) {
+	public static void mapEntryToNode(PubMedEntry entry, GraphDatabaseApi graph) {
 		
 		try ( Transaction tx = graph.get().beginTx() )
 		{
-		    // Database operations go here
-			graph.get().findNode(ARTICLE, "pmid", entry.getPMID())
-			Node node = graph.get().createNode(ARTICLE);
+			Node node;
+			node = graph.get().findNode(ARTICLE, "pmid", entry.getPMID());
+			if (node == null) node = graph.get().createNode(ARTICLE);
+			
+			
+			
 		    tx.success();
 		}
 	}
