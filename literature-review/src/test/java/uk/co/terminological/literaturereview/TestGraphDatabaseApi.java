0918e970935d9b66026ce4211bb63438cf64db67
Rob Challen <rob@terminological.co.uk>
2018-11-02 09:11:49 +0000

    / ‘literature-review/src/test/java/uk/co/terminological/literaturereview/TestGraphDatabaseApi.java’

--- literature-review/src/test/java/uk/co/terminological/literaturereview/TestGraphDatabaseApi.java
+++ literature-review/src/test/java/uk/co/terminological/literaturereview/TestGraphDatabaseApi.java
@@ -1,16 +1,18 @@
 package uk.co.terminological.literaturereview;
 
-import static org.junit.Assert.*;
-
 import java.io.File;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.common.io.Files;
 
 public class TestGraphDatabaseApi {
 
+	private static final Logger logger = LoggerFactory.getLogger(TestGraphDatabaseApi.class);
+	
 	@Before
 	public void setUp() throws Exception {
 	}
@@ -19,8 +21,11 @@ public class TestGraphDatabaseApi {
 	public final void testCreate() {
 		
 		File tmp = Files.createTempDir();
+		logger.info("Creating db in "+tmp);
 		GraphDatabaseApi api = GraphDatabaseApi.create(tmp);
+		logger.info("graph Db available in "+tmp);
 		api.shutdown();
+		logger.info("graph Db shutdown");
 		tmp.deleteOnExit();
 	}
 
