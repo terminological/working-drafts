3b0543b16012e0c2849bb3b2047bd7614348ba6f
Rob Challen <rob@terminological.co.uk>
2018-11-02 09:09:51 +0000

    / ‘literature-review/src/main/java/uk/co/terminological/literaturereview/GraphDatabaseApi.java’

--- literature-review/src/main/java/uk/co/terminological/literaturereview/GraphDatabaseApi.java
+++ literature-review/src/main/java/uk/co/terminological/literaturereview/GraphDatabaseApi.java
@@ -8,68 +8,73 @@ import org.neo4j.kernel.configuration.BoltConnector;
 import org.neo4j.kernel.configuration.Config;
 
 public class GraphDatabaseApi {
-	
+
 	private static GraphDatabaseApi singleton;
 	private GraphDatabaseService graphDb;
-	
+
 	public static void main(String[] args) {
 		File f = new File(args[0]);
 		GraphDatabaseApi.create(f).waitAndShutdown();
 	}
-	
+
 	public GraphDatabaseApi(File graphDbPath) {
-		
+
 		//http://neo4j-contrib.github.io/neo4j-jdbc/
 		Config config = Config.builder()
 				.withServerDefaults()
 				.build();
-		
+
 		BoltConnector bolt = config.boltConnectors().get(0);
 
 		graphDb = new GraphDatabaseFactory()
-		        .newEmbeddedDatabaseBuilder( graphDbPath )
-		        .setConfig( bolt.type, "BOLT" )
-		        .setConfig( bolt.enabled, "true" )
-		        .setConfig( bolt.listen_address, "localhost:7687" )
-		        .newGraphDatabase();
-		
+				.newEmbeddedDatabaseBuilder( graphDbPath )
+				.setConfig( bolt.type, "BOLT" )
+				.setConfig( bolt.enabled, "true" )
+				.setConfig( bolt.listen_address, "localhost:7687" )
+				.newGraphDatabase();
+
 		Runtime.getRuntime().addShutdownHook( new Thread()
-	    {
-	        @Override
-	        public void run()
-	        {
-	        	waitAndShutdown();
-	        }
-	    } );
+		{
+			@Override
+			public void run()
+			{
+				waitAndShutdown();
+			}
+		} );
 	}
 
 	public void shutdown() {
-		graphDb.shutdown();
+		if (graphDb != null && graphDb.isAvailable(1)) {
+			graphDb.shutdown();
+			graphDb = null;
+		}
 	}
-	
+
 	public void waitAndShutdown() {
-		System.out.println("Press Enter key to shutdown database...");
-        try
-        {
-            System.in.read();
-        }  
-        catch(Exception e)
-        {}  
-    	graphDb.shutdown();
+		if (graphDb != null && graphDb.isAvailable(1)) {
+			System.out.println("Press Enter key to shutdown database...");
+			try
+			{
+				System.in.read();
+			}  
+			catch(Exception e)
+			{}  
+			graphDb.shutdown();
+		}
 	}
-	
+
 	public GraphDatabaseService get() {return graphDb;}
-	
+
 	public static GraphDatabaseApi create(File graphDbPath) {
 		if (singleton == null) singleton = new GraphDatabaseApi(graphDbPath);
 		return singleton;
 	}
-	
+
 	public static GraphDatabaseService db() {
 		if (singleton == null) throw new NotInitialisedException();
 		return singleton.get();
- 	}
-	
-	
+	}
+
+
 	public static class NotInitialisedException extends RuntimeException {}
 }
