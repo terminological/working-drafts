ec255aed38c9227a19620ab8a2ab1b0fca520180
Robert Challen <rob@terminological.co.uk>
2018-11-12 09:31:01 +0000

    / ‘literature-review/src/main/java/uk/co/terminological/literaturereview/PubMedGraphUtils.java’

--- literature-review/src/main/java/uk/co/terminological/literaturereview/PubMedGraphUtils.java
+++ literature-review/src/main/java/uk/co/terminological/literaturereview/PubMedGraphUtils.java
@@ -37,7 +37,7 @@ import uk.co.terminological.pubmedclient.EntrezResult.PubMedEntries;
 public class PubMedGraphUtils {
 
 	private static final Logger logger = LoggerFactory.getLogger(PubMedGraphUtils.class);
-	
+
 	public static Node doMerge(Label label, String indexName, Object indexValue, GraphDatabaseService graphDb) {
 		return doMerge(label,indexName,indexValue,graphDb, null);
 	}
@@ -50,29 +50,33 @@ public class PubMedGraphUtils {
 		Node result = resultIterator.next();
 		return result;
 	}
-        
-	
+
+
 	public static Optional<Node> mapDoiToNode(String doi, GraphDatabaseApi graph, Label... additionalLabels) {
 		Node out = null;
-		
+
 		try ( Transaction tx = graph.get().beginTx() ) {
+			tx.acquireWriteLock(lockNode);
 			Node tmp = doMerge(ARTICLE, "doi", doi, graph.get());
 			tmp.addLabel(DOI_STUB);
 			tx.success();
 			out = tmp;
+
 		}
 		return Optional.ofNullable(out);
 	}
-	
-	
+
+
 	public static Optional<Node> mapPMIDToNode(String pmid, GraphDatabaseApi graph, Label... additionalLabels) {
 		Node out = null;
-		
+
 		try ( Transaction tx = graph.get().beginTx() ) {
+			tx.acquireWriteLock(lockNode);
 			Node tmp = doMerge(ARTICLE, "pmid", pmid,graph.get());
 			tmp.addLabel(PMID_STUB);
 			tx.success();
 			out = tmp;
+			tx.success();
 		}
 		return Optional.ofNullable(out);
 	}
@@ -82,14 +86,22 @@ public class PubMedGraphUtils {
 
 		List<Node> out = new ArrayList<>();
 
-		try ( Transaction tx = graph.get().beginTx() ) {
-			tx.acquireWriteLock(lockNode);
-			entries.stream().forEach(entry -> {
-				Node tmp = null;
-				Node tmp1 = entry.getPMID().isPresent() ? doMerge(ARTICLE, "pmid", entry.getPMID().get(), graph.get()) : null;
-				Node tmp2 = entry.getDoi().isPresent() ? doMerge(ARTICLE, "doi", entry.getDoi().get(), graph.get()) : null;
-				if (tmp1 != null && tmp2 != null && tmp1.getId() != tmp2.getId()) {
-					//merge tmp1 and tmp2
+
+		entries.stream().forEach(entry -> {
+			Node tmp = null;
+			Node tmp1 = null;
+			Node tmp2 = null;
+			try ( Transaction tx = graph.get().beginTx() ) {
+				tx.acquireWriteLock(lockNode);
+				tmp = null;
+				tmp1 = entry.getPMID().isPresent() ? doMerge(ARTICLE, "pmid", entry.getPMID().get(), graph.get()) : null;
+				tmp2 = entry.getDoi().isPresent() ? doMerge(ARTICLE, "doi", entry.getDoi().get(), graph.get()) : null;
+				tx.success();
+			}
+			if (tmp1 != null && tmp2 != null && tmp1.getId() != tmp2.getId()) {
+				//merge tmp1 and tmp2
+				try ( Transaction tx = graph.get().beginTx() ) {
+					tx.acquireWriteLock(lockNode);
 					tmp2.getAllProperties().forEach((k,v) -> tmp1.setProperty(k, v));
 					tmp2.getRelationships(Direction.INCOMING).forEach(r -> {
 						Node other = r.getOtherNode(tmp2);
@@ -105,41 +117,66 @@ public class PubMedGraphUtils {
 					});
 					tmp2.getLabels().forEach(l -> tmp1.addLabel(l));
 					tmp2.delete();
-					tmp = tmp1;
-				} else if (tmp1!=null) {
-					tmp = tmp1;
-				} else if (tmp2!=null) {
-					tmp = tmp2;
-				} else {
+					tx.success();
+				}
+				tmp = tmp1;
+			} else if (tmp1!=null) {
+				tmp = tmp1;
+			} else if (tmp2!=null) {
+				tmp = tmp2;
+			} else {
+				try ( Transaction tx = graph.get().beginTx() ) {
+					tx.acquireWriteLock(lockNode);
+
 					Node newNode = graph.get().createNode(ARTICLE);
 					entry.getPMID().ifPresent(pmid -> newNode.setProperty("pmid", pmid));
 					entry.getDoi().ifPresent(doi -> newNode.setProperty("doi", doi));
 					tmp = newNode;
+					tx.success();
 				}
-				
-				Node node = tmp;
+			}
+
+			Node node = tmp;
+			try ( Transaction tx = graph.get().beginTx() ) {
+				tx.acquireWriteLock(lockNode);
+
 				Arrays.stream(additionalLabels).forEach(label -> node.addLabel(label));
 				entry.getPMCID().ifPresent(pmc -> node.setProperty("pmcid", pmc));
 				//TODO:: DEadlock here
 				node.setProperty("abstract", entry.getAbstract());
 				node.setProperty("title", entry.getTitle());
-				Integer depth = null;
+				tx.success();
+			}
+			Integer depth = null;
+			try ( Transaction tx = graph.get().beginTx() ) {
+				tx.acquireWriteLock(lockNode);
+
 				for (Relationship r: node.getRelationships(Direction.INCOMING, HAS_REFERENCE, HAS_RELATED)) {
 					Node other = r.getOtherNode(node);
 					Integer tmpDepth = (Integer) other.getProperty("depth", null);
 					if (depth == null) depth=tmpDepth;
 					else if (tmpDepth!=null && tmpDepth<depth) depth=tmpDepth;
 				};
-				if (depth==null) depth = 0;
+				tx.success();
+			}
+			if (depth==null) depth = 0;
+			try ( Transaction tx = graph.get().beginTx() ) {
+				tx.acquireWriteLock(lockNode);
 				node.setProperty("depth", depth);
+				node.removeLabel(DOI_STUB);
+				node.removeLabel(PMID_STUB);
+				entry.getPubMedDate().ifPresent(dt -> node.setProperty("date", dt));
 				if (depth<maxDepth) {
 					node.addLabel(EXPAND);
 				} else {
 					logger.debug("not expanding at depth "+depth+": "+entry.getTitle());
 				}
-				entry.getPubMedDate().ifPresent(dt -> node.setProperty("date", dt));
-				node.removeLabel(DOI_STUB);
-				node.removeLabel(PMID_STUB);
+				tx.success();
+			}
+
+			try ( Transaction tx = graph.get().beginTx() ) {
+				tx.acquireWriteLock(lockNode);
+
 				entry.getAuthors().forEach(au -> {
 					Optional<Node> targetNode = mapAuthorToNode(au,graph);
 					targetNode.ifPresent(target -> node.createRelationshipTo(target, HAS_AUTHOR));
@@ -148,95 +185,95 @@ public class PubMedGraphUtils {
 					Optional<Node> targetNode = mapMeshCodeToNode(mh.getDescriptor(),graph);
 					targetNode.ifPresent(target -> node.createRelationshipTo(target, HAS_MESH));
 				});
-				out.add(node);
-			});
-			tx.success();
-
-		}
-
-		return out;
-
-	}
-
-	public static Optional<Node> mapAuthorToNode(Author author, GraphDatabaseApi graph) {
-		
-		Node out = null;
-		
-		try ( Transaction tx = graph.get().beginTx() ) {
-			tx.acquireWriteLock(lockNode);
-			
-			Node node = doMerge(AUTHOR, "identifier", author.getIdentifier(), graph.get());
-			author.firstName().ifPresent(fn -> node.setProperty("firstName", fn));
-			author.lastName().ifPresent(fn -> node.setProperty("lastName", fn));
-			author.initials().ifPresent(fn -> node.setProperty("initials", fn));
-			String[] affiliations = author.affiliations().collect(Collectors.toList()).toArray(new String[] {});
-			if (affiliations.length > 0) node.setProperty("affiliations", affiliations);
-			
-			out = node;
-		    tx.success();
-		    
-		}
-		
-		return Optional.ofNullable(out);
-		
-	}
-	
-	public static Optional<Node> mapMeshCodeToNode(MeshCode meshCode, GraphDatabaseApi graph) {
-		Node out = null;
-		
-		try ( Transaction tx = graph.get().beginTx() ) {
-			
-			Node tmp = graph.get().findNode(MESH_CODE, "code", meshCode.getCode());
-			if (tmp == null) {
-				tmp = graph.get().createNode(MESH_CODE);
-				tmp.setProperty("code", meshCode.getCode());
-				tmp.setProperty("term", meshCode.getTerm());
+				tx.success();
 			}
-			out = tmp;
-		    tx.success();
-		    
-		}
-		
-		return Optional.ofNullable(out);
+			out.add(node);
+		});
+
+
+	return out;
+
+}
+
+public static Optional<Node> mapAuthorToNode(Author author, GraphDatabaseApi graph) {
+
+	Node out = null;
+
+	try ( Transaction tx = graph.get().beginTx() ) {
+		tx.acquireWriteLock(lockNode);
+
+		Node node = doMerge(AUTHOR, "identifier", author.getIdentifier(), graph.get());
+		author.firstName().ifPresent(fn -> node.setProperty("firstName", fn));
+		author.lastName().ifPresent(fn -> node.setProperty("lastName", fn));
+		author.initials().ifPresent(fn -> node.setProperty("initials", fn));
+		String[] affiliations = author.affiliations().collect(Collectors.toList()).toArray(new String[] {});
+		if (affiliations.length > 0) node.setProperty("affiliations", affiliations);
+
+		out = node;
+		tx.success();
+
 	}
-	
-	public static List<Relationship> mapHasReferences(String citingDoi, List<String> citedDois, GraphDatabaseApi graph) {
-		List<Relationship> out = new ArrayList<>();
-		
-		try (Transaction tx = graph.get().beginTx()) {
-			tx.acquireWriteLock(lockNode);
-			Node start = doMerge(ARTICLE, "doi", citingDoi, graph.get(), DOI_STUB);
-			citedDois.forEach(citedDoi -> {
-				Node end = doMerge(ARTICLE, "doi", citedDoi,graph.get(), DOI_STUB);
-				out.add(start.createRelationshipTo(end, HAS_REFERENCE));
-			});
-			tx.success();
+
+	return Optional.ofNullable(out);
+
+}
+
+public static Optional<Node> mapMeshCodeToNode(MeshCode meshCode, GraphDatabaseApi graph) {
+	Node out = null;
+
+	try ( Transaction tx = graph.get().beginTx() ) {
+
+		Node tmp = graph.get().findNode(MESH_CODE, "code", meshCode.getCode());
+		if (tmp == null) {
+			tmp = graph.get().createNode(MESH_CODE);
+			tmp.setProperty("code", meshCode.getCode());
+			tmp.setProperty("term", meshCode.getTerm());
 		}
-		
-		return out;
+		out = tmp;
+		tx.success();
+
 	}
-	
-	public static Optional<Relationship> mapHasRelated(String sourcePMID, String targetPMID, Long relatedness, GraphDatabaseApi graph) {
-		Relationship out = null;
-		Node start;
-		Node end;
-		try (Transaction tx = graph.get().beginTx()) {
-			tx.acquireWriteLock(lockNode);
-			//TODO:: DEadlock here
-			start = doMerge(ARTICLE, "pmid", sourcePMID,graph.get(), PMID_STUB);
-			tx.success();
-		}
-		try (Transaction tx = graph.get().beginTx()) {
-			end = doMerge(ARTICLE, "pmid", targetPMID, graph.get(), PMID_STUB);
-			tx.success();
-		}
-		try (Transaction tx = graph.get().beginTx()) {
-			out = start.createRelationshipTo(end, HAS_RELATED);
-			out.setProperty("relatedness", relatedness);
-			tx.success();
-		}
-		
-		return Optional.ofNullable(out);
+
+	return Optional.ofNullable(out);
+}
+
+public static List<Relationship> mapHasReferences(String citingDoi, List<String> citedDois, GraphDatabaseApi graph) {
+	List<Relationship> out = new ArrayList<>();
+
+	try (Transaction tx = graph.get().beginTx()) {
+		tx.acquireWriteLock(lockNode);
+		Node start = doMerge(ARTICLE, "doi", citingDoi, graph.get(), DOI_STUB);
+		citedDois.forEach(citedDoi -> {
+			Node end = doMerge(ARTICLE, "doi", citedDoi,graph.get(), DOI_STUB);
+			out.add(start.createRelationshipTo(end, HAS_REFERENCE));
+		});
+		tx.success();
 	}
-	
+
+	return out;
+}
+
+public static Optional<Relationship> mapHasRelated(String sourcePMID, String targetPMID, Long relatedness, GraphDatabaseApi graph) {
+	Relationship out = null;
+	Node start;
+	Node end;
+	try (Transaction tx = graph.get().beginTx()) {
+		tx.acquireWriteLock(lockNode);
+		//TODO:: DEadlock here
+		start = doMerge(ARTICLE, "pmid", sourcePMID,graph.get(), PMID_STUB);
+		tx.success();
+	}
+	try (Transaction tx = graph.get().beginTx()) {
+		end = doMerge(ARTICLE, "pmid", targetPMID, graph.get(), PMID_STUB);
+		tx.success();
+	}
+	try (Transaction tx = graph.get().beginTx()) {
+		out = start.createRelationshipTo(end, HAS_RELATED);
+		out.setProperty("relatedness", relatedness);
+		tx.success();
+	}
+
+	return Optional.ofNullable(out);
+}
+
 }
