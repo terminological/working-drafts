be7ca19de1decbb42c3f7b767d5c7bfe9ea4e6f8
Rob Challen <rob@terminological.co.uk>
2018-11-23 13:25:40 +0000

    / ‘pubmedclient/src/test/java/uk/co/terminological/pubmedclient/TestPdfUtil.java’

--- pubmedclient/src/test/java/uk/co/terminological/pubmedclient/TestPdfUtil.java
+++ pubmedclient/src/test/java/uk/co/terminological/pubmedclient/TestPdfUtil.java
@@ -1,9 +1,31 @@
 package uk.co.terminological.pubmedclient;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.apache.log4j.BasicConfigurator;
+
 public class TestPdfUtil {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static String[] urls = {
+			"https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6227809/pdf/rsfs20180033.pdf",
+			"https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6227809/pdf",
+			"http://arxiv.org/pdf/1811.08430"
+	};
+	
+	public static void main(String[] args) throws IOException {
+
+		BasicConfigurator.configure();
+		Path tmp = Files.createTempDirectory("pdf_test");
+		System.out.println(tmp.toString());
+		
+		int i=1;
+		for (String url: urls) {
+			Files.copy(
+					PdfUtil.getPdfFromUrl(url, 10, true), 
+					tmp.resolve((i++)+".pdf"));
+		}
 
 	}
 
