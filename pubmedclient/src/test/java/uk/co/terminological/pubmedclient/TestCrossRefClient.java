daade7bbebb6788eafb185d85b26ceefe71669c2
Rob Challen <rob@terminological.co.uk>
2018-11-23 15:52:18 +0000

    / ‘pubmedclient/src/test/java/uk/co/terminological/pubmedclient/TestCrossRefClient.java’

--- pubmedclient/src/test/java/uk/co/terminological/pubmedclient/TestCrossRefClient.java
+++ pubmedclient/src/test/java/uk/co/terminological/pubmedclient/TestCrossRefClient.java
@@ -17,7 +17,8 @@ public class TestCrossRefClient {
 	static String[] articles =  {
 		"Collier,N., Nobata,C. and Tsujii,J. (2000) Extracting the names of genes and gene products with a hidden Markov model. In Proceedings of the 18th International Conference on Computational Lingustics (COLING'2003), Saarbrucken, Germany, pp. 201-207.",
 		"Kulick,S., Liberman,M., Palmer,M. and Schein,A. (2003) Shallow semantic annotations of biomedical corpora for information extraction. In Proceedings of the Third Meeting of the Special Interest Group on Text Mining at ISMB 2003.",
-		"Lafferty,J., McCallum,A. and Pereira,F. (2001) Conditional Random Fields: probabilistic models for segmenting and labeling sequence data. In Proceedings of ICML-01, pp. 282-289."
+		"Lafferty,J., McCallum,A. and Pereira,F. (2001) Conditional Random Fields: probabilistic models for segmenting and labeling sequence data. In Proceedings of ICML-01, pp. 282-289.",
+		"41. DAWSON, A. E., AUSTIN, R. E. and WENBER, D. S., American Journal of Clinical Pathology, 95 (1991), 29."
 	};
 	
 	
