b7c1370cc325e2b469f20e563bc992ab72dacbcf
Rob Challen <rob@terminological.co.uk>
2018-11-23 11:23:09 +0000

    / ‘pubmedclient/src/main/java/uk/co/terminological/pubmedclient/CrossRefClient.java’ / ‘pubmedclient/src/test/java/uk/co/terminological/pubmedclient/TestCrossRefLient.java’

--- pubmedclient/src/test/java/uk/co/terminological/pubmedclient/TestCrossRefLient.java
+++ pubmedclient/src/test/java/uk/co/terminological/pubmedclient/TestCrossRefLient.java
@@ -2,8 +2,24 @@ package uk.co.terminological.pubmedclient;
 
 public class TestCrossRefLient {
 
+	static String[] articles =  {
+		"Collier,N., Nobata,C. and Tsujii,J. (2000) Extracting the names of genes and gene products with a hidden Markov model. In Proceedings of the 18th International Conference on Computational Lingustics (COLING'2003), Saarbrucken, Germany, pp. 201-207.",
+		"Kulick,S., Liberman,M., Palmer,M. and Schein,A. (2003) Shallow semantic annotations of biomedical corpora for information extraction. In Proceedings of the Third Meeting of the Special Interest Group on Text Mining at ISMB 2003.",
+		"Lafferty,J., McCallum,A. and Pereira,F. (2001) Conditional Random Fields: probabilistic models for segmenting and labeling sequence data. In Proceedings of ICML-01, pp. 282-289."
+	};
+	
+	
+	static String DEVELOPER = "test@example.org";
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		CrossRefClient xref = CrossRefClient.create(DEVELOPER);
+		for (String ref: articles) {
+			System.out.println(ref);
+			xref.findWorkByCitationString(ref).get().title.forEach(System.out::println);
+			System.out.println();
+		}
+			
 
 	}
 
