6378682ec4777af54a3719a218bb49a435dab856
Rob Challen <rob@terminological.co.uk>
2018-11-23 13:31:43 +0000

    / ‘pubmedclient/src/test/java/uk/co/terminological/pubmedclient/TestPdfUtil.java’

--- pubmedclient/src/test/java/uk/co/terminological/pubmedclient/TestPdfUtil.java
+++ pubmedclient/src/test/java/uk/co/terminological/pubmedclient/TestPdfUtil.java
@@ -9,8 +9,8 @@ import org.apache.log4j.BasicConfigurator;
 public class TestPdfUtil {
 
 	public static String[] urls = {
-			"https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6227809/pdf/rsfs20180033.pdf",
-			"https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6227809/pdf",
+			// "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6227809/pdf/rsfs20180033.pdf",
+			// "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6227809/pdf",
 			"http://arxiv.org/pdf/1811.08430"
 	};
 	
