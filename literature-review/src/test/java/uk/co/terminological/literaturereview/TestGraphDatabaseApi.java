9daf0c5c0c55ba2647d50bfed42b3d6d05ea7e01
Rob Challen <rob@terminological.co.uk>
2018-11-26 12:02:53 +0000

    / ‘literature-review/src/test/java/uk/co/terminological/literaturereview/TestGraphDatabaseApi.java’

--- literature-review/src/test/java/uk/co/terminological/literaturereview/TestGraphDatabaseApi.java
+++ literature-review/src/test/java/uk/co/terminological/literaturereview/TestGraphDatabaseApi.java
@@ -3,18 +3,46 @@ package uk.co.terminological.literaturereview;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
 
 import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
 import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import pl.edu.icm.cermine.exception.AnalysisException;
+import uk.co.terminological.pubmedclient.BibliographicApiException;
+import uk.co.terminological.pubmedclient.BibliographicApis;
 
 
 
 public class TestGraphDatabaseApi {
 
 	private static final Logger logger = LoggerFactory.getLogger(TestGraphDatabaseApi.class);
+	
+	public static void main(String args[]) throws IOException, BibliographicApiException, AnalysisException {
+		BasicConfigurator.configure();
+		org.apache.log4j.Logger.getRootLogger().setLevel(Level.INFO);
+
+		String propFilename = args.length ==1? args[0]: "~/Dropbox/litReview/project.prop";
+		Path propPath= Paths.get(propFilename.replace("~", System.getProperty("user.home")));
+		Properties prop = System.getProperties();
+		prop.load(Files.newInputStream(propPath));
+
+		prop.forEach((k,v) -> prop.put(k, v.toString().replace("~", System.getProperty("user.home")))); 
+		Path graphDbPath = fromProperty(prop,"graph-db-directory");
+		Path graphConfPath = fromProperty(prop,"graph-conf-file");
+		
+		GraphDatabaseApi graphApi = GraphDatabaseApi.create(graphDbPath, graphConfPath);
+		graphApi.waitAndShutdown();
+	}
+	
+	static Path fromProperty(Properties prop, String name) {
+		return Paths.get(prop.getProperty(name).replace("~", System.getProperty("user.home")));
+	}
 	
 	@Before
 	public void setUp() throws Exception {
