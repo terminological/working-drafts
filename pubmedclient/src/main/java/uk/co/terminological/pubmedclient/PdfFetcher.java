31797471d53b5a2a05e15a5a7b996efa8ebcd59a
Robert Challen <rob@terminological.co.uk>
2018-11-28 10:06:20 +0000

    / ‘pubmedclient/src/main/java/uk/co/terminological/pubmedclient/PdfFetcher.java’

--- pubmedclient/src/main/java/uk/co/terminological/pubmedclient/PdfFetcher.java
+++ pubmedclient/src/main/java/uk/co/terminological/pubmedclient/PdfFetcher.java
@@ -1,6 +1,10 @@
 package uk.co.terminological.pubmedclient;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.function.Function;
 
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
@@ -24,11 +28,57 @@ public class PdfFetcher {
 
 	static Logger logger = LoggerFactory.getLogger(PdfFetcher.class);
 	
-	public static InputStream getPdfFromUrl(String url) {
-		return getPdfFromUrl(url, 10, false);
+	int maxRedirects = 10;
+	boolean debug = false;
+	Path cache = null;
+	
+	private PdfFetcher() {}
+	
+	public static PdfFetcher create() {
+		return new PdfFetcher();
 	}
 	
-	public static InputStream getPdfFromUrl(String url, int maxRedirects, boolean debug) {
+	public PdfFetcher debugMode() {
+		this.debug = true;
+		return this;
+	}
+	
+	public PdfFetcher withCache(Path cache) {
+		this.cache = cache;
+		return this;
+	}
+	
+	public PdfFetcher maxRedirects(int redirects) {
+		this.maxRedirects = redirects;
+		return this;
+	}
+	
+	public InputStream getPdfFromUrl(String url, Function<Path,Path> resolve) {
+		Path tmp = resolve.apply(cache);
+		if (Files.isDirectory(tmp)) {
+			logger.error("tried to cache pdf as a directory");
+			return getPdfFromUrl(url);
+		}
+		try {
+			if (!Files.exists(tmp)) {
+				Files.createDirectories(tmp.getParent());
+				InputStream is = getPdfFromUrl(url);
+				Files.copy(is, tmp);
+			}
+			return Files.newInputStream(tmp);
+		} catch (IOException e) {
+			logger.error("failed to cache pdf: "+e.getMessage());
+			return getPdfFromUrl(url);
+		}
+	}
+
+
+	
+	public InputStream getPdfFromUrl(String url, String filename) {
+		return getPdfFromUrl(url, p -> p.resolve(filename));
+	}
+	
+	public InputStream getPdfFromUrl(String url) {
 		ClientConfig config = new DefaultClientConfig();
 	    config.getProperties().put(ClientConfig.PROPERTY_FOLLOW_REDIRECTS, true);
 	    Client client = Client.create(config);
