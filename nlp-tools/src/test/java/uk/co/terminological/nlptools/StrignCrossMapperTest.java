7a2ccd0583765735b3de9bf1770abb95b0ccdca7
Rob Challen <rob@terminological.co.uk>
2018-11-30 22:53:49 +0000

    + ‘nlp-tools/src/test/java/uk/co/terminological/nlptools/StrignCrossMapperTest.java’

---
+++ nlp-tools/src/test/java/uk/co/terminological/nlptools/StrignCrossMapperTest.java
@@ -0,0 +1,11 @@
+package uk.co.terminological.nlptools;
+
+import junit.framework.TestCase;
+
+public class StrignCrossMapperTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+}
