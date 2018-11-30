0b00361bbdb310ea98323ed9accce7868456977f
Rob Challen <rob@terminological.co.uk>
2018-11-30 22:54:02 +0000

    < ‘nlp-tools/src/test/java/uk/co/terminological/nlptools/StrignCrossMapperTest.java’ > ‘nlp-tools/src/test/java/uk/co/terminological/nlptools/StringCrossMapperTest.java’

--- nlp-tools/src/test/java/uk/co/terminological/nlptools/StringCrossMapperTest.java
+++ nlp-tools/src/test/java/uk/co/terminological/nlptools/StringCrossMapperTest.java
@@ -2,7 +2,7 @@ package uk.co.terminological.nlptools;
 
 import junit.framework.TestCase;
 
-public class StrignCrossMapperTest extends TestCase {
+public class StringCrossMapperTest extends TestCase {
 
 	protected void setUp() throws Exception {
 		super.setUp();
