a69ac440fe877392564d5f334779bce16c0c6823
Rob Challen <rob@terminological.co.uk>
2018-11-30 22:59:22 +0000

    / ‘nlp-tools/src/test/java/uk/co/terminological/nlptools/StringCrossMapperTest.java’

--- nlp-tools/src/test/java/uk/co/terminological/nlptools/StringCrossMapperTest.java
+++ nlp-tools/src/test/java/uk/co/terminological/nlptools/StringCrossMapperTest.java
@@ -4,8 +4,25 @@ import junit.framework.TestCase;
 
 public class StringCrossMapperTest extends TestCase {
 
+	static final String[] strings = {
+			"one two three four",
+			"one two three",
+			"one two three four five",
+			"one two three four",
+	};
+	
+	
+	StringCrossMapper mapper;
+	
 	protected void setUp() throws Exception {
 		super.setUp();
+		this.mapper = new StringCrossMapper();
+		for (int i=0; i<strings.length; i++) {
+			this.mapper.addSource("S_"+i, strings[i]);
+			this.mapper.addSource("T_"+i, strings[i]);
+		}
 	}
+	
+	
 
 }
