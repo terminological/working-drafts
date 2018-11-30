a9fe81e0f7d203c6c45dec3493f1d1b65b542ccb
Rob Challen <rob@terminological.co.uk>
2018-11-30 22:38:17 +0000

    / ‘nlp-tools/src/main/java/uk/co/terminological/nlptools/Corpus.java’

--- nlp-tools/src/main/java/uk/co/terminological/nlptools/Corpus.java
+++ nlp-tools/src/main/java/uk/co/terminological/nlptools/Corpus.java
@@ -1,9 +1,7 @@
 package uk.co.terminological.nlptools;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
