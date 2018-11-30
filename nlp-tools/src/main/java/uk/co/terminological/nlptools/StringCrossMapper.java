4718c5229469f634f9533d3508d49aeb7d1604ce
Rob Challen <rob@terminological.co.uk>
2018-11-30 22:38:07 +0000

    / ‘nlp-tools/src/main/java/uk/co/terminological/nlptools/StringCrossMapper.java’

--- nlp-tools/src/main/java/uk/co/terminological/nlptools/StringCrossMapper.java
+++ nlp-tools/src/main/java/uk/co/terminological/nlptools/StringCrossMapper.java
@@ -1,10 +1,7 @@
 package uk.co.terminological.nlptools;
 
-import java.util.AbstractMap.SimpleEntry;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
