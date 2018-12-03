dc460fd9a5f50e0faa2aa7d8540e0ef633cb2ff2
Rob Challen <rob@terminological.co.uk>
2018-12-03 19:14:19 +0000

    / ‘nlp-tools/src/test/java/uk/co/terminological/nlptools/StringCrossMapperTest.java’

--- nlp-tools/src/test/java/uk/co/terminological/nlptools/StringCrossMapperTest.java
+++ nlp-tools/src/test/java/uk/co/terminological/nlptools/StringCrossMapperTest.java
@@ -38,6 +38,16 @@ public class StringCrossMapperTest extends TestCase {
 					)
 			);
 		
+		System.out.println("=================");
+		
+		mapper.getAllMatchesBySimilarity(0D, d -> d.termsByEntropy()).forEach(
+				(k,v) -> v.forEach(
+						(k2,v2) -> System.out.println(k.getIdentifier()+"\t"+k2.getIdentifier()+"\t"+v2.toString())
+					)
+			);
+		
+		System.out.println("=================");
+		
 		mapper.getSource().getTermsByTotalEntropy().forEach(term -> {
 			System.out.println(term.toString() + "\t" + mapper.getSource().totalShannonEntropy(term));
 		});
