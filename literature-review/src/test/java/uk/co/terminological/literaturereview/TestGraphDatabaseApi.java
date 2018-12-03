d8d7aa9b2bc62f8948733fdc35e3bd0f57a7d5ef
Rob Challen <rob@terminological.co.uk>
2018-12-03 09:29:49 +0000

    / ‘literature-review/src/test/java/uk/co/terminological/literaturereview/TestGraphDatabaseApi.java’

--- literature-review/src/test/java/uk/co/terminological/literaturereview/TestGraphDatabaseApi.java
+++ literature-review/src/test/java/uk/co/terminological/literaturereview/TestGraphDatabaseApi.java
@@ -70,7 +70,7 @@ public class TestGraphDatabaseApi {
 		
 		PrintStream out = new PrintStream(Files.newOutputStream(outputDir.resolve("authorSim.tsv")));
 		
-		mapper.getAllMatchesBySimilarity(0.02D).forEach(
+		mapper.getAllMatchesBySimilarity(0.75D).forEach(
 			(src,match) -> {
 				match.forEach((target,score) -> {
 					out.println(
