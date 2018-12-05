f732a5c6e11971aa21ccc634838fd980bd1107f5
Rob Challen <rob@terminological.co.uk>
2018-12-05 13:00:03 +0000

    / ‘pubmedclient/src/main/java/uk/co/terminological/pubmedclient/CrossRefResult.java’

--- pubmedclient/src/main/java/uk/co/terminological/pubmedclient/CrossRefResult.java
+++ pubmedclient/src/main/java/uk/co/terminological/pubmedclient/CrossRefResult.java
@@ -5,7 +5,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.UUID;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
