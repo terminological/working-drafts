18f33bbe3f3ae8eb57f65b8c1e0a52479570e1f1
Rob Challen <rob@terminological.co.uk>
2018-12-05 13:21:27 +0000

    / ‘pubmedclient/src/test/java/uk/co/terminological/pubmedclient/TestPdfUtil.java’

--- pubmedclient/src/test/java/uk/co/terminological/pubmedclient/TestPdfUtil.java
+++ pubmedclient/src/test/java/uk/co/terminological/pubmedclient/TestPdfUtil.java
@@ -11,7 +11,8 @@ public class TestPdfUtil {
 	public static String[] urls = {
 			"https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6227809/pdf/rsfs20180033.pdf",
 			"https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6227809/pdf",
-			"http://arxiv.org/pdf/1811.08430"
+			"http://arxiv.org/pdf/1811.08430",
+			"https://doi.org/10.1016/j.scitotenv.2018.09.257"
 	};
 	
 	public static void main(String[] args) throws IOException, BibliographicApiException {
