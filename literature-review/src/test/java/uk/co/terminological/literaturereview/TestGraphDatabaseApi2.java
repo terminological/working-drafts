e9c66f4bbed076c2dbe7e30bb6ea84790e71dafa
Robert Challen <rob@terminological.co.uk>
2018-12-06 17:57:33 +0000

    + ‘literature-review/src/test/java/uk/co/terminological/literaturereview/TestGraphDatabaseApi2.java’

---
+++ literature-review/src/test/java/uk/co/terminological/literaturereview/TestGraphDatabaseApi2.java
@@ -0,0 +1,72 @@
+package uk.co.terminological.literaturereview;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import pl.edu.icm.cermine.exception.AnalysisException;
+import uk.co.terminological.pubmedclient.BibliographicApiException;
+
+
+
+public class TestGraphDatabaseApi2 {
+
+	private static final Logger logger = LoggerFactory.getLogger(TestGraphDatabaseApi2.class);
+	
+	public static void main(String args[]) throws IOException, BibliographicApiException, AnalysisException {
+		BasicConfigurator.configure();
+		org.apache.log4j.Logger.getRootLogger().setLevel(Level.INFO);
+
+		String propFilename = args.length ==1? args[0]: "~/Dropbox/litReview/project.prop";
+		Path propPath= Paths.get(propFilename.replace("~", System.getProperty("user.home")));
+		Properties prop = System.getProperties();
+		prop.load(Files.newInputStream(propPath));
+
+		prop.forEach((k,v) -> prop.put(k, v.toString().replace("~", System.getProperty("user.home")))); 
+		Path graphDbPath = fromProperty(prop,"graph-db-directory");
+		Path graphConfPath = fromProperty(prop,"graph-conf-file");
+		Path outputDir = fromProperty(prop,"output-directory");
+		
+		GraphDatabaseApi graphApi = GraphDatabaseApi.create(graphDbPath, graphConfPath);
+		
+		graphApi.waitAndShutdown();
+	}
+	
+	static String repeat(String s, int count) {
+		StringBuilder out = new StringBuilder();
+		for (int i=0; i<count; i++) out.append(s+" ");
+		return out.toString().trim();
+	}
+	
+	static Path fromProperty(Properties prop, String name) {
+		return Paths.get(prop.getProperty(name).replace("~", System.getProperty("user.home")));
+	}
+	
+	@Before
+	public void setUp() throws Exception {
+		BasicConfigurator.configure();
+	}
+
+	@Test
+	public final void testCreate() throws IOException {
+		
+		Path tmp = Files.createTempDirectory("neo4j");
+		
+		GraphDatabaseApi api = GraphDatabaseApi.create(tmp);
+		logger.info("graph Db available in "+tmp);
+		api.shutdown();
+		
+		Files.deleteIfExists(tmp);
+	}
+
+	
+}
