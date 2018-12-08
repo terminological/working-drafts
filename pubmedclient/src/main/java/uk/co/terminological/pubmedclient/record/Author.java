d2fbf3c740c82a5a01c29e4fd06b09fba88ee105
Robert Challen <rob@terminological.co.uk>
2018-12-08 11:12:57 +0000

    / ‘pubmedclient/src/main/java/uk/co/terminological/pubmedclient/record/Author.java’ / ‘pubmedclient/src/main/java/uk/co/terminological/pubmedclient/record/Record.java’

--- pubmedclient/src/main/java/uk/co/terminological/pubmedclient/record/Author.java
+++ pubmedclient/src/main/java/uk/co/terminological/pubmedclient/record/Author.java
@@ -1,5 +1,13 @@
 package uk.co.terminological.pubmedclient.record;
 
+import java.util.Optional;
+
 public interface Author {
 
+	Optional<RecordReference> getOrcid();
+	Optional<String> getFirstName();
+	String getLastName();
+	Optional<String> getInitials();
+	Optional<Affiliation> getAffiliation();
+	
 }
--- pubmedclient/src/main/java/uk/co/terminological/pubmedclient/record/Record.java
+++ pubmedclient/src/main/java/uk/co/terminological/pubmedclient/record/Record.java
@@ -1,9 +1,19 @@
 package uk.co.terminological.pubmedclient.record;
 
+import java.net.URL;
+import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 public interface Record extends RecordReference {
 
+	public RecordReference getPrimaryIdentifier();
 	public Set<RecordReference> getOtherIdentifiers();
+	public List<Author> getAuthors();
+	public Optional<String> getAbstract();
+	public String getTitle();
+	public List<RecordReference> getCitations();
+	public String getJournalLocation();
+	public Optional<URL> getPdfLocation(); 
 	
 }
