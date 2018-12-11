482e15a117cd80ef2cf99a18e3770285c38e98ca
Robert Challen <rob@terminological.co.uk>
2018-12-11 17:06:05 +0000

    / ‘literature-review/src/main/python/visuals.py’

--- literature-review/src/main/python/visuals.py
+++ literature-review/src/main/python/visuals.py
@@ -4,15 +4,19 @@ Spyder Editor
 
 This is a temporary script file.
 """
-
+#%%
 from neo4j import GraphDatabase
+
 
 uri = "bolt://localhost:7687"
 driver = GraphDatabase.driver(uri, auth=("neo4j", "password"))
 
-def print_friends_of(tx):
-    for record in tx.run("match (n:Article), (m:Article) WHERE n.title <> '' AND n.title = m.title AND n<>m RETURN n.title,n.pmid,n.doi,m.title,m.pmid,m.doi"):
-        print(record["m.title"])
 #%%
+
+duplicateTitlesQry = "match (n:Article), (m:Article) WHERE n.title <> '' AND n.title = m.title AND n<>m RETURN n.title,n.pmid,n.doi,m.title,m.pmid,m.doi"
+   
 with driver.session() as session:
-    session.read_transaction(print_friends_of)
+    result = session.run(duplicateTitlesQry)
+    session.detach(result)
+
+#%%
