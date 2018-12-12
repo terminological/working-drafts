e23ba93a3ae4efdf017972af0ca84e2de2349b24
Robert Challen <rob@terminological.co.uk>
2018-12-12 13:37:57 +0000

    / ‘graphs/src/main/java/uk/co/terminological/simplechart/D3JSWriter.java’

--- graphs/src/main/java/uk/co/terminological/simplechart/D3JSWriter.java
+++ graphs/src/main/java/uk/co/terminological/simplechart/D3JSWriter.java
@@ -1,21 +1,27 @@
 package uk.co.terminological.simplechart;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import freemarker.template.TemplateException;
 
-public class D3JSWriter<X> extends Writer<X> {
+public abstract class D3JSWriter<X> extends Writer<X> {
 
 	@Override
 	protected void process() throws IOException, TemplateException {
-		// TODO Auto-generated method stub
+		File f = getChart().getFile("html");
+		PrintWriter out = new PrintWriter(new FileWriter(f));
+		getTemplate().process(getRoot(), out);
+		out.close();
+		Chart.log.info("Writing html to: "+f.getAbsolutePath());
 		
+		//TODO: Kick of a headless chrome instance and simulate click on download SVG item
+		// or just get user to open file in chrome
 	}
 
 	@Override
-	protected String extractData() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	protected abstract String extractData();
 
 }
