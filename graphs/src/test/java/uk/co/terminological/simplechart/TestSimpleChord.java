a51395edcfffdd72c9af9bc79bef192503653d54
Rob Challen <rob@terminological.co.uk>
2018-12-13 14:51:09 +0000

    / ‘graphs/src/test/java/uk/co/terminological/simplechart/TestSimpleChord.java’

--- graphs/src/test/java/uk/co/terminological/simplechart/TestSimpleChord.java
+++ graphs/src/test/java/uk/co/terminological/simplechart/TestSimpleChord.java
@@ -2,6 +2,7 @@ package uk.co.terminological.simplechart;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 import org.apache.log4j.BasicConfigurator;
@@ -22,11 +23,18 @@ public class TestSimpleChord {
 		
 		List<Triple<String,Double,String>> links = FluentList.create(
 				Triple.create("one", 1.0, "two"),
+				Triple.create("two", 0.5, "one"),
 				Triple.create("two", 2.0, "three"),
+				Triple.create("three", 1.0, "two"),
 				Triple.create("three", 1.0, "four"),
+				Triple.create("four", 0.5, "three"),
 				Triple.create("four", 2.0, "five"),
-				Triple.create("five", 1.0, "one")
+				Triple.create("five", 1.0, "four"),
+				Triple.create("five", 1.0, "one"),
+				Triple.create("one", 0.5, "five")
 				);
+		
+		List<String> order = Arrays.asList("one","two","three","four","five");
 		
 		Chart tmp = Figure.outputTo(new File("/home/terminological/tmp/chord"))
 			.withNewChart("Hello", ChartType.CHORD)
@@ -34,6 +42,7 @@ public class TestSimpleChord {
 				.bind(SOURCE, t -> t.getFirst())
 				.bind(WEIGHT, t -> t.getSecond())
 				.bind(TARGET, t -> t.getThird())
+				.sorting(SOURCE, (o1,o2) -> order.indexOf(o1)-(order.indexOf(o2)))
 			.done();
 			
 		tmp.render();
