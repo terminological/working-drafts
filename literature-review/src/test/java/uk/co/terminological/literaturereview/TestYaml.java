692d5048f4c397f425f1d746ccc093ec0c049123
Rob Challen <rob@terminological.co.uk>
2018-12-14 23:31:50 +0000

    / ‘literature-review/src/test/java/uk/co/terminological/literaturereview/TestYaml.java’

--- literature-review/src/test/java/uk/co/terminological/literaturereview/TestYaml.java
+++ literature-review/src/test/java/uk/co/terminological/literaturereview/TestYaml.java
@@ -1,5 +1,21 @@
 package uk.co.terminological.literaturereview;
 
+import java.io.InputStream;
+import java.util.Map;
+
+import org.yaml.snakeyaml.Yaml;
+
+
+
 public class TestYaml {
 
+	public static void main(String[] args) {
+	Yaml yaml = new Yaml();
+	InputStream inputStream = TestYaml.class
+	  .getClassLoader()
+	  .getResourceAsStream("cypherQuery.yaml");
+	Map<String, Object> obj = yaml.load(inputStream);
+	System.out.println(obj);
+	}
+	
 }
