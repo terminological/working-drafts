88ac4464ed7cf1bd025c690f32cbdb2095de704e
Rob Challen <rob@terminological.co.uk>
2018-12-17 10:24:47 +0000

    / ‘graphs/src/main/java/uk/co/terminological/simplechart/WordcloudWriter.java’

--- graphs/src/main/java/uk/co/terminological/simplechart/WordcloudWriter.java
+++ graphs/src/main/java/uk/co/terminological/simplechart/WordcloudWriter.java
@@ -1,37 +1,49 @@
 package uk.co.terminological.simplechart;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+
+import com.kennycason.kumo.CollisionMode;
+import com.kennycason.kumo.WordCloud;
+import com.kennycason.kumo.WordFrequency;
+import com.kennycason.kumo.bg.CircleBackground;
+import com.kennycason.kumo.font.scale.SqrtFontScalar;
+import com.kennycason.kumo.nlp.FrequencyAnalyzer;
+import com.kennycason.kumo.palette.*;
 
 import freemarker.template.TemplateException;
 import uk.co.terminological.simplechart.Chart.Dimension;
 
 public class WordcloudWriter extends Writer {
 
+	List<String> text = new ArrayList<>();
+	ColorPalette pallette;
+	
 	@Override
 	protected void process() throws IOException, TemplateException {
 		
 		File f = getChart().getFile("png");
 		final FrequencyAnalyzer frequencyAnalyzer = new FrequencyAnalyzer();
-		final List<WordFrequency> wordFrequencies = frequencyAnalyzer.load(Collections.singletonList(getData()));
-		final Dimension dimension = new Dimension(600, 600);
+		final List<WordFrequency> wordFrequencies = frequencyAnalyzer.load(text);
+		final java.awt.Dimension dimension = new java.awt.Dimension(600, 600);
 		final WordCloud wordCloud = new WordCloud(dimension, CollisionMode.PIXEL_PERFECT);
 		wordCloud.setPadding(2);
 		wordCloud.setBackground(new CircleBackground(300));
-		wordCloud.setColorPalette(new ColorPalette(new Color(0x4055F1), new Color(0x408DF1), new Color(0x40AAF1), new Color(0x40C5F1), new Color(0x40D3F1), new Color(0xFFFFFF)));
+		
+		wordCloud.setColorPalette(pallette);
 		wordCloud.setFontScalar(new SqrtFontScalar(10, 40));
 		wordCloud.build(wordFrequencies);
-		wordCloud.writeToFile("kumo-core/output/datarank_wordcloud_circle_sqrt_font.png");
+		wordCloud.writeToFile(f.getAbsolutePath());
 		
-		PrintWriter out = new PrintWriter(new FileWriter(f));
-		getTemplate().process(getRoot(), out);
-		out.close();
-		Chart.log.info("Writing html to: "+f.getAbsolutePath());
+		Chart.log.info("Writing word cloud to: "+f.getAbsolutePath());
 		
 	}
 
@@ -41,8 +53,11 @@ public class WordcloudWriter extends Writer {
 	}
 
 	protected <Y> String extractData(Series<Y> series) {
+		List<Color> colors = series.getScheme().values(8).stream().map(c -> c.toAwt()).collect(Collectors.toList());
+		pallette = new ColorPalette(colors);
 		Function<Y, Object> xGenerator = series.functionFor(Dimension.TEXT);
-		return series.getData().stream().map(xGenerator).map(o -> o.toString()).collect(Collectors.joining("\n"));		
+		series.getData().stream().map(xGenerator).map(o -> o.toString()).forEach(s-> text.add(s));
+		return "";
 	}
 	
 }
