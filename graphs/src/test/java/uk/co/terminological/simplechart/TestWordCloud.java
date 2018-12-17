5bde1bf96f02fb89133e02f5462476ff9722aca8
Rob Challen <rob@terminological.co.uk>
2018-12-17 10:35:13 +0000

    / ‘graphs/src/test/java/uk/co/terminological/simplechart/TestWordCloud.java’

--- graphs/src/test/java/uk/co/terminological/simplechart/TestWordCloud.java
+++ graphs/src/test/java/uk/co/terminological/simplechart/TestWordCloud.java
@@ -1,11 +1,10 @@
 package uk.co.terminological.simplechart;
 
-import static uk.co.terminological.simplechart.Chart.Dimension.ID;
-import static uk.co.terminological.simplechart.Chart.Dimension.LABEL;
-import static uk.co.terminological.simplechart.Chart.Dimension.STRENGTH;
+import static uk.co.terminological.simplechart.Chart.Dimension.TEXT;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 import org.apache.log4j.BasicConfigurator;
@@ -17,42 +16,24 @@ import uk.co.terminological.datatypes.Tuple;
 
 public class TestWordCloud {
 
+	static List<String> text = Arrays.asList(
+			"Machine learning tasks are classified into several broad categories. In supervised learning, the algorithm builds a mathematical model of a set of data that contains both the inputs and the desired outputs. For example, if the task were determining whether an image contained a certain object, the training data for a supervised learning algorithm would include images with and without that object (the input), and each image would have a label (the output) designating whether it contained the object. In special cases, the input may be only partially available, or restricted to special feedback.[clarification needed] Semi-supervised learning algorithms develop mathematical models from incomplete training data, where a portion of the sample inputs are missing the desired output.",
+			"Classification algorithms and regression algorithms are types of supervised learning. Classification algorithms are used when the outputs are restricted to a limited set of values. For a classification algorithm that filters emails, the input would be an incoming email, and the output would be the name of the folder in which to file the email. For an algorithm that identifies spam emails, the output would be the prediction of either spam or not spam, represented by the Boolean values one and zero. Regression algorithms are named for their continuous outputs, meaning they may have any value within a range. Examples of a continuous value are the temperature, length, or price of an object.",
+			"In unsupervised learning, the algorithm builds a mathematical model of a set of data which contains only inputs and no desired outputs. Unsupervised learning algorithms are used to find structure in the data, like grouping or clustering of data points. Unsupervised learning can discover patterns in the data, and can group the inputs into categories, as in feature learning. Dimensionality reduction is the process of reducing the number of features, or inputs, in a set of data.",
+			"Active learning algorithms access the desired outputs (training labels) for a limited set of inputs based on a budget, and optimize the choice of inputs for which it will acquire training labels. When used interactively, these can be presented to a human user for labeling. Reinforcement learning algorithms are given feedback in the form of positive or negative reinforcement in a dynamic environment, and are used in autonomous vehicles or in learning to play a game against a human opponent.[2]:3 Other specialized algorithms in machine learning include topic modeling, where the computer program is given a set of natural language documents and finds other documents that cover similar topics. Machine learning algorithms can be used to find the unobservable probability density function in density estimation problems. Meta learning algorithms learn their own inductive bias based on previous experience. In developmental robotics, robot learning algorithms generate their own sequences of learning experiences, also known as a curriculum, to cumulatively acquire new skills through self-guided exploration and social interaction with humans. These robots use guidance mechanisms such as active learning, maturation, motor synergies, and imitation."
+			);
+	
 	public static void main(String[] args) throws IOException, TemplateException {
 		
 		BasicConfigurator.configure();
 		
-		List<Tuple<Integer,String>> nodes = FluentList.create(
-				Tuple.create(1, "one"),
-				Tuple.create(2, "two"),
-				Tuple.create(3, "three"),
-				Tuple.create(4, "four"),
-				Tuple.create(5, "five")
-				);
-		
-		List<Triple<Integer,Double,Integer>> links = FluentList.create(
-				Triple.create(1, 1.0, 2),
-				Triple.create(2, 0.5, 3),
-				Triple.create(3, 0.1, 4),
-				Triple.create(4, 0.5, 5),
-				Triple.create(5, 1.0, 1)
-				);
-		
-		Chart tmp = Figure.outputTo(new File(System.getProperty("user.home")+"/tmp/network"))
-			.withNewChart("Hello", ChartType.NETWORK)
-			.withSeries(nodes)
-				.bind(ID, t -> t.getFirst())
-				.bind(LABEL, t -> t.getSecond())
+		Figure.outputTo(new File(System.getProperty("user.home")+"/tmp/wordcloud"))
+			.withNewChart("Hello", ChartType.WORDCLOUD)
+			.withSeries(text)
+				.bind(TEXT, t -> t)
+				.withColourScheme(ColourScheme.Blues)
 			.done()
-			.withSeries(links)
-				.bind(ID, t -> t.getFirst(), "source")
-				.bind(STRENGTH, t -> t.getSecond())
-				.bind(ID, t -> t.getThird(), "target")
-			.done()
-			.config()
-				.withXLabel("x-axis")
-				.withYLabel("y-axis")
-			.done();
-		tmp.render();
+			.render();
 		;
 	}
 
