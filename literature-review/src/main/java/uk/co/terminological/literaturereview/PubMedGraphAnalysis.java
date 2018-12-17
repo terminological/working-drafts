629709efeff59c4804d61e1100e37317e38c3650
Rob Challen <rob@terminological.co.uk>
2018-12-17 14:57:50 +0000

    / ‘literature-review/pom.xml’ / ‘literature-review/src/main/java/uk/co/terminological/literaturereview/PubMedGraphAnalysis.java’

--- literature-review/src/main/java/uk/co/terminological/literaturereview/PubMedGraphAnalysis.java
+++ literature-review/src/main/java/uk/co/terminological/literaturereview/PubMedGraphAnalysis.java
@@ -1,10 +1,73 @@
 package uk.co.terminological.literaturereview;
 
+import static uk.co.terminological.simplechart.Chart.Dimension.X;
+import static uk.co.terminological.simplechart.Chart.Dimension.Y;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.log4j.BasicConfigurator;
+import org.neo4j.driver.v1.AuthTokens;
+import org.neo4j.driver.v1.Config;
+import org.neo4j.driver.v1.Driver;
+import org.neo4j.driver.v1.GraphDatabase;
+import org.neo4j.driver.v1.Session;
+import org.neo4j.driver.v1.exceptions.ServiceUnavailableException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.yaml.snakeyaml.Yaml;
+
+import uk.co.terminological.simplechart.ChartType;
+import uk.co.terminological.simplechart.Figure;
+
 public class PubMedGraphAnalysis {
 
+	static Logger log = LoggerFactory.getLogger(PubMedGraphAnalysis.class);
+	
 	public static void main(String[] args) {
 		
-		private final Driver driver;
+		BasicConfigurator.configure();
+		
+		Config config = Config.builder()
+	            .withMaxConnectionLifetime( 30, TimeUnit.MINUTES )
+	            .withMaxConnectionPoolSize( 50 )
+	            .withConnectionAcquisitionTimeout( 2, TimeUnit.MINUTES )
+	            .build();
+		
+		final Driver driver = GraphDatabase.driver( "bolt://localhost:7687", AuthTokens.basic( "neo4j", "neo4j" ), config );
+		
+		Yaml yaml = new Yaml();
+		InputStream inputStream = PubMedGraphAnalysis.class.getClassLoader().getResourceAsStream("cypherQuery.yaml");
+		Map<String, Object> obj = yaml.load(inputStream);
+		
+		@SuppressWarnings("unchecked")
+		Map<String,String> queries = (Map<String,String> ) obj.get("analyze");
+		
+		try ( Session session = driver.session() ) {
+	        session.readTransaction( tx -> {
+	        	
+	        	tx.run( queries.get("getArticlesByAge") );
+	        	
+	        	Figure.outputTo(new File(System.getProperty("user.home")+"/tmp/ggplot"))
+				
+				.withNewChart("Hello", ChartType.XYBAR)
+				.withSeries(example)
+					.bind(X, t -> t.getFirst())
+					.bind(Y, t -> t.getSecond()+Math.random()-0.5D)
+				.done()
+				.config()
+					.withXLabel("x-axis")
+					.withYLabel("y-axis")
+				.done();
+			tmp.render();
+	        	
+	            return true;
+	        });
+	    } catch ( ServiceUnavailableException ex ) {
+	        log.error(ex.getMessage());
+	    }
 		
 	}
 	
