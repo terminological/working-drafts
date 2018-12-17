8f52c139b36e5dd435abb6cc6d68b66b33112a1e
Rob Challen <rob@terminological.co.uk>
2018-12-17 17:28:29 +0000

    / ‘literature-review/src/main/java/uk/co/terminological/literaturereview/PubMedGraphAnalysis.java’ / ‘literature-review/src/main/resources/cypherQuery.yaml’

--- literature-review/src/main/java/uk/co/terminological/literaturereview/PubMedGraphAnalysis.java
+++ literature-review/src/main/java/uk/co/terminological/literaturereview/PubMedGraphAnalysis.java
@@ -1,10 +1,13 @@
 package uk.co.terminological.literaturereview;
 
-import static uk.co.terminological.simplechart.Chart.Dimension.X;
+import static uk.co.terminological.simplechart.Chart.Dimension.ID;
+import static uk.co.terminological.simplechart.Chart.Dimension.STRENGTH;
 import static uk.co.terminological.simplechart.Chart.Dimension.Y;
 
 import java.io.File;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -21,6 +24,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.yaml.snakeyaml.Yaml;
 
+import uk.co.terminological.datatypes.Triple;
 import uk.co.terminological.simplechart.ChartType;
 import uk.co.terminological.simplechart.Figure;
 
@@ -48,7 +52,9 @@ public class PubMedGraphAnalysis {
 		Map<String,String> queries = (Map<String,String> ) obj.get("analyse");
 		
 		try ( Session session = driver.session() ) {
-	        session.readTransaction( tx -> {
+	        
+			//Plot by age
+			session.readTransaction( tx -> {
 	        	
 	        	String qry = queries.get("getArticlesByAge");
 	        	List<Record> res = tx.run( qry ).list();
@@ -68,12 +74,73 @@ public class PubMedGraphAnalysis {
 	        	
 	            return true;
 	        });
+	        
+	        session.readTransaction( tx -> {
+	        	
+	        	String qry = queries.get("getMeshCodeCooccurMutualInformation");
+	        	List<Record> res = tx.run( qry ).list();
+	        	//List<Triple<String,Double,String>> links = new ArrayList<>();
+	        	Map<String, Integer> nodes = new HashMap<>();
+	        	Map<String, DataEntry> links = new HashMap<>();
+	        	
+	        	res.forEach(r -> {
+	        		DataEntry tmp = new DataEntry();
+	        		tmp.sourceTerm = r.get("sourceTerm").asString();
+	        		tmp.sourceOccurrences = r.get("sourceOccurrences").asInt();
+	        		tmp.targetTerm = r.get("targetTerm").asString();
+	        		tmp.targetOccurrences = r.get("targetOccurrences").asInt();
+	        		tmp.cooccurrenceCount = r.get("cooccurrences").asInt();
+	        		tmp.totalOccurrence = r.get("totalOccurrences").asInt();
+	        		
+	        		if (!nodes.containsKey(tmp.sourceTerm) && nodes.size() < 50) {
+	        			nodes.put(tmp.sourceTerm, tmp.sourceOccurrences);
+	        		} else {
+	        			tmp.sourceTerm = "Other";
+	        		}
+	        		
+	        		if (!nodes.containsKey(tmp.targetTerm) && nodes.size() < 50) {
+	        			nodes.put(tmp.targetTerm, tmp.targetOccurrences);
+	        		} else {
+	        			tmp.targetTerm = "Other";
+	        		}
+	        		
+	        		DataEntry previous = links.get(tmp.key());
+	        		if (previous != null) 
+	        			tmp.cooccurrenceCount += previous.cooccurrenceCount;
+	        		links.put(tmp.key(), tmp);
+	        			        		
+	        	});
+	        	
+	        	try {
+	        	Figure.outputTo(new File(System.getProperty("user.home")+"/tmp/lit-review"))
+					.withNewChart("Articles by age", ChartType.CHORD)
+					.withSeries(new ArrayList<>(links.values()))
+						.bind(ID, t -> t.sourceTerm, "source")
+						.bind(STRENGTH, t -> t.cooccurrenceCount)
+						.bind(ID, t -> t.targetTerm, "term")
+					.done()
+					.render();
+	        	} catch (Exception e) {throw new RuntimeException(e);}
+	        	
+	            return true;
+	        });
+	        
 	    } catch ( ServiceUnavailableException ex ) {
 	        log.error(ex.getMessage());
 	    }
 		
 		driver.close();
 		
+	}
+	
+	private static class DataEntry {
+		String sourceTerm;
+		String targetTerm;
+		Integer sourceOccurrences;
+		Integer targetOccurrences;
+		Integer cooccurrenceCount;
+		Integer totalOccurrence;
+		String key() {return sourceTerm+"_"+targetTerm;}
 	}
 	
 }
