e9f589fb5ceac247048eeb0082dd1cf3ba48ca7b
Robert Challen <rob@terminological.co.uk>
2019-01-16 11:50:18 +0000

    / ‘pubmedclient/src/main/java/uk/co/terminological/pubmedclient/record/PrintRecord.java’

--- pubmedclient/src/main/java/uk/co/terminological/pubmedclient/record/PrintRecord.java
+++ pubmedclient/src/main/java/uk/co/terminological/pubmedclient/record/PrintRecord.java
@@ -1,5 +1,13 @@
 package uk.co.terminological.pubmedclient.record;
 
+import java.util.Optional;
+
 public interface PrintRecord {
 
+	public String getFirstAuthorName();
+	public Optional<String> getJournal();
+	public Optional<String> getVolume();
+	public Optional<String> getIssue();
+	public Optional<Long> getYear();
+	public Optional<String> getPage();
 }
