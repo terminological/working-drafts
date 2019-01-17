5a1014d00d77e8995e8a06a13bd0c7687e3cdc02
Rob Challen <rob@terminological.co.uk>
2019-01-17 17:14:47 +0000

    / ‘pubmedclient/src/test/java/uk/co/terminological/pubmedclient/TestJsonParsing.java’

--- pubmedclient/src/test/java/uk/co/terminological/pubmedclient/TestJsonParsing.java
+++ pubmedclient/src/test/java/uk/co/terminological/pubmedclient/TestJsonParsing.java
@@ -20,8 +20,23 @@ public class TestJsonParsing {
 		ListResult  response = new ListResult(objectMapper.readTree(is));
 		response.getMessage().getItems().forEach(
 				item -> {
+					System.out.println(item.getAbstract());
+					System.out.println(item.getCitedByCount());
+					System.out.println(item.getDate());
+					System.out.println(item.getFirstAuthorName());
+					System.out.println(item.getIdentifier());
+					System.out.println(item.getIdentifierType());
+					System.out.println(item.getIssue());
+					System.out.println(item.getJournal());
+					System.out.println(item.getLabel());
+					System.out.println(item.getPage());
+					System.out.println(item.getPdfUri());
+					System.out.println(item.getReferencesCount());
+					System.out.println(item.getScore());
 					System.out.println(item.getTitle());
-					item.getTextMiningUri().ifPresent(System.out::println);
+					System.out.println(item.getTextMiningUri());
+					System.out.println(item.getVolume());
+					System.out.println(item.getYear());
 				}
 				);
 		
