6d850d9530d246e7b89e52be0fda491289fbfaea
Rob Challen <rob@terminological.co.uk>
2019-01-21 10:03:18 +0000

    / ‘nlp-tools/src/main/java/uk/co/terminological/nlptools/TermInstance.java’

--- nlp-tools/src/main/java/uk/co/terminological/nlptools/TermInstance.java
+++ nlp-tools/src/main/java/uk/co/terminological/nlptools/TermInstance.java
@@ -1,23 +1,40 @@
 package uk.co.terminological.nlptools;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 public class TermInstance {
 
-	Optional<Term> previous = Optional.empty();
-	Term current;
-	Optional<Term> next = Optional.empty();
+	private Optional<TermInstance> previous = Optional.empty();
+	private Term current;
+	private Optional<TermInstance> next = Optional.empty();
 	
 	public TermInstance(Term current) {
 		this.current = current;
 	}
 	
-	public TermInstance(Term current, Term previous) {
+	public TermInstance(Term current, TermInstance previous) {
 		this(current);
 		this.previous = Optional.ofNullable(previous);
 	}
 	
 	public Term getTerm() {
 		return current;
+	}
+	
+	public boolean isFirst() {
+		return !previous.isPresent();
+	}
+	
+	public boolean isLast() {
+		return !next.isPresent();
+	}
+	
+	public List<TermInstance> getNext(int n) {
+		if (next.isPresent()) {
+			if (n==1) return Collections.singletonList(next.get());
+			return next.
+		}
 	}
 }
