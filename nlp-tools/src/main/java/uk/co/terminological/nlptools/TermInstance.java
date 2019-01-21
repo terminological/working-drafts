f12fba05df54771a79c4fcb5cff292f73c690bfa
Rob Challen <rob@terminological.co.uk>
2019-01-21 10:09:29 +0000

    / ‘nlp-tools/src/main/java/uk/co/terminological/nlptools/TermInstance.java’

--- nlp-tools/src/main/java/uk/co/terminological/nlptools/TermInstance.java
+++ nlp-tools/src/main/java/uk/co/terminological/nlptools/TermInstance.java
@@ -4,6 +4,8 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
+import uk.co.terminological.datatypes.FluentList;
+
 public class TermInstance {
 
 	private Optional<TermInstance> previous = Optional.empty();
@@ -33,8 +35,29 @@ public class TermInstance {
 	
 	public List<TermInstance> getNext(int n) {
 		if (next.isPresent()) {
-			if (n==1) return Collections.singletonList(next.get());
-			return next.
+			if (n==1) {
+				return FluentList.create(next.get());
+			} else {
+				List<TermInstance> tmp = getNext(n-1);
+				tmp.set(0, next.get());
+				return tmp;
+			}
+		} else {
+			return FluentList.empty();
+		}
+	}
+	
+	public List<TermInstance> getPrevious(int n) {
+		if (previous.isPresent()) {
+			if (n==1) {
+				return FluentList.create(previous.get());
+			} else {
+				List<TermInstance> tmp = getNext(n-1);
+				tmp.set(0, previous.get());
+				return tmp;
+			}
+		} else {
+			return FluentList.empty();
 		}
 	}
 }
