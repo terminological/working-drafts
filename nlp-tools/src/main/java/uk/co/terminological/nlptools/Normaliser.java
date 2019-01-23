659c95bb7251b0cb1a3056be7762f1eb8e186d7d
Rob Challen <rob@terminological.co.uk>
2019-01-23 21:22:15 +0000

    / ‘nlp-tools/src/main/java/uk/co/terminological/nlptools/Normaliser.java’ / ‘nlp-tools/src/main/java/uk/co/terminological/nlptools/Tokeniser.java’

--- nlp-tools/src/main/java/uk/co/terminological/nlptools/Normaliser.java
+++ nlp-tools/src/main/java/uk/co/terminological/nlptools/Normaliser.java
@@ -1,8 +1,10 @@
 package uk.co.terminological.nlptools;
 
 import java.util.function.Function;
+import java.util.stream.Stream;
 
 interface Normaliser extends Function<String,String> {
 	
+	static Normaliser DEFAULT = string -> string.replaceAll("[_,\\.]"," ").replaceAll("[^a-zA-Z0-9\\s]", "-").replaceAll("\\s+", " ").toLowerCase();
 	
 }
--- nlp-tools/src/main/java/uk/co/terminological/nlptools/Tokeniser.java
+++ nlp-tools/src/main/java/uk/co/terminological/nlptools/Tokeniser.java
@@ -3,4 +3,8 @@ package uk.co.terminological.nlptools;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
-interface Tokeniser extends Function<String,Stream<String>> {}
+interface Tokeniser extends Function<String,Stream<String>> {
+	
+	public static Tokeniser DEFAULT = string -> Stream.of(string.split("\\s+")).filter(s -> !s.equals("-"));
+	
+}
