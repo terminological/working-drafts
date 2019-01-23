a074c6ffccbb7d95d12331e4cb82861cc087b652
Robert Challen <rob@terminological.co.uk>
2019-01-23 14:42:20 +0000

    / ‘nlp-tools/src/main/java/uk/co/terminological/nlptools/TopicModelBuilder.java’

--- nlp-tools/src/main/java/uk/co/terminological/nlptools/TopicModelBuilder.java
+++ nlp-tools/src/main/java/uk/co/terminological/nlptools/TopicModelBuilder.java
@@ -2,4 +2,15 @@ package uk.co.terminological.nlptools;
 
 public class TopicModelBuilder {
 
+	public static TopicModelBuilder create(Corpus corpus) {
+		return new TopicModelBuilder(corpus);
+	}
+
+	private Corpus corpus;
+	
+	private  TopicModelBuilder(Corpus corpus) {
+		this.corpus = corpus;
+	}
+	
+	
 }
