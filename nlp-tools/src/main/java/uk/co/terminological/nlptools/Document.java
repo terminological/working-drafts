d532ed7d7b6d4e7a553c8dd90ab9289d52dcd526
Rob Challen <rob@terminological.co.uk>
2019-01-25 10:25:25 +0000

    / ‘nlp-tools/src/main/java/uk/co/terminological/nlptools/Document.java’

--- nlp-tools/src/main/java/uk/co/terminological/nlptools/Document.java
+++ nlp-tools/src/main/java/uk/co/terminological/nlptools/Document.java
@@ -1,11 +1,9 @@
 package uk.co.terminological.nlptools;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
