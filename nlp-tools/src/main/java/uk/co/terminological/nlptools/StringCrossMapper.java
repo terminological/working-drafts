2cc993acef5ace53a59b33acaf14afe1f7627448
Rob Challen <rob@terminological.co.uk>
2019-01-25 10:58:51 +0000

    / ‘nlp-tools/src/main/java/uk/co/terminological/nlptools/StringCrossMapper.java’

--- nlp-tools/src/main/java/uk/co/terminological/nlptools/StringCrossMapper.java
+++ nlp-tools/src/main/java/uk/co/terminological/nlptools/StringCrossMapper.java
@@ -142,7 +142,8 @@ public class StringCrossMapper {
 	 * @param minValue
 	 * @return
 	 */
-	public List<Triple<Document,Document,Double>> getAllMatchesBySimilarity(Double minValue, Function<Document,Stream<Weighted<Term>>> mapper, BiFunction<Stream<Weighted<Term>>,Stream<Weighted<Term>>,Double> algorithm) {
+	public List<Triple<Document,Document,Double>> getAllMatchesBySimilarity(Double minValue, 
+			Function<Document,Stream<Weighted<Term>>> mapper, BiFunction<Stream<Weighted<Term>>,Stream<Weighted<Term>>,Double> algorithm) {
 		//<Double> scores = new ArrayList<>(); 
 		List<Triple<Document,Document,Double>> match = new ArrayList<>();
 		Double max = 0D;
