1db72f844f32740934f109bc4165edb5a19a9536
Rob Challen <rob@terminological.co.uk>
2019-01-25 15:10:20 +0000

    / ‘nlp-tools/src/main/java/uk/co/terminological/nlptools/Counted.java’

--- nlp-tools/src/main/java/uk/co/terminological/nlptools/Counted.java
+++ nlp-tools/src/main/java/uk/co/terminological/nlptools/Counted.java
@@ -7,34 +7,34 @@ public class Counted<T> {
 
 	
 
-	Double weight;
+	Integer count;
 	T thing;
 	
-	public Double getWeight() {return weight;}
+	public Integer getCount() {return count;}
 	public T getTarget() {return thing;}
 	
 	public Counted(T thing, Double d) {
 		this.thing = thing;
-		this.weight = d;
+		this.count = d;
 	}
 	
 	public static <X> Counted<X> create(X thing, Double d) {
 		return new Counted<X>(thing, d);
 	}
 	
-	public String toString() {return thing.toString()+"(:"+weight.toString()+")";}
+	public String toString() {return thing.toString()+"(:"+count.toString()+")";}
 	
 	public static <X> SortedSet<Counted<X>> descending() {
 		return new TreeSet<Counted<X>>(
 				(w1,w2) -> {
-					int comp = -w1.getWeight().compareTo(w2.getWeight());
+					int comp = -w1.getCount().compareTo(w2.getCount());
 					return comp == 0 ? w1.hashCode()-w2.hashCode() : comp;
 				});
 	}
 	
 	public static <X> SortedSet<Counted<X>> ascending() {
 		return new TreeSet<Counted<X>>((w1,w2) -> {
-			int comp = w1.getWeight().compareTo(w2.getWeight());
+			int comp = w1.getCount().compareTo(w2.getCount());
 			return comp == 0 ? w1.hashCode()-w2.hashCode() : comp;
 		});
 	}
