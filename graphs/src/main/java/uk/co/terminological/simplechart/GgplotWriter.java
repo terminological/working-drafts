746e3910171a352f7c765f0a1b99963fc734691b
Rob Challen <rob@terminological.co.uk>
2019-01-28 13:21:59 +0000

    / ‘graphs/src/main/java/uk/co/terminological/simplechart/GgplotWriter.java’

--- graphs/src/main/java/uk/co/terminological/simplechart/GgplotWriter.java
+++ graphs/src/main/java/uk/co/terminological/simplechart/GgplotWriter.java
@@ -149,9 +149,9 @@ public abstract class GgplotWriter extends Writer {
 		@Override
 		public List<String> getPlots() {
 			return Arrays.asList(
-					"geom_bar(stat='identity', aes(x=1, y=Y, fill=LABEL))",
-					"theme(axis.line.y=element_blank(),axis.text.y=element_blank(),axis.ticks.y=element_blank(),axis.title.x=element_blank())",
-					"scale_fill_brewer(palette=schemeName)",
+					"geom_bar(stat='identity', aes(x=1, y=Y, fill=factor(LABEL, levels=rev(LABEL))))+",
+					"theme(axis.line.y=element_blank(),axis.text.y=element_blank(),axis.ticks.y=element_blank(),axis.title.y=element_blank())",
+					"scale_fill_brewer(palette=schemeName, breaks=LABEL))",
 					"coord_flip()"
 					);
 		}
