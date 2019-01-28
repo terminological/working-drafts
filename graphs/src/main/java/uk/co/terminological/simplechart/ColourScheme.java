f25eef5033c34a780bf63ea10079aece6d7b3306
Rob Challen <rob@terminological.co.uk>
2019-01-28 10:41:27 +0000

    / ‘graphs/src/main/java/uk/co/terminological/simplechart/ColourScheme.java’

--- graphs/src/main/java/uk/co/terminological/simplechart/ColourScheme.java
+++ graphs/src/main/java/uk/co/terminological/simplechart/ColourScheme.java
@@ -125,7 +125,7 @@ public class ColourScheme {
 	
 	public ColourScheme darker(float ratio) {
 		for (int i = 0; i<values.length; i++) {
-			for (int j = 0; i<values[i].length; j++) {
+			for (int j = 0; j<values[i].length; j++) {
 				values[i][j].darken(ratio);
 			}
 		}
