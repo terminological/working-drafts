0d9e691e7635618b81dd857a935412c3017f9cbd
Robert Challen <rob@terminological.co.uk>
2019-01-30 13:48:42 +0000

    / ‘nlp-tools/src/test/java/uk/co/terminological/nlptools/TestCalculation.java’

--- nlp-tools/src/test/java/uk/co/terminological/nlptools/TestCalculation.java
+++ nlp-tools/src/test/java/uk/co/terminological/nlptools/TestCalculation.java
@@ -9,6 +9,7 @@ public class TestCalculation {
 	@Test
 	public void testPMI() {
 		double pmi = Calculation.pmi(1159, 1938, 1311, 50000952);
+		System.out.println(pmi);
 		assert(Math.abs(pmi-10.0349081703D) < 0.0001 );
 	}
 
