5c870635ebdef5ebda01da8f5bd68d7940f981c2
Rob Challen <rob@terminological.co.uk>
2019-01-30 20:40:08 +0000

    / ‘nlp-tools/src/main/java/uk/co/terminological/nlptools/Calculation.java’

--- nlp-tools/src/main/java/uk/co/terminological/nlptools/Calculation.java
+++ nlp-tools/src/main/java/uk/co/terminological/nlptools/Calculation.java
@@ -3,6 +3,7 @@ package uk.co.terminological.nlptools;
 public class Calculation {
 
 	public static double pmi(int x1y1, int x1, int y1, int total) {
+		if (x1y1 > x1 || x1y1 > y1 || x1 > total || y1 > total) throw new RuntimeException("Parameters invalid");
 		return pmi(
 				((double) x1y1)/total,
 				((double) x1)/total,
@@ -10,6 +11,7 @@ public class Calculation {
 	}
 
 	public static double npmi(int x1y1, int x1, int y1, int total) {
+		if (x1y1 > x1 || x1y1 > y1 || x1 > total || y1 > total) throw new RuntimeException("Parameters invalid");
 		return npmi(
 				((double) x1y1)/total,
 				((double) x1)/total,
@@ -17,16 +19,19 @@ public class Calculation {
 	}
 	
 	public static double pmi(double pxy, double px, double py) {
+		if (pxy < 0D || pxy > 1D || px < 0D || px > 1D || py < 0D || py > 1D || pxy > px || pxy > py ) throw new RuntimeException("Parameters invalid");
 		if (px==0 || py==0) return 0D; //pxy must also be zero at this point. could return NaN...
 		if (pxy == 0) return Double.NEGATIVE_INFINITY;
 		return Math.log(pxy/(px*py));
 	}
 	
 	public static double npmi(double pxy, double px, double py) {
+		if (pxy < 0D || pxy > 1D || px < 0D || px > 1D || py < 0D || py > 1D || pxy > px || pxy > py ) throw new RuntimeException("Parameters invalid");
 		return pmi(pxy,px,py)/(-Math.log(pxy));
 	}
 	
 	public static double mi(int x1y1, int x1, int y1, int total) {
+		if (x1y1 > x1 || x1y1 > y1 || x1 > total || y1 > total) throw new RuntimeException("Parameters invalid");
 		int x1y0 = x1-x1y1;
 		int x0y1 = y1-x1y1;
 		double px1y1 = ((double) x1y1)/total;
@@ -36,6 +41,7 @@ public class Calculation {
 	}
 	
 	public static double mi(double px1y1, double px0y1, double px1y0) {
+		if (px1y1 < 0D || px1y1 > 1D || px0y1 < 0D || px0y1 > 1D || px1y0 < 0D || px1y0 > 1D ) throw new RuntimeException("Parameters invalid");
 		double px0y0 = 1.0-(px1y1+px0y1+px1y0);
 		double px1 = px1y0+px1y1;
 		double px0 = 1.0-px1;
