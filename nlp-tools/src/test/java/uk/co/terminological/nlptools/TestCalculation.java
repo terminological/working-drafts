f58a8e1de2250970ebe20a0b6709b8df9a12ce31
Robert Challen <rob@terminological.co.uk>
2019-01-30 13:54:55 +0000

    / ‘nlp-tools/src/test/java/uk/co/terminological/nlptools/TestCalculation.java’

--- nlp-tools/src/test/java/uk/co/terminological/nlptools/TestCalculation.java
+++ nlp-tools/src/test/java/uk/co/terminological/nlptools/TestCalculation.java
@@ -13,4 +13,11 @@ public class TestCalculation {
 		assert(Math.abs(pmi-10.0349081703D) < 0.0001 );
 	}
 
+	@Test
+	public void testMI() {
+		double mi = Calculation.mi(0.05, 0.15, 0.7, 0.1);
+		System.out.println(mi);
+		assert(Math.abs(mi-0.2141709D) < 0.0001 );
+	}
+	
 }
