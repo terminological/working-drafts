1924f9a325876b1b182ad7ec1e42af0b6f1a7260
Rob Challen <rob@terminological.co.uk>
2019-02-01 08:55:35 +0000

    / ‘nlp-tools/src/main/java/uk/co/terminological/nlptools/Normaliser.java’

--- nlp-tools/src/main/java/uk/co/terminological/nlptools/Normaliser.java
+++ nlp-tools/src/main/java/uk/co/terminological/nlptools/Normaliser.java
@@ -4,6 +4,7 @@ import java.util.function.Function;
 
 public interface Normaliser extends Function<String,String> {
 	
-	static Normaliser DEFAULT = string -> string.replaceAll("[_,\\.]"," ").replaceAll("[^a-zA-Z0-9\\s]", "-").replaceAll("-*\\s+-*", " ").toLowerCase();
+	static Normaliser DEFAULT = string -> string.replaceAll("[_,\\.]"," ").replaceAll("[^\\p{IsAlphabetic}^\\p{IsDigit}]", "-").replaceAll("-*\\s+-*", " ").toLowerCase();
+	static Normaliser ASCII = string -> string.replaceAll("[_,\\.]"," ").replaceAll("[^a-zA-Z0-9\\s]", "-").replaceAll("-*\\s+-*", " ").toLowerCase();
 	
 }
