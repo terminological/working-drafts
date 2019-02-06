16b31858c65a237f8f9757f85fefe187ce739e74
Robert Challen <rob@terminological.co.uk>
2019-02-06 15:59:36 +0000

    / ‘nlp-tools/src/main/java/uk/co/terminological/nlptools/TermInstance.java’

--- nlp-tools/src/main/java/uk/co/terminological/nlptools/TermInstance.java
+++ nlp-tools/src/main/java/uk/co/terminological/nlptools/TermInstance.java
@@ -11,18 +11,26 @@ import uk.co.terminological.datatypes.FluentList;
 
 public class TermInstance implements Serializable {
 
-	private Optional<TermInstance> previous = Optional.empty();
+	private TermInstance previous = null;
 	private Term current;
-	private Optional<TermInstance> next = Optional.empty();
+	private TermInstance next = null;
 	
 	public TermInstance(Term current) {
 		this.current = current;
 	}
 	
+	public Optional<TermInstance> getNext() {
+		return Optional.ofNullable(next);
+	}
+	
+	public Optional<TermInstance> getPrevious() {
+		return Optional.ofNullable(previous);
+	}
+	
 	public TermInstance(Term current, TermInstance previous) {
 		this(current);
-		this.previous = Optional.ofNullable(previous);
-		this.previous.ifPresent( p -> p.next = Optional.of(this) );
+		this.previous = previous;
+		this.getPrevious().ifPresent( p -> p.next = this );
 	}
 	
 	public Term getTerm() {
@@ -30,11 +38,11 @@ public class TermInstance implements Serializable {
 	}
 	
 	public boolean isFirst() {
-		return !previous.isPresent();
+		return !getPrevious().isPresent();
 	}
 	
 	public boolean isLast() {
-		return !next.isPresent();
+		return !getNext().isPresent();
 	}
 	
 	public Set<TermInstance> getNeighbours(int n) {
@@ -44,15 +52,15 @@ public class TermInstance implements Serializable {
 	}
 	
 	public List<TermInstance> getNext(int n) {
-		if (next.isPresent()) {
+		if (getNext().isPresent()) {
 			if (n==1) {
-				return FluentList.create(next.get());
+				return FluentList.create(next);
 			} else {
 				List<TermInstance> tmp = getNext(n-1);
 				if (tmp.size() == 0) {
-					tmp.add(next.get()); 
+					tmp.add(next); 
 				} else {
-					tmp.set(0, next.get());
+					tmp.set(0, next);
 				}
 				return tmp;
 			}
@@ -62,15 +70,15 @@ public class TermInstance implements Serializable {
 	}
 	
 	public List<TermInstance> getPrevious(int n) {
-		if (previous.isPresent()) {
+		if (getPrevious().isPresent()) {
 			if (n==1) {
-				return FluentList.create(previous.get());
+				return FluentList.create(previous);
 			} else {
 				List<TermInstance> tmp = getPrevious(n-1);
 				if (tmp.size() == 0) {
-					tmp.add(previous.get()); 
+					tmp.add(previous); 
 				} else {
-					tmp.set(0, previous.get());
+					tmp.set(0, previous);
 				}
 				return tmp;
 			}
@@ -80,9 +88,9 @@ public class TermInstance implements Serializable {
 	}
 	
 	public String toString() {
-		return previous.map(ti -> ti.current.tag).orElse("X")+"-"+
+		return getPrevious().map(ti -> ti.current.tag).orElse("X")+"-"+
 				current.tag+"-"+
-				next.map(ti -> ti.current.tag).orElse("X");
+				getNext().map(ti -> ti.current.tag).orElse("X");
 	}
 	
 	public Token asToken() {
