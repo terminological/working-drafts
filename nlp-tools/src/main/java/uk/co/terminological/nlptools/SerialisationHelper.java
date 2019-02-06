2c0a3cd116d1172f856bed7c0f2047a3a0cf8dbb
Rob Challen <rob@terminological.co.uk>
2019-02-06 21:59:03 +0000

    / ‘nlp-tools/src/main/java/uk/co/terminological/nlptools/SerialisationHelper.java’

--- nlp-tools/src/main/java/uk/co/terminological/nlptools/SerialisationHelper.java
+++ nlp-tools/src/main/java/uk/co/terminological/nlptools/SerialisationHelper.java
@@ -1,8 +1,9 @@
 package uk.co.terminological.nlptools;
 
 import java.io.IOException;
+import java.io.Serializable;
 
-public class SerialisationHelper {
+public class SerialisationHelper implements Serializable {
 
 	//see https://bugs.openjdk.java.net/browse/JDK-8201131
 	private void readObject(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException { 
