3f007568ec2d3120937b06f569fd7d49721af87c
Robert Challen <rob@terminological.co.uk>
2019-03-14 17:57:47 +0000

    / ‘nlp-tools/src/main/java/uk/co/terminological/nlptools/words/Description.java’

--- nlp-tools/src/main/java/uk/co/terminological/nlptools/words/Description.java
+++ nlp-tools/src/main/java/uk/co/terminological/nlptools/words/Description.java
@@ -2,22 +2,6 @@ package uk.co.terminological.nlptools.words;
 
 public class Description {
 
-	@Override
-	public String getValue() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public long getIndex() {
-		
-		return 0;
-	}
-
-	@Override
-	public Dictionary<Description, String> getDictionary() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	
 
 }
