c9b64a4f08c8da90de9c8570d438e4a2056bd1f2
Robert Challen <rob@terminological.co.uk>
2019-03-14 17:40:01 +0000

    / ‘nlp-tools/src/main/java/uk/co/terminological/nlptools/words/StringIntMap.java’

--- nlp-tools/src/main/java/uk/co/terminological/nlptools/words/StringIntMap.java
+++ nlp-tools/src/main/java/uk/co/terminological/nlptools/words/StringIntMap.java
@@ -1,5 +1,7 @@
 package uk.co.terminological.nlptools.words;
 
+import com.koloboke.compile.KolobokeMap;
+
 @KolobokeMap
 abstract class StringIntMap {
     static StringIntMap withExpectedSize(int expectedSize) {
