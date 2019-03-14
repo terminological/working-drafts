64a59333c473f6baa6539bab4563d8ca21ab3b71
Robert Challen <rob@terminological.co.uk>
2019-03-14 16:58:17 +0000

    / ‘nlp-tools/src/main/java/uk/co/terminological/nlptools/words/Description.java’ / ‘nlp-tools/src/main/java/uk/co/terminological/nlptools/words/DictionaryEntry.java’ / ‘nlp-tools/src/main/java/uk/co/terminological/nlptools/words/Ngram.java’

--- nlp-tools/src/main/java/uk/co/terminological/nlptools/words/Description.java
+++ nlp-tools/src/main/java/uk/co/terminological/nlptools/words/Description.java
@@ -2,4 +2,16 @@ package uk.co.terminological.nlptools.words;
 
 public class Description implements DictionaryEntry<String> {
 
+	@Override
+	public String getValue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getIndex() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
