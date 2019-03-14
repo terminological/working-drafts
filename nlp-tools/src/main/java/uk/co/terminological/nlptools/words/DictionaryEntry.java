4af94b4afca64cda3e1abacbda95560c884eb50b
Robert Challen <rob@terminological.co.uk>
2019-03-14 16:49:32 +0000

    / ‘nlp-tools/src/main/java/uk/co/terminological/nlptools/words/DictionaryEntry.java’

--- nlp-tools/src/main/java/uk/co/terminological/nlptools/words/DictionaryEntry.java
+++ nlp-tools/src/main/java/uk/co/terminological/nlptools/words/DictionaryEntry.java
@@ -2,4 +2,8 @@ package uk.co.terminological.nlptools.words;
 
 public interface DictionaryEntry<X> {
 
+	public Dictionary<DictionaryEntry<X>> getDictionary();
+	public X getValue();
+	public long getIndex();
+	
 }
