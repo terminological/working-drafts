097880a2b31b7224d29b0ecde7ce4a672cb39b73
Robert Challen <rob@terminological.co.uk>
2019-03-14 17:51:40 +0000

    / ‘nlp-tools/src/main/java/uk/co/terminological/nlptools/words/Ngram.java’

--- nlp-tools/src/main/java/uk/co/terminological/nlptools/words/Ngram.java
+++ nlp-tools/src/main/java/uk/co/terminological/nlptools/words/Ngram.java
@@ -2,9 +2,14 @@ package uk.co.terminological.nlptools.words;
 
 public class Ngram {
 
-	long index;
+	int index;
 	
 	
+	public Ngram(int int1) {
+		index = int1;
+	}
+
+
 	@Override
 	public String getValue(NGramDictionary dict) {
 		// TODO Auto-generated method stub
