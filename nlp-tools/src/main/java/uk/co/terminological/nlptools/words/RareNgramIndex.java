3bf9f7ddf5d254b46a4cc359d65dffcf9bbc7b79
Robert Challen <rob@terminological.co.uk>
2019-03-14 18:34:29 +0000

    / ‘nlp-tools/src/main/java/uk/co/terminological/nlptools/words/RareNgramIndex.java’

--- nlp-tools/src/main/java/uk/co/terminological/nlptools/words/RareNgramIndex.java
+++ nlp-tools/src/main/java/uk/co/terminological/nlptools/words/RareNgramIndex.java
@@ -1,12 +1,21 @@
 package uk.co.terminological.nlptools.words;
 
+import java.util.Map;
+
+import com.koloboke.collect.map.hash.HashIntIntMaps;
+
 public class RareNgramIndex {
 
-	IntIntMap counts = IntIntMap.withExpectedSize(100000);
+	Map<Integer,Integer> counts = HashIntIntMaps.newMutableMap(100000);
 	
 	public void update(Ngram ngram) {
-		// TODO Auto-generated method stub
-		
+		counts.put(
+				ngram.index,
+				counts.getOrDefault(ngram.index, 0)+1);
+	}
+	
+	public void addDescription(Description desc) {
+		//TODO: create ngrams for description 
 	}
 
 }
