3165d719622dca17f29a24f2cd6857ed477e0abd
Robert Challen <rob@terminological.co.uk>
2019-05-03 12:01:29 +0100

    / ‘nlp-tools/src/main/java/uk/co/terminological/nlptools/words/Ngram.java’

--- nlp-tools/src/main/java/uk/co/terminological/nlptools/words/Ngram.java
+++ nlp-tools/src/main/java/uk/co/terminological/nlptools/words/Ngram.java
@@ -17,10 +17,7 @@ public class Ngram {
 
 	@Override
 	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + index;
-		return result;
+		return index;
 	}
 
 
