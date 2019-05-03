b4a080f72b4bd906d5c748dddf11714ee5e19cd9
Robert Challen <rob@terminological.co.uk>
2019-05-03 12:15:14 +0100

    / ‘nlp-tools/src/main/java/uk/co/terminological/nlptools/words/RareNgramIndex.java’

--- nlp-tools/src/main/java/uk/co/terminological/nlptools/words/RareNgramIndex.java
+++ nlp-tools/src/main/java/uk/co/terminological/nlptools/words/RareNgramIndex.java
@@ -2,9 +2,6 @@ package uk.co.terminological.nlptools.words;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-
-import com.koloboke.collect.map.hash.HashIntIntMaps;
 
 public class RareNgramIndex {
 
