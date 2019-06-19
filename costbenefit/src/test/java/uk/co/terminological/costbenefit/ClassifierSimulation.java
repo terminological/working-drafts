abff0d142a18126d04699171906741fb8f2bf9ac
Rob Challen <rob@terminological.co.uk>
2019-06-19 16:22:12 +0100

    / ‘costbenefit/src/test/java/uk/co/terminological/costbenefit/ClassifierSimulation.java’

--- costbenefit/src/test/java/uk/co/terminological/costbenefit/ClassifierSimulation.java
+++ costbenefit/src/test/java/uk/co/terminological/costbenefit/ClassifierSimulation.java
@@ -16,6 +16,7 @@ import org.junit.Test;
 import freemarker.template.TemplateException;
 import uk.co.terminological.simplechart.ChartType;
 import uk.co.terminological.simplechart.Figure;
+import uk.co.terminological.simplechart.Series;
 import uk.co.terminological.simplechart.SeriesBuilder;
 import uk.co.terminological.simplechart.SeriesBuilder.Range;
 
@@ -75,22 +76,23 @@ public class ClassifierSimulation {
 		Range aRange = Range.of(-1D, 1D, 5);
 		Range bRange = Range.of(-1D, 1D, 5);
 		Range xRange = Range.of(0D, 1D, 100);
-		DecimalFormat df = new DecimalFormat("#.00"); 
-		SeriesBuilder.space(aRange,bRange).forEach(arr -> {
-			Double a = arr[0];
-			Double b = arr[1];
-			String title = "a="+df.format(a)+" b="+df.format(b);
-			figures.withNewChart(title, ChartType.XY_LINE)
-			.config().withXScale(-1F, 1F)
+		DecimalFormat df = new DecimalFormat("0.00"); 
+		
+			
+			Series<Double> tmp = figures.withNewChart("plots", ChartType.XY_MULTI_LINE)
+			.config().withXScale(0F, 1F)
 			.withXLabel("x")
 			.withYLabel("kumaraswamy")
 			.withYScale(0F, 1F)
 			.done()
 			.withSeries(SeriesBuilder.range(xRange))
-			.bind(X, t -> t)
-			.bind(Y, Kumaraswamy.fn(a,b))
-			.done()
-			.render();
-		});
+			.bind(X, t -> t);
+			SeriesBuilder.space(aRange,bRange).forEach(arr -> {
+				Double a = arr[0];
+				Double b = arr[1];
+				String title = "a="+df.format(a)+" b="+df.format(b);
+				tmp.bind(Y, Kumaraswamy.fn(a,b),title);
+			});
+			tmp.done().render();
 	}
 }
