24337e2703be527c51a54ba8313b67f38f727b67
Rob Challen <rob@terminological.co.uk>
2019-06-19 13:25:34 +0100

    / ‘costbenefit/src/main/java/uk/co/terminological/costbenefit/GaussianCDF.java’

--- costbenefit/src/main/java/uk/co/terminological/costbenefit/GaussianCDF.java
+++ costbenefit/src/main/java/uk/co/terminological/costbenefit/GaussianCDF.java
@@ -4,15 +4,15 @@ import java.util.function.Function;
 
 import org.apache.commons.math3.analysis.ParametricUnivariateFunction;
 import org.apache.commons.math3.analysis.function.Gaussian;
-import org.apache.commons.math3.analysis.integration.SimpsonIntegrator;
+
 
 public class GaussianCDF implements ParametricUnivariateFunction {
 
-	static SimpsonIntegrator integrator = new SimpsonIntegrator(0.001D, 0.001D,2,20);
+	static SimpsonIntegrator integrator = new SimpsonIntegrator(4);
 	
 	@Override
 	public double value(double x, double... parameters) {
-		return integrator.integrate(20, new Gaussian(parameters[0], parameters[1]), Double.NEGATIVE_INFINITY, x);
+		return integrator.integrate(-100D, x,d -> new Gaussian(parameters[0], parameters[1]).value(d));
 	}
 
 	@Override
