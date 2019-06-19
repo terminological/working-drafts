45bb3aa73cceac8a7e9ae856f131a3b6a2fae122
Rob Challen <rob@terminological.co.uk>
2019-06-19 14:48:11 +0100

    / ‘costbenefit/src/main/java/uk/co/terminological/costbenefit/Analysis.java’ + ‘costbenefit/src/main/java/uk/co/terminological/costbenefit/Coordinate.java’ / ‘costbenefit/src/main/java/uk/co/terminological/costbenefit/CoordinateFinder.java’

--- costbenefit/src/main/java/uk/co/terminological/costbenefit/Analysis.java
+++ costbenefit/src/main/java/uk/co/terminological/costbenefit/Analysis.java
@@ -18,7 +18,6 @@ import org.apache.commons.lang3.StringUtils;
 import com.google.common.collect.Lists;
 
 import freemarker.template.TemplateException;
-import uk.co.terminological.costbenefit.CoordinateFinder.Coordinate;
 import uk.co.terminological.costbenefit.CoordinateFinder.Interceptions;
 import uk.co.terminological.costbenefit.Kumaraswamy.Fitted;
 import uk.co.terminological.datatypes.EavMap;
