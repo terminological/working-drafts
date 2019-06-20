007857c4efd1d1b629c6988a233b4d4f3a192cb2
Rob Challen <rob@terminological.co.uk>
2019-06-20 23:19:19 +0100

    / ‘costbenefit/src/main/java/uk/co/terminological/costbenefit/KumaraswamyCDF.java’

--- costbenefit/src/main/java/uk/co/terminological/costbenefit/KumaraswamyCDF.java
+++ costbenefit/src/main/java/uk/co/terminological/costbenefit/KumaraswamyCDF.java
@@ -59,7 +59,7 @@ public class KumaraswamyCDF implements ParametricUnivariateFunction {
 	}
 	
 	public static Double a(Double spread, Double mode) {
-		return (1+mode+spread)/(1+spread);
+		return (1+2*mode+2*spread)/(1+2*spread);
 	}
 	
 	public static Double b(Double spread, Double mode) {
