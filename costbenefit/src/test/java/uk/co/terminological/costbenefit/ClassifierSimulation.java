04ca9f8e37383cad8be139e9624576e2dc407296
Rob Challen <rob@terminological.co.uk>
2019-06-20 17:24:39 +0100

    / ‘costbenefit/src/test/java/uk/co/terminological/costbenefit/ClassifierSimulation.java’

--- costbenefit/src/test/java/uk/co/terminological/costbenefit/ClassifierSimulation.java
+++ costbenefit/src/test/java/uk/co/terminological/costbenefit/ClassifierSimulation.java
@@ -76,7 +76,7 @@ public class ClassifierSimulation {
 	
 	@Test
 	public void plotKumaraswarmy() {
-		Range spreadRange = Range.of(0.1D, 1D, 8);
+		Range spreadRange = Range.of(0.5D, 1D, 8);
 		//Range bRange = Range.of(2D, 5D, 4);
 		Range modeRange = Range.of(0.1, 0.9, 3);
 		//Double mode = 0.75D;
