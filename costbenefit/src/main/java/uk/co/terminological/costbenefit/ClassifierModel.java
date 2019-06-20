d3571e62c247654a821f6baca1ffd87486d3909d
Rob Challen <rob@terminological.co.uk>
2019-06-21 00:34:33 +0100

    / ‘costbenefit/src/main/java/uk/co/terminological/costbenefit/ClassifierModel.java’ / ‘costbenefit/src/main/java/uk/co/terminological/costbenefit/ConfusionMatrix.java’

--- costbenefit/src/main/java/uk/co/terminological/costbenefit/ClassifierModel.java
+++ costbenefit/src/main/java/uk/co/terminological/costbenefit/ClassifierModel.java
@@ -2,12 +2,24 @@ package uk.co.terminological.costbenefit;
 
 public class ClassifierModel {
 
-	Double spreadPos = 0.5;
-	Double modePos = 0.9;
-	Double spreadNeg = 0.8;
-	Double modeNeg = 0.7;
+	
 	
 	Double prevalence = 0.2;
 	
+	public static class AlwaysPositive extends ClassifierModel {
+		
+	}
 	
+	public static class Kurasawamy extends ClassifierModel {
+		
+		Double spreadPos = 0.5;
+		Double modePos = 0.9;
+		Double spreadNeg = 0.8;
+		Double modeNeg = 0.7;
+		
+	}
+	
+	public static class AlwaysNegative extends ClassifierModel {
+		
+	}
 }
--- costbenefit/src/main/java/uk/co/terminological/costbenefit/ConfusionMatrix.java
+++ costbenefit/src/main/java/uk/co/terminological/costbenefit/ConfusionMatrix.java
@@ -2,28 +2,55 @@ package uk.co.terminological.costbenefit;
 
 public class ConfusionMatrix {
 
-	Double tpr;
-	Double tnr;
-	Double fpr;
-	Double fnr;
+	Double tp;
+	Double tn;
+	Double fp;
+	Double fn;
 	
 	int total;
 	
 	public ConfusionMatrix(int TP, int TN, int FP, int FN) {
 		total = TP+TN+FP+FN;
-		tpr = TP/((double) total);
-		fpr = FP/((double) total);
-		tnr = TN/((double) total);
-		fnr = FN/((double) total);
+		tp = TP/((double) total);
+		fp = FP/((double) total);
+		tn = TN/((double) total);
+		fn = FN/((double) total);
 	}
 	
 	public ConfusionMatrix(double TPR, double TNR, double FPR, double FNR) {
 		total = 1;
 		if (TPR+TNR+FPR+FNR != 1) throw new ConstraintViolationException("Sum of paramters must be 1");
-		tpr = TPR;
-		fpr = FPR;
-		tnr = TNR;
-		fnr = FNR;
+		tp = TPR;
+		fp = FPR;
+		tn = TNR;
+		fn = FNR;
 	}
+	
+	public Double sensitivity() {return truePositiveRate();}
+	public Double specificity() {return trueNegativeRate();}
+	
+	
+	public Double recall() {return truePositiveRate();}
+	public Double precision() {return positivePredictiveValue();}
+	
+	
+	public Double truePositiveRate() {return tp/(tp+fn);}
+	public Double trueNegativeRate() {return tn/(tn+fp);}
+	public Double falseNegativeRate() {return fn/(fn+tp);}
+	public Double falsePositiveRate() {return fp/(fp+tn);}
+	
+	public Double positivePredictiveValue() {return tp/(tp+fp);}
+	public Double negativePredictiveValue() {return tn/(tn+fn);}
+	public Double falseDiscoveryRate() {return fp/(fp+tp);}
+	public Double falseOmissionRate() {return fn/(fn+tn);}
+	
+	public Double accuracy() {return (tp+tn)/total;}
+	public Double matthewsCorrelationCoefficient() {return (tp*tn-fp*fn)/Math.sqrt((tp+fp)*(tp+fn)*(tn+fp)*(tn+fn));}
+	public Double fScore(Double beta) {
+		Double b2 = Math.pow(beta, 2);
+		return ((1+b2)*tp)/((1+b2)*tp+b2*fn+fp);
+	}
+	public Double f1Score() {return fScore(1D);}
+	public Double youdensJ() {return truePositiveRate()+trueNegativeRate()-1;}
 	
 }
