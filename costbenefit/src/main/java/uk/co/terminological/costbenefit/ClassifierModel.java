e15b1ff284f18a21026631b46d92f2e1b3c718cf
Rob Challen <rob@terminological.co.uk>
2019-06-21 00:47:59 +0100

    / ‘costbenefit/src/main/java/uk/co/terminological/costbenefit/ClassifierModel.java’

--- costbenefit/src/main/java/uk/co/terminological/costbenefit/ClassifierModel.java
+++ costbenefit/src/main/java/uk/co/terminological/costbenefit/ClassifierModel.java
@@ -1,25 +1,41 @@
 package uk.co.terminological.costbenefit;
 
-public class ClassifierModel {
+public abstract class ClassifierModel<X> {
 
-	
+	public abstract ConfusionMatrix matrix(X param);
 	
 	Double prevalence = 0.2;
 	
-	public static class AlwaysPositive extends ClassifierModel {
+	public static class AlwaysPositive extends ClassifierModel<Void> {
+
+		@Override
+		public ConfusionMatrix matrix(Void param) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
 		
 	}
 	
-	public static class Kurasawamy extends ClassifierModel {
+	public static class Kurasawamy extends ClassifierModel<Double> {
 		
 		Double spreadPos = 0.5;
 		Double modePos = 0.9;
 		Double spreadNeg = 0.8;
 		Double modeNeg = 0.7;
 		
+		public ConfusionMatrix matrix(Double cutoff) {
+			return null;
+		}
 	}
 	
-	public static class AlwaysNegative extends ClassifierModel {
+	public static class AlwaysNegative extends ClassifierModel<Void> {
+
+		@Override
+		public ConfusionMatrix matrix(Void param) {
+			// TODO Auto-generated method stub
+			return null;
+		}
 		
 	}
 }
