3a4979e80d14fb2421579fec21a693aa4077c902
Rob Challen <rob@terminological.co.uk>
2019-06-21 09:44:22 +0100

    / ‘costbenefit/src/main/java/uk/co/terminological/costbenefit/ClassifierModel.java’

--- costbenefit/src/main/java/uk/co/terminological/costbenefit/ClassifierModel.java
+++ costbenefit/src/main/java/uk/co/terminological/costbenefit/ClassifierModel.java
@@ -10,22 +10,39 @@ public abstract class ClassifierModel<X> {
 
 		@Override
 		public ConfusionMatrix matrix(Void param) {
-			// TODO Auto-generated method stub
-			return null;
+			
+			Double tp = prevalence;
+			Double tn = 0D;
+			Double fp = 1-prevalence;
+			Double fn = 0D;
+			
+			return new ConfusionMatrix(tp,tn,fp,fn);
 		}
 
 		
 	}
 	
-	public static class Kurasawamy extends ClassifierModel<Double> {
+	public static class Kumaraswamy extends ClassifierModel<Double> {
 		
 		Double spreadPos = 0.5;
 		Double modePos = 0.9;
 		Double spreadNeg = 0.8;
 		Double modeNeg = 0.7;
+		Double prev = 0.1;
 		
 		public ConfusionMatrix matrix(Double cutoff) {
-			return null;
+			
+			Double cdfPos = KumaraswamyCDF.cdf(KumaraswamyCDF.a(spreadPos, modePos),KumaraswamyCDF.a(spreadPos, modePos)).apply(cutoff);
+			Double cdfNeg = KumaraswamyCDF.cdf(KumaraswamyCDF.a(spreadNeg, modeNeg),KumaraswamyCDF.a(spreadNeg, modeNeg)).apply(cutoff);
+			
+			Double eTp = prev*(1 - cdfPos);
+			Double eTn = (1-prev)*cdfNeg;
+			Double eFp = prev*cdfPos;
+			Double eFn = (1-prev)*1-cdfPos;
+			
+			//TODO: prevalence is independent of cutoff - this should be correct. Classifier model accounts for prevalence.
+			
+			return new ConfusionMatrix(eTp,eTn,eFp,eFn);
 		}
 	}
 	
@@ -33,9 +50,13 @@ public abstract class ClassifierModel<X> {
 
 		@Override
 		public ConfusionMatrix matrix(Void param) {
-			// TODO Auto-generated method stub
-			return null;
+			
+			Double tp = 0D;
+			Double tn = 1-prevalence;
+			Double fp = 0D;
+			Double fn = prevalence;
+			
+			return new ConfusionMatrix(tp,tn,fp,fn);
 		}
-		
 	}
 }
