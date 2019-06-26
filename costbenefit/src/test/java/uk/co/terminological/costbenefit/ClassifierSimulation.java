6695945c819dc5679a8da113c871fec9a95219f1
Rob Challen <rob@terminological.co.uk>
2019-06-26 19:15:45 +0100

    / ‘costbenefit/Rplots.pdf’ / ‘costbenefit/src/test/java/uk/co/terminological/costbenefit/ClassifierSimulation.java’

--- costbenefit/src/test/java/uk/co/terminological/costbenefit/ClassifierSimulation.java
+++ costbenefit/src/test/java/uk/co/terminological/costbenefit/ClassifierSimulation.java
@@ -176,30 +176,31 @@ public class ClassifierSimulation {
 	@Test
 	public void plotValue() {
 		Stream.of(ClassifierConfigEnum.values()).forEach( c-> {
-			Stream.of(CostModelEnum.values()).forEach( cm-> {
-		ParameterSet defaults = new ParameterSet(0.1,c,cm,null);
-		ParameterSpace space = new ParameterSpace(defaults);
-		space.cutOff = SeriesBuilder.range(0.0, 1.0, 100).collect(Collectors.toList());
-		space.prevalence = SeriesBuilder.range(0.005,0.995,0.01).collect(Collectors.toList());
-		figures.withNewChart(c+" "+cm+" value", ChartType.XYZ_HEATMAP)
-				.config().withXScale(0F, 1F)
-				.withXLabel("cutoff")
-				.withYLabel("prevalence")
-				.withLabel(Z, "value")
-				.withYScale(0F, 1F)
-				.done()
-				.withSeries(space.stream()).withColourScheme(ColourScheme.Greens)
-				.bind(X, t -> t.cutOff)
-				//.bind(Y, t -> t.matrix().tp,"tp")
-				//.bind(Y, t -> t.matrix().tn,"tn")
-				//.bind(Y, t -> t.matrix().fp,"fp")
-				//.bind(Y, t -> t.matrix().fn,"fn")
-				.bind(Z, t -> t.matrix().relativeValue(),"value")
-				.bind(Y, t -> t.prevalence,"prevalence")
-				.done()
-				.render();
-			});
-		});
+			//Stream.of(CostModelEnum.values()).forEach( cm-> {
+				CostModelEnum cm = CostModelEnum.CANCER_IS_UNTREATABLE;
+			ParameterSet defaults = new ParameterSet(0.1,c,cm,null);
+			ParameterSpace space = new ParameterSpace(defaults);
+			space.cutOff = SeriesBuilder.range(0.0, 1.0, 100).collect(Collectors.toList());
+			space.prevalence = SeriesBuilder.range(0.005,0.995,0.01).collect(Collectors.toList());
+			figures.withNewChart(c+" "+cm+" value", ChartType.XYZ_HEATMAP)
+					.config().withXScale(0F, 1F)
+					.withXLabel("cutoff")
+					.withYLabel("prevalence")
+					.withLabel(Z, "value")
+					.withYScale(0F, 1F)
+					.done()
+					.withSeries(space.stream()).withColourScheme(ColourScheme.Greens)
+					.bind(X, t -> t.cutOff)
+					//.bind(Y, t -> t.matrix().tp,"tp")
+					//.bind(Y, t -> t.matrix().tn,"tn")
+					//.bind(Y, t -> t.matrix().fp,"fp")
+					//.bind(Y, t -> t.matrix().fn,"fn")
+					.bind(Z, t -> t.matrix().relativeValue(),"value")
+					.bind(Y, t -> t.prevalence,"prevalence")
+					.done()
+					.render();
+				});
+		//});
 	}
 	
 	@Test
