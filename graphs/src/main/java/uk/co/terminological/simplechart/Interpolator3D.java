eac901fa571b73eb4de6b3715f16705e9e74ee0b
Rob Challen <rob@terminological.co.uk>
2019-06-26 09:32:21 +0100

    / ‘graphs/src/main/java/uk/co/terminological/simplechart/Interpolator3D.java’

--- graphs/src/main/java/uk/co/terminological/simplechart/Interpolator3D.java
+++ graphs/src/main/java/uk/co/terminological/simplechart/Interpolator3D.java
@@ -1,5 +1,71 @@
 package uk.co.terminological.simplechart;
 
-public class Interpolator3D {
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+import java.util.stream.Stream;
 
+import uk.co.terminological.datatypes.EavMap;
+
+public class Interpolator3D<IN> implements Collector<IN, EavMap<Double,Double,Double>, Result> {
+
+	EavMap<Double,Double,Double> coords = new EavMap<>();
+	Function<IN,Double> xAdaptor;
+	Function<IN,Double> yAdaptor;
+	Function<IN,Double> zAdaptor;
+	
+	
+	public Interpolator3D(Function<IN,Double> xAdaptor, Function<IN,Double> yAdaptor, Function<IN,Double> zAdaptor ) {
+		this.xAdaptor = xAdaptor;
+		this.yAdaptor = yAdaptor;
+		this.zAdaptor = zAdaptor;
+	}
+	
+	public static <X> Result fromStream(Stream<X> input, Function<X,Double> xAdaptor, Function<X,Double> yAdaptor, Function<X,Double> zAdaptor) {
+		return input.collect(new Interpolator3D(xAdaptor,yAdaptor,zAdaptor));
+	}
+	
+	
+	public static class Result {
+		
+	}
+
+
+	@Override
+	public Supplier<EavMap<Double, Double, Double>> supplier() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public BiConsumer<EavMap<Double, Double, Double>, IN> accumulator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public BinaryOperator<EavMap<Double, Double, Double>> combiner() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public Function<EavMap<Double, Double, Double>, Result> finisher() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public Set<Characteristics> characteristics() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
