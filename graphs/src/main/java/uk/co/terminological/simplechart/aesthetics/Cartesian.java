a703337975c6341a57e006d650ffabb9e85b0b3f
Rob Challen <rob@terminological.co.uk>
2019-06-26 15:00:00 +0100

    / ‘graphs/src/main/java/uk/co/terminological/simplechart/aesthetics/Cartesian.java’ + ‘graphs/src/main/java/uk/co/terminological/simplechart/aesthetics/Y.java’

--- graphs/src/main/java/uk/co/terminological/simplechart/aesthetics/Cartesian.java
+++ graphs/src/main/java/uk/co/terminological/simplechart/aesthetics/Cartesian.java
@@ -2,10 +2,6 @@ package uk.co.terminological.simplechart.aesthetics;
 
 public interface Cartesian {
 	
-	public static interface Y {
-		public Double getY();
-	}
-	
 	public static interface Z {
 		public Double getZ();
 	}
