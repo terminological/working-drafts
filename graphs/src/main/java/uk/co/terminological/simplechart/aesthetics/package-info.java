f08fe40e0ab602cbda097d6b354d0b09136d5f6e
Rob Challen <rob@terminological.co.uk>
2019-06-26 14:48:45 +0100

    + ‘graphs/src/main/java/uk/co/terminological/simplechart/aesthetics/package-info.java’

---
+++ graphs/src/main/java/uk/co/terminological/simplechart/aesthetics/package-info.java
@@ -0,0 +1,13 @@
+@Model(
+		builtins = { 
+				BuiltIn.IMPL,
+				BuiltIn.MIRROR,
+				BuiltIn.VISITOR,
+				BuiltIn.FLUENT,
+				BuiltIn.FLUENT_IMPL,
+				BuiltIn.FACTORY,
+				BuiltIn.DOTUML,
+				BuiltIn.DEBUG
+		})
+package uk.co.terminological.javapig.test.model;
+import uk.co.terminological.javapig.annotations.Model;
