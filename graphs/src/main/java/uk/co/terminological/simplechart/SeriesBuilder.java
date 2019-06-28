10e9ebb8d1e9544e465b5d683a833d873a53738e
Robert Challen <rob@terminological.co.uk>
2019-06-28 09:54:31 +0100

    / ‘graphs/src/main/java/uk/co/terminological/simplechart/SeriesBuilder.java’

--- graphs/src/main/java/uk/co/terminological/simplechart/SeriesBuilder.java
+++ graphs/src/main/java/uk/co/terminological/simplechart/SeriesBuilder.java
@@ -107,7 +107,7 @@ public class SeriesBuilder<X> {
 		}
 		
 		public static Range of(Double from, Double to, Long values) {
-			return new Range(from,to,Math.abs(to-from)/values);
+			return new Range(from,to,(to-from)/values);
 		}
 		
 		public static Range of(Double from, Double to, Integer values) {
