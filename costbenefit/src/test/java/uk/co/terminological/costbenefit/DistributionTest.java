870f15cc22100e09c98e5f822fb8aff41f54f3b6
Robert Challen <rob@terminological.co.uk>
2019-06-28 09:15:03 +0100

    + ‘costbenefit/src/test/java/uk/co/terminological/costbenefit/DistributionTest.java’

---
+++ costbenefit/src/test/java/uk/co/terminological/costbenefit/DistributionTest.java
@@ -0,0 +1,14 @@
+package uk.co.terminological.costbenefit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DistributionTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
