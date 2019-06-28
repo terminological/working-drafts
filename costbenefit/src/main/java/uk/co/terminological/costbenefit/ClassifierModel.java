50311a39ca0a9f248c59db101346794611055ce3
Rob Challen <rob@terminological.co.uk>
2019-06-28 22:52:04 +0100

    / ‘costbenefit/src/main/java/uk/co/terminological/costbenefit/ClassifierModel.java’

--- costbenefit/src/main/java/uk/co/terminological/costbenefit/ClassifierModel.java
+++ costbenefit/src/main/java/uk/co/terminological/costbenefit/ClassifierModel.java
@@ -1,10 +1,7 @@
 package uk.co.terminological.costbenefit;
 
-import java.util.Collections;
-import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.apache.commons.math3.util.Precision;
 
@@ -98,10 +95,11 @@ public abstract class ClassifierModel<X> {
 		}
 		
 		public Double AUROC() {
-			return TrapeziodIntegrator.integrate(
-			SeriesBuilder.range(0.0, 1.0, 1000).map(c -> matrix(c)).map(m -> Tuple.create(m.sensitivity(), m.specificity()))
-				.collect(Collectors.toList())
-				);
+			return 
+			SeriesBuilder.range(0.0, 1.0, 1000)
+				.map(c -> matrix(c))
+				.map(m -> Tuple.create(m.sensitivity(), m.specificity()))
+				.collect(TrapeziodIntegrator.integrator());
 		}
 		
 		public Double KLDivergence() {
@@ -137,198 +135,6 @@ public abstract class ClassifierModel<X> {
 			Double fn = prev;
 			
 			return new ConfusionMatrix2D(tp,tn,fp,fn);
-		}
-	}
-	
-	public static class ParameterSpace {
-		public ParameterSpace(ParameterSet defaults) {
-			this.prevalence = Collections.singletonList(defaults.prevalence);
-			this.centralityIfPositive = Collections.singletonList(defaults.centralityIfPositive);
-			this.spreadIfPositive = Collections.singletonList(defaults.spreadIfPositive);
-			this.centralityIfNegative = Collections.singletonList(defaults.centralityIfNegative);
-			this.spreadIfNegative = Collections.singletonList(defaults.spreadIfNegative);
-			this.tpValue = Collections.singletonList(defaults.tpValue);
-			this.tnValue = Collections.singletonList(defaults.tnValue);
-			this.fpCost = Collections.singletonList(defaults.fpCost);
-			this.fnCost = Collections.singletonList(defaults.fnCost);
-			this.cutOff = Collections.singletonList(defaults.cutOff);
-		}
-
-
-
-		List<Double> prevalence;
-		List<Double> centralityIfPositive;
-		List<Double> spreadIfPositive;
-		List<Double> centralityIfNegative;
-		List<Double> spreadIfNegative;
-		List<Double> tpValue;
-		List<Double> tnValue;
-		List<Double> fpCost;
-		List<Double> fnCost;
-		List<Double> cutOff;
-		
-		
-		
-		public Stream<ParameterSet> stream() {
-			ParameterSet tmp = new ParameterSet();
-			return prevalence.stream().flatMap(p -> {
-				tmp.prevalence = p;
-				return centralityIfPositive.stream(); //TODO: gets consumed.
-			}).flatMap(cPos -> {
-				tmp.centralityIfPositive = cPos;
-				return spreadIfPositive.stream();
-			}).flatMap(sPos -> {
-				tmp.spreadIfPositive = sPos;
-				return centralityIfNegative.stream();
-			}).flatMap(cNeg -> {
-				tmp.centralityIfNegative = cNeg;
-				return spreadIfNegative.stream();
-			}).flatMap(sNeg -> {
-				tmp.spreadIfNegative = sNeg;
-				return tpValue.stream();
-			}).flatMap(tpVal -> {
-				tmp.tpValue = tpVal;
-				return tnValue.stream();
-			}).flatMap(tnVal -> {
-				tmp.tnValue = tnVal;
-				return fpCost.stream();
-			}).flatMap(fpC -> {
-				tmp.fpCost = fpC;
-				return fnCost.stream();
-			}).flatMap(fnC -> {
-				tmp.fnCost = fnC;
-				return cutOff.stream();
-			}).map(co -> {
-				tmp.cutOff = co;
-				return tmp.clone();
-			});
-		}
-	}
-	
-	public static interface ClassifierConfig {
-		Double centralityIfPositive(); 
-		Double spreadIfPositive();
-		Double centralityIfNegative(); 
-		Double spreadIfNegative();
-	}
-	
-	public static enum ClassifierConfigEnum implements ClassifierConfig {
-		HIGH_INFORMATION(0.8,0.1,0.2,0.1),
-		MID_INFORMATION(0.7,0.3,0.3,0.3),
-		LOW_INFORMATION(0.6,0.5,0.4,0.5),
-		;
-		
-		private ClassifierConfigEnum(Double centralityIfPositive, Double spreadIfPositive, Double centralityIfNegative,	Double spreadIfNegative) {
-			this.centralityIfPositive = centralityIfPositive;
-			this.spreadIfPositive = spreadIfPositive;
-			this.centralityIfNegative = centralityIfNegative;
-			this.spreadIfNegative = spreadIfNegative;
-		}
-		
-		Double centralityIfPositive;
-		Double spreadIfPositive;
-		Double centralityIfNegative;
-		Double spreadIfNegative;
-		
-		@Override public Double centralityIfPositive() {return centralityIfPositive;}
-		@Override public Double spreadIfPositive() {return spreadIfPositive;}
-		@Override public Double centralityIfNegative() {return centralityIfNegative;}
-		@Override public Double spreadIfNegative() {return centralityIfNegative;}
-		
-		public String toString() { return this.name().toLowerCase().replace("_", " "); }
-		
-	}
-	
-	public static interface CostModel {
-		Double tpValue(); 
-		Double tnValue();
-		Double fpCost();
-		Double fnCost();
-	}
-	
-	public static enum CostModelEnum implements CostModel {
-		EARLY_STAGE_CANCER(10.0,1.0,-0.1,-100.0),
-		//CANCER_IS_UNTREATABLE(2.0,10.0,-10.0,-0.5),
-		DIABETES(4.0,0.1,-1.0,-0.1),
-		SEPSIS(10.0,0.0,0.0,-5.0),
-		//EARLY_STAGE_DEMENTIA(2.0,5.0,-2.0,-1.0),
-		//NON_ACCIDENTAL_INJURY(100.0,1.0,-20.0,-20.0),
-		//IMMINENT_END_OF_LIFE(2.0,0.0,-1.0,-0.1),
-		ENDOSCOPY_UNINFORMATIVE(2.0,0.1,-1.0,-2.0)
-		;
-		
-		private CostModelEnum(Double tpValue, Double tnValue, Double fpCost, Double fnCost) {
-			this.tpValue = tpValue;
-			this.tnValue = tnValue;
-			this.fpCost = fpCost;
-			this.fnCost = fnCost;
-		}
-		
-		Double tpValue;
-		Double tnValue;
-		Double fpCost;
-		Double fnCost;
-		
-		@Override public Double tpValue() {return tpValue;}
-		@Override public Double tnValue() {return tnValue;}
-		@Override public Double fpCost() {return fpCost;}
-		@Override public Double fnCost() {return fpCost;}
-		
-		public String toString() { return this.name().toLowerCase().replace("_", " "); }
-		
-	}
-	
-	public static class ParameterSet {
-		public ParameterSet(Double prevalence, Double centralityIfPositive, Double spreadIfPositive,
-				Double centralityIfNegative, Double spreadIfNegative, Double tpValue, Double tnValue, Double fpCost,
-				Double fnCost, Double cutOff) {
-			this.prevalence = prevalence;
-			this.centralityIfPositive = centralityIfPositive;
-			this.spreadIfPositive = spreadIfPositive;
-			this.centralityIfNegative = centralityIfNegative;
-			this.spreadIfNegative = spreadIfNegative;
-			this.tpValue = tpValue;
-			this.tnValue = tnValue;
-			this.fpCost = fpCost;
-			this.fnCost = fnCost;
-			this.cutOff = cutOff;
-		}
-		public ParameterSet() {}
-		public ParameterSet(Double prevalence, ClassifierConfig classifier, CostModel cost, Double cutOff) {
-			this(prevalence,
-					classifier.centralityIfPositive(),
-					classifier.spreadIfPositive(),
-					classifier.centralityIfNegative(),
-					classifier.spreadIfNegative(),
-					cost.tpValue(),
-					cost.tnValue(),
-					cost.fpCost(),
-					cost.fnCost(),
-					cutOff);
-		}
-
-		Double prevalence;
-		Double centralityIfPositive;
-		Double spreadIfPositive;
-		Double centralityIfNegative;
-		Double spreadIfNegative;
-		Double tpValue;
-		Double tnValue;
-		Double fpCost;
-		Double fnCost;
-		Double cutOff;
-		
-		public ParameterSet clone() {
-			return new ParameterSet(prevalence, centralityIfPositive, spreadIfPositive, centralityIfNegative, 
-					spreadIfNegative, tpValue, tnValue, fpCost, fnCost, cutOff);
-		}
-		
-		public Kumaraswamy model() {
-			return new Kumaraswamy(centralityIfPositive, spreadIfPositive, centralityIfNegative, spreadIfNegative, prevalence);
-		}
-		
-		public ConfusionMatrix2D matrix() {
-			return model().matrix(cutOff);
 		}
 	}
 	
