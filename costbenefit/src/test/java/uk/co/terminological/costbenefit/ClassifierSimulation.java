33a54ae1bb0263b503812e1546dcfcf17b76e8f0
Robert Challen <rob@terminological.co.uk>
2019-06-28 09:18:05 +0100

    / ‘costbenefit/src/test/java/uk/co/terminological/costbenefit/ClassifierSimulation.java’ / ‘costbenefit/src/test/java/uk/co/terminological/costbenefit/DistributionTest.java’

--- costbenefit/src/test/java/uk/co/terminological/costbenefit/ClassifierSimulation.java
+++ costbenefit/src/test/java/uk/co/terminological/costbenefit/ClassifierSimulation.java
@@ -52,40 +52,7 @@ public class ClassifierSimulation {
 		figures = Figure.outputTo(dir);
 	}
 
-	@Test
-	public void test123() throws IOException, TemplateException {
-		figures.withNewChart("normal", ChartType.XY_LINE)
-		.config().withXScale(-1F, 1F)
-		.withXLabel("x")
-		.withYLabel("gauss")
-		.withYScale(0F, 1F)
-		.done()
-		.withSeries(SeriesBuilder.range(-1D, 1D, 1000))
-		.bind(X, t -> t)
-		.bind(Y, GaussianCDF.fn(0, 1))
-		.done()
-		.render();
-	}
-
-	@Test
-	public void testSeries() {
-		SeriesBuilder.range(-1D, 1D, 1000).forEach(System.out::println);
-	}
 	
-	@Test
-	public void testCoords() {
-		SeriesBuilder.grid(-1D, 1D, 0D, 1D, 50).forEach(System.out::println);
-	}
-	
-	@Test
-	public void testSpace() {
-		SeriesBuilder.space(
-				Range.of(-1.0, 1.0, 3),
-				Range.of(-1D, 1D, 5),
-				Range.of(-1D, 1D, 7),
-				Range.of(-1D, 1D, 9)
-				).map(l -> Arrays.toString(l)).forEach(System.out::println);
-	}
 	
 	@Test
 	public void plotKumaraswarmy() {
--- costbenefit/src/test/java/uk/co/terminological/costbenefit/DistributionTest.java
+++ costbenefit/src/test/java/uk/co/terminological/costbenefit/DistributionTest.java
@@ -1,14 +1,80 @@
 package uk.co.terminological.costbenefit;
 
 import static org.junit.Assert.*;
+import static uk.co.terminological.simplechart.Chart.Dimension.X;
+import static uk.co.terminological.simplechart.Chart.Dimension.Y;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+import org.apache.log4j.BasicConfigurator;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import freemarker.template.TemplateException;
+import uk.co.terminological.simplechart.ChartType;
+import uk.co.terminological.simplechart.Figure;
+import uk.co.terminological.simplechart.SeriesBuilder;
+import uk.co.terminological.simplechart.SeriesBuilder.Range;
 
 public class DistributionTest {
 
+	Figure figures;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() {
+		BasicConfigurator.configure();
+	}
+	
+	@Before
+	public void setUp() throws Exception {
+		Path dir = Paths.get(System.getProperty("user.home"),"tmp/distTest");
+		Files.createDirectories(dir);
+		figures = Figure.outputTo(dir);
+	}
+	
 	@Test
 	public void test() {
 		fail("Not yet implemented");
 	}
 
+	
+	@Test
+	public void test123() throws IOException, TemplateException {
+		figures.withNewChart("normal", ChartType.XY_LINE)
+		.config().withXScale(-1F, 1F)
+		.withXLabel("x")
+		.withYLabel("gauss")
+		.withYScale(0F, 1F)
+		.done()
+		.withSeries(SeriesBuilder.range(-1D, 1D, 1000))
+		.bind(X, t -> t)
+		.bind(Y, GaussianCDF.fn(0, 1))
+		.done()
+		.render();
+	}
+
+	@Test
+	public void testSeries() {
+		SeriesBuilder.range(-1D, 1D, 1000).forEach(System.out::println);
+	}
+	
+	@Test
+	public void testCoords() {
+		SeriesBuilder.grid(-1D, 1D, 0D, 1D, 50).forEach(System.out::println);
+	}
+	
+	@Test
+	public void testSpace() {
+		SeriesBuilder.space(
+				Range.of(-1.0, 1.0, 3),
+				Range.of(-1D, 1D, 5),
+				Range.of(-1D, 1D, 7),
+				Range.of(-1D, 1D, 9)
+				).map(l -> Arrays.toString(l)).forEach(System.out::println);
+	}
 }
