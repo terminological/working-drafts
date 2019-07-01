7c216893b8caf127655a8161a49761a8a6d1d3ea
Rob Challen <rob@terminological.co.uk>
2019-07-01 20:29:56 +0100

    / ‘costbenefit/src/test/java/uk/co/terminological/costbenefit/ClassifierSimulation.java’

--- costbenefit/src/test/java/uk/co/terminological/costbenefit/ClassifierSimulation.java
+++ costbenefit/src/test/java/uk/co/terminological/costbenefit/ClassifierSimulation.java
@@ -105,7 +105,7 @@ public class ClassifierSimulation {
 		
 		data = SeriesBuilder.grid(
 				//Stream<List<Double>> data = SeriesBuilder.grid(
-					Range.of(0D, 0.8D, 0.005D),Range.of(-0.5D, 0.5D, 0.005D)
+					Range.of(0D, 0.8D, 0.01D),Range.of(-0.5D, 0.5D, 0.01D)
 				).map( c-> 
 					Triple.create(c.getFirst(), c.getSecond(), new Kumaraswamy(c.getFirst(), c.getSecond(), ""))
 					/*FluentList.create(c.getFirst(), c.getSecond(), 
