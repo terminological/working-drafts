ff7cae93fce89001a5e0968405e4c3378c658105
Rob Challen <rob@terminological.co.uk>
2019-07-01 09:25:55 +0100

    / ‘costbenefit/src/main/java/uk/co/terminological/costbenefit/CostModel.java’

--- costbenefit/src/main/java/uk/co/terminological/costbenefit/CostModel.java
+++ costbenefit/src/main/java/uk/co/terminological/costbenefit/CostModel.java
@@ -1,6 +1,9 @@
 package uk.co.terminological.costbenefit;
 
+import java.util.Optional;
+
 import uk.co.terminological.costbenefit.ClassifierModel.Kumaraswamy;
+import uk.co.terminological.simplechart.SeriesBuilder;
 
 public interface CostModel {
 	Double tpValue(); 
@@ -8,7 +11,15 @@ public interface CostModel {
 	Double fpCost();
 	Double fnCost();
 	
-	default Kumaraswamy minimumNeeded() {
-		
+	default Optional<Kumaraswamy> minimumNeeded(Double prev) {
+		for (Double d=0D;d<=1.0;d+=0.001) {
+			Kumaraswamy k = new Kumaraswamy(d,"min");
+			Double maxValue = k.bestCutoff(prev, m -> m.normalisedValue(this)).getSecond();
+			if (maxValue>0) return Optional.of(k);
+		}
+		return Optional.empty();
+		//TODO: this just looks for the answer in the middle of a balanced classifier.
+		// Need to explore the space somehow.
+		// Possibly start in the 
 	}
 }
