1aa3fe0416ec0cb1d6175545a6b2c2f0e605b3d3
Robert Challen <rob@terminological.co.uk>
2019-07-25 16:57:14 +0100

    / ‘costbenefit/src/main/java/uk/co/terminological/costbenefit/ClassifierModel.java’

--- costbenefit/src/main/java/uk/co/terminological/costbenefit/ClassifierModel.java
+++ costbenefit/src/main/java/uk/co/terminological/costbenefit/ClassifierModel.java
@@ -182,7 +182,7 @@ public abstract class ClassifierModel<X> {
 			.done()
 			.withSeries(SeriesBuilder.range(0D, 1D, 1000)).withColourScheme(ColourScheme.Dark2)
 			.bind(X, t -> 1-matrix(0.5,t).sensitivity())
-			.bind(Y_LINE, t -> matrix(0.5,t).specificity())
+			.bind(Y_LINE, t -> matrix(0.5,t).specificity(),"spec")
 			.done()
 			.render();
 	
