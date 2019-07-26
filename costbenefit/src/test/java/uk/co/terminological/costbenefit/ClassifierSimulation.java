9649d08d1900bc2ce339f7fe8bcc6599616f7b19
Rob Challen <rob@terminological.co.uk>
2019-07-26 13:32:34 +0100

    / ‘costbenefit/src/test/java/uk/co/terminological/costbenefit/ClassifierSimulation.java’

--- costbenefit/src/test/java/uk/co/terminological/costbenefit/ClassifierSimulation.java
+++ costbenefit/src/test/java/uk/co/terminological/costbenefit/ClassifierSimulation.java
@@ -247,7 +247,7 @@ public class ClassifierSimulation {
 			ParameterSpace space = new ParameterSpace(defaults);
 			space.cutOff = SeriesBuilder.range(0.0, 1.0, 100).collect(Collectors.toList());
 			space.prevalence = SeriesBuilder.range(0.005,0.995,0.01).collect(Collectors.toList());
-			figures.withNewChart(c+" "+cm+" value", ChartType.XYZ_HEATMAP)
+			figures.withNewChart(c.nickname()+":"+cm.nickname(), ChartType.XYZ_HEATMAP)
 					.config().withXScale(0F, 1F)
 					.withXLabel("cutoff")
 					.withYLabel("prevalence")
