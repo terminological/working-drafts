6490dfa3d901e2c7df6c1123be80dcf321cb74ba
Rob Challen <rob@terminological.co.uk>
2019-08-02 08:57:06 +0100

    / ‘graphs/src/main/java/uk/co/terminological/simplechart/GgplotWriter2.java’

--- graphs/src/main/java/uk/co/terminological/simplechart/GgplotWriter2.java
+++ graphs/src/main/java/uk/co/terminological/simplechart/GgplotWriter2.java
@@ -1,13 +1,16 @@
 package uk.co.terminological.simplechart;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.lang.ProcessBuilder.Redirect;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -46,14 +49,47 @@ public abstract class GgplotWriter2 extends Writer {
 	
 	private <X> String extractData(Series<X> series) {
 		
-		StringBuilder dfConstruct = new StringBuilder();
+		Set<String> names = 
+				series.getBindings().stream()
+				.filter(t -> !t.value().equals(getChart().config.getLabel(t.entity())))
+				.map(t -> t.value())
+				.collect(Collectors.toSet());
+		
+		int index = this.getChart().getSeries().indexOf(series);
+		Path tsv =  this.getChart().getFile(""+index+".tsv").toPath();
+		
+		try {
+			BufferedWriter w = Files.newBufferedWriter(tsv);
+			// write headers
+			w.append(series.getBindings().stream().map(t -> t.entity().name()).collect(Collectors.joining("\t"))+"\tNAME\n");
+			series.getData().forEach(x -> {
+				
+				names.stream().sorted().forEach(name -> {
+					
+					String line = series.getBindings().stream()
+						.filter(t -> t.value() == getChart().config.getLabel(dim) || t.value().equals(name))
+						.map(t -> t.attribute().apply(x))
+						.map(GgplotWriter2::format)
+						.collect(Collectors.joining("\t"));
+					
+					line = line+"\t"+name;
+				
+				try {
+					w.append(line+"\n");
+				} catch (IOException e) {
+					new RuntimeException(e);
+				}
+			});
+			});
+			w.close();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		
+		/*StringBuilder dfConstruct = new StringBuilder();
 		StringBuilder vectors = new StringBuilder();
 		StringBuilder nameVec = new StringBuilder();
 		
-		Set<String> names = series.getBindings().stream()
-				.filter(t -> !t.value().equals(getChart().config.getLabel(t.entity())))
-				.map(t -> t.value()).collect(Collectors.toSet());
-		Set<Dimension> dimensions = series.getBindings().stream().map(t -> t.entity()).collect(Collectors.toSet());
 		
 		int size = series.getData().size() * (names.size()==0?1:names.size());
 		
@@ -97,7 +133,8 @@ public abstract class GgplotWriter2 extends Writer {
 		
 		dfConstruct.append(",NAME=name,stringsAsFactors = FALSE);\n");
 		
-		return vectors.toString()+"\n"+nameVec.toString()+"\n"+dfConstruct.toString();
+		return vectors.toString()+"\n"+nameVec.toString()+"\n"+dfConstruct.toString();*/
+		return "df <- read_tsv(\""+tsv.toAbsolutePath()+"\")";
 	}
 
 	//TODO: localDate support?
