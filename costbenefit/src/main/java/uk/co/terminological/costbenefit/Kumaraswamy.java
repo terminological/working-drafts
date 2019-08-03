0e0cf3a9d0cad37ebc98eef790368ab278a64df0
Rob Challen <rob@terminological.co.uk>
2019-08-03 08:05:14 +0100

    / ‘costbenefit/src/main/java/uk/co/terminological/costbenefit/Kumaraswamy.java’

--- costbenefit/src/main/java/uk/co/terminological/costbenefit/Kumaraswamy.java
+++ costbenefit/src/main/java/uk/co/terminological/costbenefit/Kumaraswamy.java
@@ -166,6 +166,7 @@ public class Kumaraswamy extends ClassifierModel<Double> {
 		.config().withXScale(0F, 1F)
 		.withXLabel("x")
 		.withYLabel("density")
+		.withYScale(0F, 10F)
 		.done()
 		.withSeries(SeriesBuilder.range(0D, 1D, 1000)).withColourScheme(ColourScheme.Dark2)
 		.bind(X, t -> t)
