863bb159416b4bf2992c84cca744d3ff8b8cbd8a
Rob Challen <rob@terminological.co.uk>
2019-10-04 16:26:33 +0100

    / ‘pubmedclient/src/main/java/uk/co/terminological/bibliography/opencitations/ListResult.java’

--- pubmedclient/src/main/java/uk/co/terminological/bibliography/opencitations/ListResult.java
+++ pubmedclient/src/main/java/uk/co/terminological/bibliography/opencitations/ListResult.java
@@ -1,13 +1,10 @@
 package uk.co.terminological.bibliography.opencitations;
 
-import java.util.List;
-import java.util.Optional;
 import java.util.stream.Stream;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
 import uk.co.terminological.bibliography.ExtensibleJson;
-import uk.co.terminological.bibliography.crossref.Message;
 
 public class ListResult extends ExtensibleJson {
 	
