bd741fde4fdfc747fa8ad0b770a32b0998d69ebf
Rob Challen <rob@terminological.co.uk>
2019-10-04 16:26:58 +0100

    / ‘pubmedclient/src/main/java/uk/co/terminological/bibliography/opencitations/Citation.java’

--- pubmedclient/src/main/java/uk/co/terminological/bibliography/opencitations/Citation.java
+++ pubmedclient/src/main/java/uk/co/terminological/bibliography/opencitations/Citation.java
@@ -1,7 +1,12 @@
 package uk.co.terminological.bibliography.opencitations;
 
+import com.fasterxml.jackson.databind.JsonNode;
+
 import uk.co.terminological.bibliography.ExtensibleJson;
 
 public class Citation extends ExtensibleJson {
-
+	
+	public Citation(JsonNode node) { super(node); }
+	
+	
 }
