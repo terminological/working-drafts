9d6790d8b974363a8c6624828e81f52e5f322d4d
Rob Challen <rob@terminological.co.uk>
2019-10-07 13:14:41 +0100

    / ‘pubmedclient/src/main/java/uk/co/terminological/bibliography/europepmc/Citation.java’

--- pubmedclient/src/main/java/uk/co/terminological/bibliography/europepmc/Citation.java
+++ pubmedclient/src/main/java/uk/co/terminological/bibliography/europepmc/Citation.java
@@ -2,6 +2,23 @@ package uk.co.terminological.bibliography.europepmc;
 
 import uk.co.terminological.bibliography.ExtensibleJson;
 
+/*
+
+id: "31481663",
+source: "MED",
+citationType: "research-article; journal article",
+title: "Pituitary cell translation and secretory capacities are enhanced cell autonomously by the transcription factor Creb3l2.",
+authorString: "Khetchoumian K, Balsalobre A, Mayran A, Christian H, Chénard V, St-Pierre J, Drouin J.",
+journalAbbreviation: "Nat Commun",
+pubYear: 2019,
+volume: "10",
+issue: "1",
+pageInfo: "3960",
+citedByCount: 0 
+
+*/
+
+
 public class Citation extends ExtensibleJson {
 
 }
