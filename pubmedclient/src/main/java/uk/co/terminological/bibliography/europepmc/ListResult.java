b615b0a0f78f0631a09b557ccda462512c681044
Rob Challen <rob@terminological.co.uk>
2019-10-07 13:15:11 +0100

    / ‘pubmedclient/src/main/java/uk/co/terminological/bibliography/europepmc/ListResult.java’

--- pubmedclient/src/main/java/uk/co/terminological/bibliography/europepmc/ListResult.java
+++ pubmedclient/src/main/java/uk/co/terminological/bibliography/europepmc/ListResult.java
@@ -1,5 +1,8 @@
 package uk.co.terminological.bibliography.europepmc;
 
+import java.util.Optional;
+import java.util.stream.Stream;
+
 import com.fasterxml.jackson.databind.JsonNode;
 
 import uk.co.terminological.bibliography.ExtensibleJson;
@@ -17,5 +20,17 @@ public class ListResult<X extends ExtensibleJson> extends ExtensibleJson {
 		return new ListResult<Y>(clazz,raw);
 	}
 	
+	public Stream<X> getItems() {
+		return Stream.concat(Stream.concat(
+				this.streamPath(clazz,"referenceList","reference"),
+				this.streamPath(clazz,"citationList","citation")),
+				this.streamPath(clazz,"resultList","result")
+				);
+		
+	}
 	
+	public Optional<String> nextCursorMark() {return this.asString("nextCursorMark");}
+	public Optional<Long> hitCount() {return this.asLong("hitCount");}
+	public Optional<Long> pageSize() {return this.streamPath("request","pageSize").findFirst().map(jn -> jn.asLong());}
+	public Optional<Long> pageNumber() {return this.streamPath("request","page").findFirst().map(jn -> jn.asLong());}
 }
