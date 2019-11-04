8536444dfd919f5dcfeacd64d0e9725f9023f03d
Rob Challen <rob@terminological.co.uk>
2019-11-04 12:28:30 +0000

    / ‘pubmedclient/src/main/java/uk/co/terminological/bibliography/record/RecordWithCitations.java’

--- pubmedclient/src/main/java/uk/co/terminological/bibliography/record/RecordWithCitations.java
+++ pubmedclient/src/main/java/uk/co/terminological/bibliography/record/RecordWithCitations.java
@@ -3,11 +3,9 @@ package uk.co.terminological.bibliography.record;
 import java.util.Optional;
 import java.util.stream.Stream;
 
-import uk.co.terminological.bibliography.crossref.Reference;
-
 public interface RecordWithCitations extends Record {
 
-	public Stream<? extends Reference> getCitations();
+	public Stream<? extends CitationLink> getCitations();
 	public Optional<Long> getCitedByCount();
 	public Optional<Long> getReferencesCount();
 	 		
