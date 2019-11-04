0fa56309941435cd8e627fdbac3f98fb5815f2ee
Rob Challen <rob@terminological.co.uk>
2019-11-04 16:58:33 +0000

    / ‘pubmedclient/src/main/java/uk/co/terminological/bibliography/europepmc/EuropePmcAuthor.java’

--- pubmedclient/src/main/java/uk/co/terminological/bibliography/europepmc/EuropePmcAuthor.java
+++ pubmedclient/src/main/java/uk/co/terminological/bibliography/europepmc/EuropePmcAuthor.java
@@ -1,5 +1,52 @@
 package uk.co.terminological.bibliography.europepmc;
 
-public class EuropePmcAuthor {
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import uk.co.terminological.bibliography.ExtensibleJson;
+import uk.co.terminological.bibliography.record.Author;
+
+public class EuropePmcAuthor extends ExtensibleJson implements Author {
+
+	/*
+	 * authorList
+author
+fullName
+firstName
+lastName
+initials
+authorId
+affiliation
+affiliationOrgId
+	 */
+	
+	public EuropePmcAuthor(JsonNode node) { super(node); }
+	
+	@Override
+	public Optional<String> getORCID() {
+		return this.streamPath("authorId","value").findFirst().map(n -> n.asString())
+	}
+
+	@Override
+	public Optional<String> getFirstName() {
+		return this.asString("firstName");
+	}
+
+	@Override
+	public String getLastName() {
+		return this.asString("lastName").orElse(this.asString("fullName").orElse("Unknown"));
+	}
+
+	@Override
+	public Optional<String> getInitials() {
+		return this.asString("initials");
+	}
+
+	@Override
+	public Stream<String> getAffiliations() {
+		return this.asString("affiliation").stream();
+	}
 
 }
