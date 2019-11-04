18db2c796aae2f09ff3c15bf6ce10a38f54ab521
Rob Challen <rob@terminological.co.uk>
2019-11-04 14:44:20 +0000

    / ‘pubmedclient/src/main/java/uk/co/terminological/bibliography/record/CitationSource.java’

--- pubmedclient/src/main/java/uk/co/terminological/bibliography/record/CitationSource.java
+++ pubmedclient/src/main/java/uk/co/terminological/bibliography/record/CitationSource.java
@@ -1,5 +1,11 @@
 package uk.co.terminological.bibliography.record;
 
+import java.util.stream.Stream;
+
+import uk.co.terminological.bibliography.opencitations.Citation;
+
 public interface CitationSource {
 
+	public Stream<Citation> getCitations()
+	
 }
