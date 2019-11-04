d944f1866330e758edc351a86d9ef26e70348f91
Rob Challen <rob@terminological.co.uk>
2019-11-04 12:57:32 +0000

    / ‘pubmedclient/src/main/java/uk/co/terminological/bibliography/entrez/MeshHeading.java’

--- pubmedclient/src/main/java/uk/co/terminological/bibliography/entrez/MeshHeading.java
+++ pubmedclient/src/main/java/uk/co/terminological/bibliography/entrez/MeshHeading.java
@@ -3,9 +3,10 @@ package uk.co.terminological.bibliography.entrez;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import uk.co.terminological.bibliography.record.Raw;
 import uk.co.terminological.fluentxml.XmlElement;
 
-public class MeshHeading {
+public class MeshHeading implements Raw<XmlElement> {
 	private XmlElement raw;
 	public MeshHeading(XmlElement raw) {this.raw = raw;}
 	public MeshCode getDescriptor() {
@@ -16,5 +17,9 @@ public class MeshHeading {
 	}
 	public String toString() { 
 		return getDescriptor().toString()+ " ["+getQualifiers().map(q -> q.toString()).collect(Collectors.joining("; "))+"]";
+	}
+	@Override
+	public XmlElement getRaw() {
+		return raw;
 	}
 }
