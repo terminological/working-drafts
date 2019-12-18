98164078ecc2cf43697345ea9485f286792ae681
Rob Challen <rob@terminological.co.uk>
2019-12-18 21:36:18 +0000

    / ‘pubmedclient/src/main/java/uk/co/terminological/bibliography/client/IdMapper.java’

--- pubmedclient/src/main/java/uk/co/terminological/bibliography/client/IdMapper.java
+++ pubmedclient/src/main/java/uk/co/terminological/bibliography/client/IdMapper.java
@@ -1,6 +1,11 @@
 package uk.co.terminological.bibliography.client;
 
+import java.util.Collection;
+
+import uk.co.terminological.bibliography.record.RecordIdentifierMapping;
+import uk.co.terminological.bibliography.record.RecordReference;
+
 public interface IdMapper {
 
-	public Collection<Identifier> mappings()
+	public Collection<RecordIdentifierMapping> mappings(RecordReference source);
 }
