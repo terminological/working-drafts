08dbdb8a76c3057995fe0d7e05d021bd6c338544
Rob Challen <rob@terminological.co.uk>
2019-12-18 13:04:18 +0000

    / ‘pubmedclient/src/main/java/uk/co/terminological/bibliography/client/Searcher.java’

--- pubmedclient/src/main/java/uk/co/terminological/bibliography/client/Searcher.java
+++ pubmedclient/src/main/java/uk/co/terminological/bibliography/client/Searcher.java
@@ -10,4 +10,18 @@ public interface Searcher {
 
 	Collection<? extends Record> search(String search, Optional<Date> from,  Optional<Date> to, Optional<Integer> limit);
 	
+	default Collection<? extends Record> search(String search) {
+		return search(search, Optional.empty(), Optional.empty(), Optional.empty());
+	};
+	
+	default Collection<? extends Record> search(String search, Integer limit) {
+		return search(search, Optional.empty(), Optional.empty(), Optional.of(limit));
+	};
+	
+	default Collection<? extends Record> search(String search, Date from, Date to) {
+		return search(search, Optional.of(from), Optional.of(to), Optional.empty());
+	};
+	
+	
+	
 }
