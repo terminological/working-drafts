a00a4426558c7c7297da1887f464ab384dd102e8
Rob Challen <rob@terminological.co.uk>
2019-12-18 13:07:10 +0000

    / ‘pubmedclient/src/main/java/uk/co/terminological/bibliography/client/Searcher.java’

--- pubmedclient/src/main/java/uk/co/terminological/bibliography/client/Searcher.java
+++ pubmedclient/src/main/java/uk/co/terminological/bibliography/client/Searcher.java
@@ -1,14 +1,14 @@
 package uk.co.terminological.bibliography.client;
 
+import java.time.LocalDate;
 import java.util.Collection;
-import java.util.Date;
 import java.util.Optional;
 
 import uk.co.terminological.bibliography.record.Record;
 
 public interface Searcher {
 
-	Collection<? extends Record> search(String search, Optional<Date> from,  Optional<Date> to, Optional<Integer> limit);
+	Collection<? extends Record> search(String search, Optional<LocalDate> from,  Optional<LocalDate> to, Optional<Integer> limit);
 	
 	default Collection<? extends Record> search(String search) {
 		return search(search, Optional.empty(), Optional.empty(), Optional.empty());
@@ -18,7 +18,7 @@ public interface Searcher {
 		return search(search, Optional.empty(), Optional.empty(), Optional.of(limit));
 	};
 	
-	default Collection<? extends Record> search(String search, Date from, Date to) {
+	default Collection<? extends Record> search(String search, LocalDate from, LocalDate to) {
 		return search(search, Optional.of(from), Optional.of(to), Optional.empty());
 	};
 	
