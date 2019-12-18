154c18fa65cd44241bf6a3fc0b50eb642d0dbc46
Rob Challen <rob@terminological.co.uk>
2019-12-18 14:08:12 +0000

    / ‘pubmedclient/src/main/java/uk/co/terminological/bibliography/client/IdLocator.java’

--- pubmedclient/src/main/java/uk/co/terminological/bibliography/client/IdLocator.java
+++ pubmedclient/src/main/java/uk/co/terminological/bibliography/client/IdLocator.java
@@ -1,12 +1,29 @@
 package uk.co.terminological.bibliography.client;
 
+import java.util.Collections;
 import java.util.Optional;
+import java.util.Set;
 
 import uk.co.terminological.bibliography.record.IdType;
 import uk.co.terminological.bibliography.record.Record;
+import uk.co.terminological.bibliography.record.RecordReference;
 
 public interface IdLocator {
 
-	Optional<? extends Record> getById(IdType type, String id);
+	Optional<? extends Record> getOneById(Set<RecordReference> equivalentIds);
+	
+	default Optional<? extends Record> getById(IdType type, String id) {
+		return getOneById(Collections.singleton(new RecordReference() {
+
+			@Override
+			public Optional<String> getIdentifier() {
+				return Optional.of(id);
+			}
+
+			@Override
+			public IdType getIdentifierType() {
+				return type;
+			}}));
+	};
 	
 }
