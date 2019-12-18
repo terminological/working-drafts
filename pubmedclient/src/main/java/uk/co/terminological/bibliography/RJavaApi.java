44c73ff051c272ae741346c336a0ef08dbaeb78a
Rob Challen <rob@terminological.co.uk>
2019-12-18 23:01:12 +0000

    / ‘pubmedclient/src/main/java/uk/co/terminological/bibliography/RJavaApi.java’

--- pubmedclient/src/main/java/uk/co/terminological/bibliography/RJavaApi.java
+++ pubmedclient/src/main/java/uk/co/terminological/bibliography/RJavaApi.java
@@ -1,6 +1,13 @@
 package uk.co.terminological.bibliography;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class RJavaApi {
 	//TODO: more task based methods 
 	//TODO: 
+	
+	public List<String> getSupportedClients() {
+		return Arrays.asList("crossref","entrez","europepmc","opencitations","pmcid","unpaywall");
+	}
 }
