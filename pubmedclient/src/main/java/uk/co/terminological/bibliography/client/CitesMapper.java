42b2480aaa55c09e2fdb0eb136e61500da42589d
Rob Challen <rob@terminological.co.uk>
2019-12-18 15:26:39 +0000

    / ‘pubmedclient/src/main/java/uk/co/terminological/bibliography/client/CitesMapper.java’

--- pubmedclient/src/main/java/uk/co/terminological/bibliography/client/CitesMapper.java
+++ pubmedclient/src/main/java/uk/co/terminological/bibliography/client/CitesMapper.java
@@ -2,11 +2,8 @@ package uk.co.terminological.bibliography.client;
 
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Map;
-import java.util.Set;
 
 import uk.co.terminological.bibliography.record.CitationLink;
-import uk.co.terminological.bibliography.record.RecordIdentifier;
 import uk.co.terminological.bibliography.record.RecordReference;
 
 public interface CitesMapper {
