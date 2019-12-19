ab17200871b7056444e3145d158859618672f211
Rob Challen <rob@terminological.co.uk>
2019-12-19 11:52:56 +0000

    / ‘pubmedclient/src/main/java/uk/co/terminological/bibliography/client/CitesMapper.java’

--- pubmedclient/src/main/java/uk/co/terminological/bibliography/client/CitesMapper.java
+++ pubmedclient/src/main/java/uk/co/terminological/bibliography/client/CitesMapper.java
@@ -2,15 +2,16 @@ package uk.co.terminological.bibliography.client;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Set;
 
 import uk.co.terminological.bibliography.record.CitationLink;
 import uk.co.terminological.bibliography.record.RecordReference;
 
 public interface CitesMapper {
 	
-	public Collection<? extends CitationLink> citesReferences(Collection<RecordReference> ref);
+	public Set<? extends CitationLink> citesReferences(Collection<RecordReference> ref);
 	
-	default Collection<? extends CitationLink> citesReferences(RecordReference ref) {
+	default Set<? extends CitationLink> citesReferences(RecordReference ref) {
 		return citesReferences(Collections.singleton(ref));
 	};
 	
